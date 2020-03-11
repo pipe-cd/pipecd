6af761b38918042c2381ad85d07bf1e5bcf48dbc
Le Van Nghia <nghialv2607@gmail.com>
2020-03-11 23:35:56 +0900

    Preinstall helm in runner-base (#4)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```

---
+++ dockers/runner-base/dockertest/check-helm.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The Pipe Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+declare -A pathcases
+pathcases["helm-2.16.3"]="/usr/local/runner/helm-2.16.3"
+pathcases["helm-2.16"]="/usr/local/runner/helm-2.16"
+pathcases["helm-3.1.1"]="/usr/local/runner/helm-3.1.1"
+pathcases["helm-3.1"]="/usr/local/runner/helm-3.1"
+
+for h in "${!pathcases[@]}"
+do
+    got=$(which $h)
+    want=${pathcases[$h]}
+    if [[ ${got} == ${want} ]]; then
+        echo -e "PASSED: Correct path for ${h}."
+        echo "  want: ${want}"
+        echo "  got : ${got}"
+    else
+        echo "FAILED: Wrong path for ${h}."
+        echo "  want: ${want}"
+        echo "  got : ${got}"
+        exit 1
+    fi
+done
+
+declare -A versioncases
+versioncases["helm-2.16.3"]="Client: \&version.Version{SemVer:\"v2.16.3\", GitCommit:\"1ee0254c86d4ed6887327dabed7aa7da29d7eb0d\", GitTreeState:\"clean\"}"
+versioncases["helm-2.16"]="Client: \&version.Version{SemVer:\"v2.16.3\", GitCommit:\"1ee0254c86d4ed6887327dabed7aa7da29d7eb0d\", GitTreeState:\"clean\"}"
+versioncases["helm-3.1.1"]="version.BuildInfo{Version:\"v3.1.1\", GitCommit:\"afe70585407b420d0097d07b21c47dc511525ac8\", GitTreeState:\"clean\", GoVersion:\"go1.13.8\"}"
+versioncases["helm-3.1"]="version.BuildInfo{Version:\"v3.1.1\", GitCommit:\"afe70585407b420d0097d07b21c47dc511525ac8\", GitTreeState:\"clean\", GoVersion:\"go1.13.8\"}"
+
+for h in "${!versioncases[@]}"
+do
+    got=$($h version --client)
+    want=${versioncases[$h]}
+    if [[ ${got} == ${want} ]]; then
+        echo -e "PASSED: Correct version for ${h}."
+        echo "  want: ${want}"
+        echo "  got : ${got}"
+    else
+        echo "FAILED: Wrong version for ${h}."
+        echo "  want: ${want}"
+        echo "  got : ${got}"
+        exit 1
+    fi
+done
---
+++ dockers/runner-base/install-helm.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The Pipe Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+BASE_URL="https://get.helm.sh"
+
+for version in "2.16.3" "3.1.1"
+do
+  shortversion=${version%.*}
+  echo "Installing helm-${version} into ${RUNNER_BIN_DIR}/helm-${shortversion}..."
+  curl -L ${BASE_URL}/helm-v${version}-linux-amd64.tar.gz | tar xvz
+  mv linux-amd64/helm ${RUNNER_BIN_DIR}/helm-${shortversion}
+  chmod +x ${RUNNER_BIN_DIR}/helm-${shortversion}
+  ln -s ${RUNNER_BIN_DIR}/helm-${shortversion} ${RUNNER_BIN_DIR}/helm-${version}
+  rm -rf linux-amd64
+  echo "Successfully installed helm-${version} into ${RUNNER_BIN_DIR}/helm-${shortversion}..."
+done
