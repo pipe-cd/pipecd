c94adaeee8ae30eee8c11685632f0841346dab6c
Akinori Yamada <stormcat24@stormcat.io>
2020-06-01 22:11:29 +0900

    Add deployment detail API interface and fake response (#164)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/api/api/web_api.go
+++ pkg/app/api/api/web_api.go
@@ -16,6 +16,7 @@ package api
 
 import (
 	"context"
+	"time"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -23,6 +24,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/kapetaniosci/pipe/pkg/app/api/service/webservice"
+	"github.com/kapetaniosci/pipe/pkg/model"
 )
 
 // PipedAPI implements the behaviors for the gRPC definitions of WebAPI.
@@ -87,8 +89,204 @@ func (a *WebAPI) ListDeployments(ctx context.Context, req *webservice.ListDeploy
 	return nil, status.Error(codes.Unimplemented, "")
 }
 
-func (a *WebAPI) GetDeployment(ctx context.Context, req *webservice.GetDeploymentRequest) (*webservice.GetDeploymentResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "")
+func (a *WebAPI) GetDeploymentDetail(ctx context.Context, req *webservice.GetDeploymentDetailRequest) (*webservice.GetDeploymentDetailResponse, error) {
+	// Creating fake response
+	now := time.Now()
+	resp := &model.Deployment{
+		Id:            "debug-deployment-id",
+		ApplicationId: "debug-project/development/debug-app",
+		EnvId:         "development",
+		PipedId:       "debug-piped",
+		ProjectId:     "debug-project",
+		Kind:          model.ApplicationKind_KUBERNETES,
+		GitPath: &model.ApplicationGitPath{
+			RepoId: "pipe-debug",
+			Path:   "k8s",
+		},
+		Trigger: &model.DeploymentTrigger{
+			Commit: &model.Commit{
+				Hash:      "3808585b46f1e90196d7ffe8dd04c807a251febc",
+				Message:   "Add web page routing (#133)",
+				Author:    "cakecatz",
+				Branch:    "master",
+				CreatedAt: now.Add(-30 * time.Minute).Unix(),
+			},
+			User:      "cakecatz",
+			Timestamp: now.Add(-30 * time.Minute).Unix(),
+		},
+		RunningCommitHash: "3808585b46f1e90196d7ffe8dd04c807a251febc",
+		Description:       "This deployment is debug",
+		Status:            model.DeploymentStatus_DEPLOYMENT_RUNNING,
+		Stages: []*model.PipelineStage{
+			{
+				Id:           "fake-stage-id-0-0",
+				Name:         model.StageK8sCanaryRollout.String(),
+				Index:        0,
+				Predefined:   true,
+				Status:       model.StageStatus_STAGE_SUCCESS,
+				RetriedCount: 0,
+				CompletedAt:  now.Unix(),
+				CreatedAt:    now.Unix(),
+				UpdatedAt:    now.Unix(),
+			},
+			{
+				Id:         "fake-stage-id-1-0",
+				Name:       model.StageK8sCanaryRollout.String(),
+				Index:      0,
+				Predefined: true,
+				Requires: []string{
+					"fake-stage-id-0-0",
+				},
+				Status:       model.StageStatus_STAGE_RUNNING,
+				RetriedCount: 0,
+				CompletedAt:  0,
+				CreatedAt:    now.Unix(),
+				UpdatedAt:    now.Unix(),
+			},
+			{
+				Id:         "fake-stage-id-1-1",
+				Name:       model.StageK8sPrimaryUpdate.String(),
+				Index:      1,
+				Predefined: true,
+				Requires: []string{
+					"fake-stage-id-0-0",
+				},
+				Status:       model.StageStatus_STAGE_SUCCESS,
+				RetriedCount: 0,
+				CompletedAt:  now.Unix(),
+				CreatedAt:    now.Unix(),
+				UpdatedAt:    now.Unix(),
+			},
+			{
+				Id:         "fake-stage-id-1-2",
+				Name:       model.StageK8sCanaryRollout.String(),
+				Index:      2,
+				Predefined: true,
+				Requires: []string{
+					"fake-stage-id-0-0",
+				},
+				Status:       model.StageStatus_STAGE_FAILURE,
+				RetriedCount: 0,
+				CompletedAt:  now.Unix(),
+				CreatedAt:    now.Unix(),
+				UpdatedAt:    now.Unix(),
+			},
+			{
+				Id:         "fake-stage-id-2-0",
+				Name:       model.StageK8sCanaryClean.String(),
+				Desc:       "waiting approval",
+				Index:      0,
+				Predefined: true,
+				Requires: []string{
+					"fake-stage-id-1-0",
+					"fake-stage-id-1-1",
+					"fake-stage-id-1-2",
+				},
+				Status:       model.StageStatus_STAGE_NOT_STARTED_YET,
+				RetriedCount: 0,
+				CompletedAt:  0,
+				CreatedAt:    now.Unix(),
+				UpdatedAt:    now.Unix(),
+			},
+			{
+				Id:         "fake-stage-id-2-1",
+				Name:       model.StageK8sCanaryClean.String(),
+				Desc:       "approved by cakecatz",
+				Index:      1,
+				Predefined: true,
+				Requires: []string{
+					"fake-stage-id-1-0",
+					"fake-stage-id-1-1",
+					"fake-stage-id-1-2",
+				},
+				Status:       model.StageStatus_STAGE_NOT_STARTED_YET,
+				RetriedCount: 0,
+				CompletedAt:  0,
+				CreatedAt:    now.Unix(),
+				UpdatedAt:    now.Unix(),
+			},
+			{
+				Id:         "fake-stage-id-3-0",
+				Name:       model.StageK8sCanaryRollout.String(),
+				Index:      0,
+				Predefined: true,
+				Requires: []string{
+					"fake-stage-id-2-0",
+					"fake-stage-id-2-1",
+				},
+				Status:       model.StageStatus_STAGE_NOT_STARTED_YET,
+				RetriedCount: 0,
+				CompletedAt:  0,
+				CreatedAt:    now.Unix(),
+				UpdatedAt:    now.Unix(),
+			},
+		},
+		CreatedAt: now.Unix(),
+		UpdatedAt: now.Unix(),
+	}
+
+	return &webservice.GetDeploymentDetailResponse{
+		Deployment: resp,
+	}, nil
+}
+
+func (a *WebAPI) GetDeploymentStageLog(ctx context.Context, req *webservice.GetDeploymentStageLogRequest) (*webservice.GetDeploymentStageLogResponse, error) {
+	// Creating fake response
+	startTime := time.Now().Add(-10 * time.Minute)
+	resp := []*model.LogBlock{
+		{
+			Index:     1,
+			Log:       "+ make build",
+			Severity:  model.LogSeverity_INFO,
+			CreatedAt: startTime.Unix(),
+		},
+		{
+			Index:     2,
+			Log:       "bazelisk  --output_base=/workspace/bazel_out build  --config=ci -- //...",
+			Severity:  model.LogSeverity_INFO,
+			CreatedAt: startTime.Add(5 * time.Second).Unix(),
+		},
+		{
+			Index:     3,
+			Log:       "2020/06/01 08:52:07 Downloading https://releases.bazel.build/3.1.0/release/bazel-3.1.0-linux-x86_64...",
+			Severity:  model.LogSeverity_INFO,
+			CreatedAt: startTime.Add(10 * time.Second).Unix(),
+		},
+		{
+			Index:     4,
+			Log:       "Extracting Bazel installation...",
+			Severity:  model.LogSeverity_INFO,
+			CreatedAt: startTime.Add(15 * time.Second).Unix(),
+		},
+		{
+			Index:     5,
+			Log:       "Starting local Bazel server and connecting to it...",
+			Severity:  model.LogSeverity_INFO,
+			CreatedAt: startTime.Add(20 * time.Second).Unix(),
+		},
+		{
+			Index:     6,
+			Log:       "(08:52:14) Loading: 0 packages loaded",
+			Severity:  model.LogSeverity_SUCCESS,
+			CreatedAt: startTime.Add(30 * time.Second).Unix(),
+		},
+		{
+			Index:     7,
+			Log:       "(08:53:21) Analyzing: 157 targets (88 packages loaded, 0 targets configured)",
+			Severity:  model.LogSeverity_SUCCESS,
+			CreatedAt: startTime.Add(35 * time.Second).Unix(),
+		},
+		{
+			Index:     8,
+			Log:       "Error: Error building: logged 2 error(s)",
+			Severity:  model.LogSeverity_ERROR,
+			CreatedAt: startTime.Add(45 * time.Second).Unix(),
+		},
+	}
+
+	return &webservice.GetDeploymentStageLogResponse{
+		Blocks: resp,
+	}, nil
 }
 
 func (a *WebAPI) CancelDeployment(ctx context.Context, req *webservice.CancelDeploymentRequest) (*webservice.CancelDeploymentResponse, error) {
