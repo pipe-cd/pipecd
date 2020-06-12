a40a6ca0a27463a0da4ba6bc6fe3b6600c277eb6
Ryo Narita <mail.ryonarita@gmail.com>
2020-06-12 15:31:10 +0900

    Fix link components (#278)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/web/src/components/header.tsx
+++ pkg/app/web/src/components/header.tsx
@@ -6,26 +6,29 @@ import {
   Button,
   makeStyles,
   Avatar,
-  Link
+  Link,
 } from "@material-ui/core";
 import {
   APP_NAME,
   PAGE_PATH_APPLICATIONS,
-  PAGE_PATH_DEPLOYMENTS
+  PAGE_PATH_DEPLOYMENTS,
+  PAGE_PATH_INSIGHTS,
+  PAGE_PATH_SETTINGS,
 } from "../constants";
+import { Link as RouterLink } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   title: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   appIcon: {
     marginRight: theme.spacing(2),
     width: theme.spacing(4),
-    height: theme.spacing(4)
+    height: theme.spacing(4),
   },
   link: {
-    marginRight: theme.spacing(2)
-  }
+    marginRight: theme.spacing(2),
+  },
 }));
 
 export const Header: React.FC = () => {
@@ -38,23 +41,35 @@ export const Header: React.FC = () => {
           {APP_NAME}
         </Typography>
         <Link
+          component={RouterLink}
           className={classes.link}
           color="inherit"
-          href={PAGE_PATH_APPLICATIONS}
+          to={PAGE_PATH_APPLICATIONS}
         >
           Applications
         </Link>
         <Link
+          component={RouterLink}
           className={classes.link}
           color="inherit"
-          href={PAGE_PATH_DEPLOYMENTS}
+          to={PAGE_PATH_DEPLOYMENTS}
         >
           Deployments
         </Link>
-        <Link className={classes.link} color="inherit">
+        <Link
+          component={RouterLink}
+          className={classes.link}
+          color="inherit"
+          to={PAGE_PATH_INSIGHTS}
+        >
           Insights
         </Link>
-        <Link className={classes.link} color="inherit">
+        <Link
+          component={RouterLink}
+          className={classes.link}
+          color="inherit"
+          to={PAGE_PATH_SETTINGS}
+        >
           Settings
         </Link>
         <Button color="inherit">Login</Button>
--- pkg/app/web/src/constants.ts
+++ pkg/app/web/src/constants.ts
@@ -4,3 +4,5 @@ export const APP_NAME = "PipeCD";
 export const PAGE_PATH_TOP = "/";
 export const PAGE_PATH_DEPLOYMENTS = "/deployments";
 export const PAGE_PATH_APPLICATIONS = "/applications";
+export const PAGE_PATH_INSIGHTS = "/insights";
+export const PAGE_PATH_SETTINGS = "/settings";
