5e176b8838735da3fe8841d6300f24999b04aaf3
Ryo Narita <mail.ryonarita@gmail.com>
2020-06-23 17:20:47 +0900

    Fix storybook stories

--- pkg/app/web/src/components/application-state-view.stories.tsx
+++ pkg/app/web/src/components/application-state-view.stories.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { ApplicationStateView } from "./application-state-view";
+import { createDecoratorRedux } from "../../.storybook/redux-decorator";
 
 export default {
   title: "ApplicationStateView",
   component: ApplicationStateView,
+  decorators: [createDecoratorRedux({})],
 };
 
 export const overview: React.FC = () => (
