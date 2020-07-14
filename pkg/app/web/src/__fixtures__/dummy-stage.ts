c7d3f74d8df7d10b95e1736fa9c21054e9f4371b
Ryo Narita <mail.ryonarita@gmail.com>
2020-07-14 14:41:06 +0900

    Fix incorect line between 2 stage nodes (#403)
    **What this PR does / why we need it**:
    
    Change stage node width as same as other nodes attribute to same column.
    
    ![Kapture 2020-07-14 at 14 31 26](https://user-images.githubusercontent.com/6136383/87387970-ca8ef700-c5de-11ea-853a-5b1e21886d78.gif)
    
    
    **Which issue(s) this PR fixes**:
    
    Fixes #332
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/web/src/__fixtures__/dummy-stage.ts
@@ -0,0 +1,18 @@
+import { Stage } from "../modules/deployments";
+
+export const dummyStage: Stage = {
+  id: "fake-stage-id-0-0",
+  name: "K8S_CANARY_ROLLOUT",
+  desc: "",
+  index: 0,
+  predefined: true,
+  requiresList: [],
+  visible: true,
+  status: 2,
+  statusDescription: "",
+  metadataMap: [],
+  retriedCount: 0,
+  completedAt: 1592203166,
+  createdAt: 1592203166,
+  updatedAt: 1592203166,
+};
