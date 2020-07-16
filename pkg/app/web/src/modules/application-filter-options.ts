3b1f08c2914968aa1448f58c9d6e6346985ab272
Ryo Narita <mail.ryonarita@gmail.com>
2020-07-16 19:18:37 +0900

    Implement application enabling

---
+++ pkg/app/web/src/modules/application-filter-options.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ApplicationKind, ApplicationSyncStatus } from "./applications";
+
+export type ApplicationFilterOptions = {
+  enabled?: { value: boolean };
+  kindsList: ApplicationKind[];
+  envIdsList: string[];
+  syncStatusesList: ApplicationSyncStatus[];
+};
+
+const initialState: ApplicationFilterOptions = {
+  enabled: { value: true },
+  kindsList: [],
+  envIdsList: [],
+  syncStatusesList: [],
+};
+
+export const applicationFilterOptionsSlice = createSlice({
+  name: "applicationFilterOptions",
+  initialState,
+  reducers: {
+    updateApplicationFilter(
+      state,
+      action: PayloadAction<Partial<ApplicationFilterOptions>>
+    ) {
+      return { ...state, ...action.payload };
+    },
+    clearApplicationFilter() {
+      return initialState;
+    },
+  },
+});
+
+export const {
+  updateApplicationFilter,
+  clearApplicationFilter,
+} = applicationFilterOptionsSlice.actions;
