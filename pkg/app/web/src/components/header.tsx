50344db8baae32bd0049309187dcf306a6f6a5bf
Ryo Narita <mail.ryonarita@gmail.com>
2020-08-21 18:39:56 +0900

    Add logout link to user menu (#677)
    **What this PR does / why we need it**:
    
    ![image](https://user-images.githubusercontent.com/6136383/90875872-bc51a900-e3dc-11ea-8b71-0956f4228d42.png)
    
    
    **Which issue(s) this PR fixes**:
    
    Fixes #256 
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/web/src/components/header.tsx
+++ pkg/app/web/src/components/header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo } from "react";
+import React, { FC, memo, useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -7,6 +7,9 @@ import {
   Avatar,
   Link,
   Button,
+  IconButton,
+  MenuItem,
+  Menu,
 } from "@material-ui/core";
 import {
   APP_NAME,
@@ -15,6 +18,7 @@ import {
   PAGE_PATH_INSIGHTS,
   PAGE_PATH_SETTINGS,
   PAGE_PATH_LOGIN,
+  LOGOUT_ENDPOINT,
 } from "../constants";
 import { NavLink as RouterLink } from "react-router-dom";
 import { useMe } from "../modules/me";
@@ -57,6 +61,11 @@ const useStyles = makeStyles((theme) => ({
 export const Header: FC = memo(function Header() {
   const classes = useStyles();
   const me = useMe();
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClose = (): void => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar position="static" className={classes.appBar}>
@@ -111,13 +120,31 @@ export const Header: FC = memo(function Header() {
           Settings
         </Link>
         {me?.isLogin ? (
-          <Avatar className={classes.userAvatar} src={me.avatarUrl} />
+          <IconButton
+            aria-controls="user-menu"
+            aria-haspopup="true"
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+          >
+            <Avatar className={classes.userAvatar} src={me.avatarUrl} />
+          </IconButton>
         ) : (
           <Link color="inherit" component={RouterLink} to={PAGE_PATH_LOGIN}>
             <Typography variant="body2">Login</Typography>
           </Link>
         )}
       </Toolbar>
+
+      <Menu
+        id="user-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem component={Link} href={LOGOUT_ENDPOINT}>
+          Logout
+        </MenuItem>
+      </Menu>
     </AppBar>
   );
 });
