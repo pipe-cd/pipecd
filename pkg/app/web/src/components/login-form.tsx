8836b30a11aac432d667703be97435a9f05db148
Ryo Narita <mail.ryonarita@gmail.com>
2020-08-21 17:47:59 +0900

    Add github login button to the login form

--- pkg/app/web/src/components/login-form.tsx
+++ pkg/app/web/src/components/login-form.tsx
@@ -1,8 +1,9 @@
 import React, { FC, memo } from "react";
 import { makeStyles, TextField, Button, Typography } from "@material-ui/core";
-import { STATIC_LOGIN_ENDPOINT } from "../constants";
+import { STATIC_LOGIN_ENDPOINT, LOGIN_ENDPOINT } from "../constants";
 import { useProjectName, clearProjectName } from "../modules/login";
 import { useDispatch } from "react-redux";
+import { MarkGithubIcon } from "@primer/octicons-react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,12 +12,15 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     flex: 1,
   },
+  content: {
+    width: 320,
+  },
   form: {
     display: "flex",
     flexDirection: "column",
     textAlign: "center",
     marginTop: theme.spacing(4),
-    width: 320,
+    width: "100%",
   },
   fields: {
     display: "flex",
@@ -27,6 +31,27 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "flex-end",
     marginTop: theme.spacing(3),
+  },
+  githubMark: {
+    marginRight: theme.spacing(1),
+  },
+  githubLoginButton: {
+    background: "#24292E",
+  },
+  divider: {
+    display: "flex",
+    alignItems: "center",
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+  },
+  dividerText: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+  },
+  line: {
+    flex: 1,
+    border: "none",
+    borderTop: "1px solid #ddd",
   },
 }));
 
@@ -42,43 +67,69 @@ export const LoginForm: FC = memo(function LoginForm() {
   return (
     <div className={classes.root}>
       <Typography variant="h4">Sign in to {projectName}</Typography>
-      <form
-        method="POST"
-        action={STATIC_LOGIN_ENDPOINT}
-        className={classes.form}
-      >
-        <input
-          type="hidden"
-          id="project"
-          name="project"
-          value={projectName || undefined}
-        />
-        <TextField
-          id="username"
-          name="username"
-          label="Username"
-          variant="outlined"
-          margin="dense"
-          required
-        />
-        <TextField
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          variant="outlined"
-          margin="dense"
-          required
-        />
-        <div className={classes.buttons}>
-          <Button type="reset" color="primary" onClick={handleReset}>
-            back
+      <div className={classes.content}>
+        <form method="POST" action={LOGIN_ENDPOINT} className={classes.form}>
+          <input
+            type="hidden"
+            id="project"
+            name="project"
+            value={projectName || undefined}
+          />
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            className={classes.githubLoginButton}
+          >
+            <MarkGithubIcon className={classes.githubMark} />
+            LOGIN WITH GITHUB
           </Button>
-          <Button type="submit" color="primary" variant="contained">
-            login
-          </Button>
+        </form>
+
+        <div className={classes.divider}>
+          <span className={classes.line} />
+          <div className={classes.dividerText}>OR</div>
+          <span className={classes.line} />
         </div>
-      </form>
+
+        <form
+          method="POST"
+          action={STATIC_LOGIN_ENDPOINT}
+          className={classes.form}
+        >
+          <input
+            type="hidden"
+            id="project"
+            name="project"
+            value={projectName || undefined}
+          />
+          <TextField
+            id="username"
+            name="username"
+            label="Username"
+            variant="outlined"
+            margin="dense"
+            required
+          />
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            margin="dense"
+            required
+          />
+          <div className={classes.buttons}>
+            <Button type="reset" color="primary" onClick={handleReset}>
+              back
+            </Button>
+            <Button type="submit" color="primary" variant="contained">
+              login
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 });
