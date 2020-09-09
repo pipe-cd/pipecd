ce58b4896bc7628b04922b200fbba5cfdd8297ba
Ryo Narita <mail.ryonarita@gmail.com>
2020-09-09 13:17:57 +0900

    Update toast texts

--- pkg/app/web/src/constants/toast-text.ts
+++ pkg/app/web/src/constants/toast-text.ts
@@ -1,6 +1,6 @@
-export const UPDATE_STATIC_ADMIN_INFO_SUCCESS = `Updated "Static Admin" settings success.`;
-export const UPDATE_STATIC_ADMIN_INFO_FAILED = `Updated "Static Admin" settings failed.`;
-export const UPDATE_RBAC_SUCCESS = `Updated "Role-Based Access Control" settings success.`;
-export const UPDATE_RBAC_FAILED = `Updated "Role-Based Access Control" settings failed.`;
-export const UPDATE_SSO_SUCCESS = `Updated "Single Sign-On" settings success.`;
-export const UPDATE_SSO_FAILED = `Updated "Single Sign-On" settings failed.`;
+export const UPDATE_STATIC_ADMIN_INFO_SUCCESS = `Successfully updated Static Admin configurations.`;
+export const UPDATE_STATIC_ADMIN_INFO_FAILED = `Failed to update Static Admin configurations.`;
+export const UPDATE_RBAC_SUCCESS = `Successfully updated RBAC configurations.`;
+export const UPDATE_RBAC_FAILED = `Failed to update RBAC configurations.`;
+export const UPDATE_SSO_SUCCESS = `Successfully updated SSO configurations.`;
+export const UPDATE_SSO_FAILED = `Failed to update SSO configurations.`;
