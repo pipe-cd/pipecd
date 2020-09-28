54bc3b6bc3a7612acaaca7737e6c3c4908d8e964
Ryo Narita <mail.ryonarita@gmail.com>
2020-09-28 13:26:17 +0900

    Create and use test utils to test web components (#861)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/web/.storybook/redux-decorator.tsx
+++ pkg/app/web/.storybook/redux-decorator.tsx
@@ -1,21 +1,8 @@
+import { DeepPartial } from "@reduxjs/toolkit";
 import React from "react";
 import { Provider } from "react-redux";
-import configureMockStore from "redux-mock-store";
 import { AppState } from "../src/modules";
-import { reducers } from "../src/modules";
-import {
-  configureStore,
-  DeepPartial,
-  getDefaultMiddleware,
-} from "@reduxjs/toolkit";
-
-const mockStore = configureMockStore(getDefaultMiddleware());
-const store = configureStore({ reducer: reducers });
-const baseState = store.getState();
-
-export const createStore = (initialState: DeepPartial<AppState>) => {
-  return mockStore(Object.assign({}, baseState, initialState));
-};
+import { createStore } from "../test-utils";
 
 export const createDecoratorRedux = (initialState: DeepPartial<AppState>) => (
   storyFn: any
