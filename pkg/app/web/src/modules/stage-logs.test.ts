093d3181b5007325743bfd793b86e3e01a5b638c
Ryo Narita <mail.ryonarita@gmail.com>
2020-10-01 19:42:06 +0900

    Add web tests (#895)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/web/src/modules/stage-logs.test.ts
+++ pkg/app/web/src/modules/stage-logs.test.ts
@@ -1,4 +1,16 @@
-import { stageLogsSlice } from "./stage-logs";
+import {
+  stageLogsSlice,
+  createActiveStageKey,
+  fetchStageLog,
+  StageLog,
+  LogSeverity,
+} from "./stage-logs";
+
+test("createActiveStageKey", () => {
+  expect(
+    createActiveStageKey({ deploymentId: "deployment-1", stageId: "stage-1" })
+  ).toBe("deployment-1stage-1");
+});
 
 describe("stageLogsSlice reducer", () => {
   it("should handle initial state", () => {
@@ -6,6 +18,86 @@ describe("stageLogsSlice reducer", () => {
       stageLogsSlice.reducer(undefined, {
         type: "TEST_ACTION",
       })
-    ).toMatchInlineSnapshot(`Object {}`);
+    ).toEqual({});
+  });
+
+  describe("fetchStageLog", () => {
+    it(`should handle ${fetchStageLog.pending.type}`, () => {
+      expect(
+        stageLogsSlice.reducer(undefined, {
+          type: fetchStageLog.pending.type,
+          meta: {
+            arg: { deploymentId: "deployment-1", stageId: "stage-1" },
+          },
+        })
+      ).toEqual({
+        "deployment-1stage-1": {
+          stageId: "stage-1",
+          deploymentId: "deployment-1",
+          logBlocks: [],
+          completed: false,
+        },
+      });
+    });
+
+    it(`should handle ${fetchStageLog.fulfilled.type}`, () => {
+      const payload: StageLog = {
+        stageId: "stage-1",
+        deploymentId: "deployment-1",
+        logBlocks: [
+          { createdAt: 0, index: 0, log: "log", severity: LogSeverity.SUCCESS },
+        ],
+        completed: false,
+      };
+      expect(
+        stageLogsSlice.reducer(
+          {
+            "deployment-1stage-1": {
+              stageId: "stage-1",
+              deploymentId: "deployment-1",
+              logBlocks: [],
+              completed: false,
+            },
+          },
+          {
+            type: fetchStageLog.fulfilled.type,
+            meta: {
+              arg: { deploymentId: "deployment-1", stageId: "stage-1" },
+            },
+            payload,
+          }
+        )
+      ).toEqual({
+        "deployment-1stage-1": payload,
+      });
+    });
+
+    it(`should handle ${fetchStageLog.rejected.type}`, () => {
+      expect(
+        stageLogsSlice.reducer(
+          {
+            "deployment-1stage-1": {
+              stageId: "stage-1",
+              deploymentId: "deployment-1",
+              logBlocks: [],
+              completed: false,
+            },
+          },
+          {
+            type: fetchStageLog.rejected.type,
+            meta: {
+              arg: { deploymentId: "deployment-1", stageId: "stage-1" },
+            },
+          }
+        )
+      ).toEqual({
+        "deployment-1stage-1": {
+          stageId: "stage-1",
+          deploymentId: "deployment-1",
+          logBlocks: [],
+          completed: true,
+        },
+      });
+    });
   });
 });
