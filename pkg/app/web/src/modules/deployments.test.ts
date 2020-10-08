eb39051746e9cfff465b0de786d2ae43629c2c40
Ryo Narita <mail.ryonarita@gmail.com>
2020-10-08 20:17:06 +0900

    Add deployments module tests

--- pkg/app/web/src/modules/deployments.test.ts
+++ pkg/app/web/src/modules/deployments.test.ts
@@ -1,4 +1,51 @@
-import { deploymentsSlice } from "./deployments";
+import { dummyDeployment } from "../__fixtures__/dummy-deployment";
+import { CommandModel, fetchCommand, CommandStatus } from "./commands";
+import {
+  deploymentsSlice,
+  isDeploymentRunning,
+  DeploymentStatus,
+  StageStatus,
+  isStageRunning,
+  fetchDeploymentById,
+  fetchDeployments,
+  fetchMoreDeployments,
+  cancelDeployment,
+} from "./deployments";
+
+const initialState = {
+  canceling: {},
+  entities: {},
+  hasMore: true,
+  ids: [],
+  isLoadingItems: false,
+  isLoadingMoreItems: false,
+  loading: {},
+};
+
+test("isDeploymentRunning", () => {
+  expect(isDeploymentRunning(undefined)).toBeFalsy();
+
+  expect(isDeploymentRunning(DeploymentStatus.DEPLOYMENT_PENDING)).toBeTruthy();
+  expect(isDeploymentRunning(DeploymentStatus.DEPLOYMENT_PLANNED)).toBeTruthy();
+  expect(isDeploymentRunning(DeploymentStatus.DEPLOYMENT_RUNNING)).toBeTruthy();
+  expect(
+    isDeploymentRunning(DeploymentStatus.DEPLOYMENT_ROLLING_BACK)
+  ).toBeTruthy();
+
+  expect(
+    isDeploymentRunning(DeploymentStatus.DEPLOYMENT_CANCELLED)
+  ).toBeFalsy();
+  expect(isDeploymentRunning(DeploymentStatus.DEPLOYMENT_FAILURE)).toBeFalsy();
+  expect(isDeploymentRunning(DeploymentStatus.DEPLOYMENT_SUCCESS)).toBeFalsy();
+});
+
+test("isStageRunning", () => {
+  expect(isStageRunning(StageStatus.STAGE_CANCELLED)).toBeFalsy();
+  expect(isStageRunning(StageStatus.STAGE_FAILURE)).toBeFalsy();
+  expect(isStageRunning(StageStatus.STAGE_SUCCESS)).toBeFalsy();
+  expect(isStageRunning(StageStatus.STAGE_NOT_STARTED_YET)).toBeTruthy();
+  expect(isStageRunning(StageStatus.STAGE_RUNNING)).toBeTruthy();
+});
 
 describe("deploymentsSlice reducer", () => {
   it("should handle initial state", () => {
@@ -6,16 +53,218 @@ describe("deploymentsSlice reducer", () => {
       deploymentsSlice.reducer(undefined, {
         type: "TEST_ACTION",
       })
-    ).toMatchInlineSnapshot(`
-      Object {
-        "canceling": Object {},
-        "entities": Object {},
-        "hasMore": true,
-        "ids": Array [],
-        "isLoadingItems": false,
-        "isLoadingMoreItems": false,
-        "loading": Object {},
-      }
-    `);
+    ).toEqual(initialState);
+  });
+
+  describe("fetchDeploymentById", () => {
+    it(`should handle ${fetchDeploymentById.pending.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(initialState, {
+          type: fetchDeploymentById.pending.type,
+          meta: {
+            arg: "deployment-1",
+          },
+        })
+      ).toEqual({
+        canceling: {},
+        entities: {},
+        hasMore: true,
+        ids: [],
+        isLoadingItems: false,
+        isLoadingMoreItems: false,
+        loading: {
+          "deployment-1": true,
+        },
+      });
+    });
+
+    it(`should handle ${fetchDeploymentById.rejected.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(
+          {
+            ...initialState,
+            loading: {
+              "deployment-1": true,
+            },
+          },
+          {
+            type: fetchDeploymentById.rejected.type,
+            meta: {
+              arg: "deployment-1",
+            },
+          }
+        )
+      ).toEqual({
+        ...initialState,
+        loading: {
+          "deployment-1": false,
+        },
+      });
+    });
+
+    it(`should handle ${fetchDeploymentById.fulfilled.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(
+          {
+            ...initialState,
+            loading: {
+              "deployment-1": false,
+            },
+          },
+          {
+            type: fetchDeploymentById.fulfilled.type,
+            meta: {
+              arg: "deployment-1",
+            },
+            payload: dummyDeployment,
+          }
+        )
+      ).toEqual({
+        ...initialState,
+        entities: { [dummyDeployment.id]: dummyDeployment },
+        ids: [dummyDeployment.id],
+        loading: {
+          "deployment-1": false,
+        },
+      });
+    });
+  });
+
+  describe("fetchDeployments", () => {
+    it(`should handle ${fetchDeployments.pending.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(initialState, {
+          type: fetchDeployments.pending.type,
+        })
+      ).toEqual({
+        ...initialState,
+        isLoadingItems: true,
+      });
+    });
+
+    it(`should handle ${fetchDeployments.rejected.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(
+          {
+            ...initialState,
+            isLoadingItems: true,
+          },
+          {
+            type: fetchDeployments.rejected.type,
+          }
+        )
+      ).toEqual({
+        ...initialState,
+        isLoadingItems: false,
+      });
+    });
+
+    it(`should handle ${fetchDeployments.fulfilled.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(
+          {
+            ...initialState,
+            isLoadingItems: true,
+          },
+          {
+            type: fetchDeployments.fulfilled.type,
+            payload: [dummyDeployment],
+          }
+        )
+      ).toEqual({
+        ...initialState,
+        entities: { [dummyDeployment.id]: dummyDeployment },
+        hasMore: false,
+        ids: [dummyDeployment.id],
+        isLoadingItems: false,
+      });
+    });
+  });
+
+  describe("fetchMoreDeployments", () => {
+    it(`should handle ${fetchMoreDeployments.pending.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(initialState, {
+          type: fetchMoreDeployments.pending.type,
+        })
+      ).toEqual({ ...initialState, isLoadingMoreItems: true });
+    });
+
+    it(`should handle ${fetchMoreDeployments.rejected.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(
+          { ...initialState, isLoadingMoreItems: true },
+          {
+            type: fetchMoreDeployments.rejected.type,
+          }
+        )
+      ).toEqual({ ...initialState, isLoadingMoreItems: false });
+    });
+
+    it(`should handle ${fetchMoreDeployments.fulfilled.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(
+          { ...initialState, isLoadingMoreItems: true },
+          {
+            type: fetchMoreDeployments.fulfilled.type,
+            payload: [dummyDeployment],
+          }
+        )
+      ).toEqual({
+        ...initialState,
+        isLoadingMoreItems: false,
+        hasMore: false,
+        ids: [dummyDeployment.id],
+        entities: { [dummyDeployment.id]: dummyDeployment },
+      });
+    });
+  });
+
+  describe("cancelDeployment", () => {
+    it(`should handle ${cancelDeployment.pending.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(initialState, {
+          type: cancelDeployment.pending.type,
+          meta: {
+            arg: {
+              deploymentId: "deployment-1",
+            },
+          },
+        })
+      ).toEqual({
+        ...initialState,
+        canceling: {
+          "deployment-1": true,
+        },
+      });
+    });
+  });
+
+  describe("fetchCommand", () => {
+    it(`should handle ${fetchCommand.fulfilled.type}`, () => {
+      expect(
+        deploymentsSlice.reducer(
+          {
+            ...initialState,
+            canceling: {
+              "deployment-1": true,
+            },
+          },
+          {
+            type: fetchCommand.fulfilled.type,
+            payload: {
+              deploymentId: "deployment-1",
+              type: CommandModel.Type.CANCEL_DEPLOYMENT,
+              status: CommandStatus.COMMAND_SUCCEEDED,
+            },
+          }
+        )
+      ).toEqual({
+        ...initialState,
+        canceling: {
+          "deployment-1": false,
+        },
+      });
+    });
   });
 });
