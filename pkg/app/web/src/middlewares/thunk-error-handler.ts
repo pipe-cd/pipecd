84880355dfaf8544c9fc632e7beed0b11c0c0f24
Ryo Narita <mail.ryonarita@gmail.com>
2020-10-15 16:28:41 +0900

    Add toast logic for showing request errors

---
+++ pkg/app/web/src/middlewares/thunk-error-handler.ts
@@ -0,0 +1,35 @@
+import { Middleware, AnyAction, MiddlewareAPI } from "redux";
+import { AppThunk } from "../modules";
+import { addToast } from "../modules/toasts";
+import { AppDispatch } from "../store";
+
+function isPlainAction(action: AppThunk | AnyAction): action is AnyAction {
+  return typeof action !== "function";
+}
+
+export const thunkErrorHandler: Middleware = ({
+  dispatch,
+}: MiddlewareAPI<AppDispatch>) => (next) => async (action) => {
+  let res;
+  try {
+    res = await next(action);
+  } catch (err) {
+    if (process.env.NODE_ENV === "development") {
+      console.error(err);
+    }
+
+    if (err && err.code && err.message) {
+      dispatch(addToast({ message: err.message, severity: "error" }));
+    } else {
+      throw err;
+    }
+  }
+
+  if (isPlainAction(action)) {
+    if (action.type.includes("rejected")) {
+      dispatch(addToast({ message: action.error.message, severity: "error" }));
+    }
+  }
+
+  return res;
+};
