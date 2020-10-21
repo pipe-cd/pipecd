73c3905ac0afd4fabbc2a6748ffe7bbbfe099357
Ryo Narita <mail.ryonarita@gmail.com>
2020-10-21 20:33:49 +0900

    Add copy button for copying of encrypted secret (#990)
    **What this PR does / why we need it**:
    
    ![Kapture 2020-10-21 at 18 34 48](https://user-images.githubusercontent.com/6136383/96701938-203c2200-13cc-11eb-8ed6-e3da9c00c974.gif)
    
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    Add a copy to clipboard button to the dialog for showing encrypted secret.
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/web/src/components/sealed-secret-dialog.test.tsx
@@ -0,0 +1,51 @@
+import { fireEvent } from "@testing-library/react";
+import React from "react";
+import { render, screen } from "../../test-utils";
+import { dummyApplication } from "../__fixtures__/dummy-application";
+import { SealedSecretDialog } from "./sealed-secret-dialog";
+
+test("render", () => {
+  render(
+    <SealedSecretDialog
+      applicationId={dummyApplication.id}
+      onClose={() => null}
+      open
+    />,
+    {
+      initialState: {
+        applications: {
+          entities: {
+            [dummyApplication.id]: dummyApplication,
+          },
+          ids: [dummyApplication.id],
+        },
+      },
+    }
+  );
+
+  expect(screen.getByText(dummyApplication.name)).toBeInTheDocument();
+});
+
+test("cancel", () => {
+  const onClose = jest.fn();
+  render(
+    <SealedSecretDialog
+      applicationId={dummyApplication.id}
+      onClose={onClose}
+      open
+    />,
+    {
+      initialState: {
+        applications: {
+          entities: {
+            [dummyApplication.id]: dummyApplication,
+          },
+          ids: [dummyApplication.id],
+        },
+      },
+    }
+  );
+
+  fireEvent.click(screen.getByRole("button", { name: "Cancel" }));
+  expect(onClose).toHaveBeenCalled();
+});
