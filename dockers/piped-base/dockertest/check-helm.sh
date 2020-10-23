2ed462761b9d50fc5789deccb2dcee0de5129db4
Ryo Nakao <ryo@nakao.dev>
2020-10-23 12:49:14 +0900

    Make sure to add user with "adduser" (#999)
    **What this PR does / why we need it**:
    I don't get what `docker_rules` is doing behind the scenes when building. But I can see it's possible to make the home directory whose owner is the user (rather than root).
    
    **Which issue(s) this PR fixes**:
    
    Part of https://github.com/pipe-cd/pipe/issues/987
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```

--- dockers/piped-base/dockertest/check-helm.sh
+++ dockers/piped-base/dockertest/check-helm.sh
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 declare -A pathcases
-#pathcases["helm-2.16.7"]="/usr/local/piped/helm-2.16.7"
-pathcases["helm"]="/usr/local/piped/helm"
+#pathcases["helm-2.16.7"]="/home/pipecd/tools/helm-2.16.7"
+pathcases["helm"]="/home/pipecd/tools/helm"
 
 for h in "${!pathcases[@]}"
 do
