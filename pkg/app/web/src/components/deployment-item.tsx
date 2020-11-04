aa083876c7904713706b808c78036a30eb3c3dd6
Ryo Narita <mail.ryonarita@gmail.com>
2020-11-04 18:11:03 +0900

    Redesign the deployment item component
    * Update design
    * Show application kind

--- pkg/app/web/src/components/deployment-item.tsx
+++ pkg/app/web/src/components/deployment-item.tsx
@@ -1,4 +1,4 @@
-import { ListItem, makeStyles, Typography } from "@material-ui/core";
+import { Box, ListItem, makeStyles, Typography } from "@material-ui/core";
 import dayjs from "dayjs";
 import React, { FC, memo } from "react";
 import { useSelector } from "react-redux";
@@ -12,6 +12,8 @@ import {
 } from "../modules/deployments";
 import { Environment, selectById } from "../modules/environments";
 import { StatusIcon } from "./deployment-status-icon";
+import { APPLICATION_KIND_TEXT } from "../constants/application-kind";
+import { ellipsis } from "../styles/text";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,34 +24,26 @@ const useStyles = makeStyles((theme) => ({
     height: 72,
     backgroundColor: theme.palette.background.paper,
   },
-  name: {
+  info: {
     marginLeft: theme.spacing(1),
-  },
-  env: {
-    marginLeft: theme.spacing(1),
-    color: theme.palette.text.secondary,
   },
   statusText: {
     marginLeft: theme.spacing(1),
-  },
-  commitHash: {
-    marginLeft: theme.spacing(1),
-  },
-  head: {
-    display: "flex",
-    alignItems: "center",
+    lineHeight: "1.5rem",
+    // Fix width to prevent misalignment of application name.
+    width: "100px",
   },
   description: {
+    ...ellipsis,
     color: theme.palette.text.hint,
-  },
-  main: {
-    flex: 1,
   },
 }));
 
 interface Props {
   id: string;
 }
+
+const NO_DESCRIPTION = "No description.";
 
 export const DeploymentItem: FC<Props> = memo(function DeploymentItem({ id }) {
   const classes = useStyles();
@@ -76,21 +70,46 @@ export const DeploymentItem: FC<Props> = memo(function DeploymentItem({ id }) {
       component={RouterLink}
       to={`${PAGE_PATH_DEPLOYMENTS}/${deployment.id}`}
     >
-      <div className={classes.main}>
-        <div className={classes.head}>
-          <StatusIcon status={deployment.status} />
-          <Typography variant="body1" className={classes.statusText}>
-            {DEPLOYMENT_STATE_TEXT[deployment.status]}
-          </Typography>
-          <Typography variant="h6" className={classes.name}>
+      <Box display="flex" alignItems="center">
+        <StatusIcon status={deployment.status} />
+        <Typography
+          variant="subtitle2"
+          className={classes.statusText}
+          component="span"
+        >
+          {DEPLOYMENT_STATE_TEXT[deployment.status]}
+        </Typography>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        flex={1}
+        pl={2}
+        overflow="hidden"
+      >
+        <Box display="flex" alignItems="baseline">
+          <Typography variant="h6" component="span">
             {deployment.applicationName}
           </Typography>
-          <Typography className={classes.env}>{env.name}</Typography>
-        </div>
+          <Typography
+            variant="subtitle2"
+            className={classes.info}
+            component="span"
+          >
+            {env.name}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            className={classes.info}
+          >
+            {APPLICATION_KIND_TEXT[deployment.kind]}
+          </Typography>
+        </Box>
         <Typography variant="body1" className={classes.description}>
-          {deployment.summary}
+          {deployment.summary || NO_DESCRIPTION}
         </Typography>
-      </div>
+      </Box>
       <div>{dayjs(deployment.createdAt * 1000).fromNow()}</div>
     </ListItem>
   );
---
+++ pkg/app/web/src/styles/text.ts
@@ -0,0 +1,7 @@
+import { CSSProperties } from "@material-ui/core/styles/withStyles";
+
+export const ellipsis: CSSProperties = {
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+};
