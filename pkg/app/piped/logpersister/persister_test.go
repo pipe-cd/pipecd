03e7dc61b98808ffaabeef0109d107545bc936f5
Le Van Nghia <nghialv2607@gmail.com>
2020-11-04 14:31:23 +0900

    Fix a bug that piped was handling only one stage LogPersister at a time (#1060)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/piped/logpersister/persister_test.go
+++ pkg/app/piped/logpersister/persister_test.go
@@ -13,3 +13,77 @@
 // limitations under the License.
 
 package logpersister
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+
+	"github.com/pipe-cd/pipe/pkg/app/api/service/pipedservice"
+)
+
+type fakeAPIClient struct {
+	reportStageLogsCount                   atomic.Uint32
+	reportStageLogsFromLastCheckpointCount atomic.Uint32
+}
+
+func (c *fakeAPIClient) ReportStageLogs(ctx context.Context, in *pipedservice.ReportStageLogsRequest, opts ...grpc.CallOption) (*pipedservice.ReportStageLogsResponse, error) {
+	c.reportStageLogsCount.Inc()
+	return &pipedservice.ReportStageLogsResponse{}, nil
+}
+
+func (c *fakeAPIClient) ReportStageLogsFromLastCheckpoint(ctx context.Context, in *pipedservice.ReportStageLogsFromLastCheckpointRequest, opts ...grpc.CallOption) (*pipedservice.ReportStageLogsFromLastCheckpointResponse, error) {
+	c.reportStageLogsFromLastCheckpointCount.Inc()
+	return &pipedservice.ReportStageLogsFromLastCheckpointResponse{}, nil
+}
+
+func (c *fakeAPIClient) NumberOfReportStageLogs() int {
+	return int(c.reportStageLogsCount.Load())
+}
+
+func (c *fakeAPIClient) NumberOfReportStageLogsFromLastCheckpoint() int {
+	return int(c.reportStageLogsFromLastCheckpointCount.Load())
+}
+
+func TestPersister(t *testing.T) {
+	apiClient := &fakeAPIClient{}
+	p := NewPersister(apiClient, zap.NewNop())
+	p.stalePeriod = 0
+
+	flushes, deletes := p.flush(context.TODO())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogs())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogsFromLastCheckpoint())
+	assert.Equal(t, 0, flushes)
+	assert.Equal(t, 0, deletes)
+
+	num := p.flushAll(context.TODO())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogs())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogsFromLastCheckpoint())
+	assert.Equal(t, 0, num)
+
+	sp1 := p.StageLogPersister("deployment-1", "stage-1")
+	p.StageLogPersister("deployment-2", "stage-2")
+
+	num = p.flushAll(context.TODO())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogs())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogsFromLastCheckpoint())
+	assert.Equal(t, 2, num)
+
+	sp1.Complete(0)
+
+	flushes, deletes = p.flush(context.TODO())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogs())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogsFromLastCheckpoint())
+	assert.Equal(t, 1, flushes)
+	assert.Equal(t, 1, deletes)
+
+	num = p.flushAll(context.TODO())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogs())
+	require.Equal(t, 0, apiClient.NumberOfReportStageLogsFromLastCheckpoint())
+	assert.Equal(t, 1, num)
+}
