bbd61d938268bed8690021de5bc4e5567b097a0e
Ryo Narita <mail.ryonarita@gmail.com>
2020-11-11 15:23:43 +0900

    Update deployment detail test

--- pkg/app/web/src/mocks/handlers.ts
+++ pkg/app/web/src/mocks/handlers.ts
@@ -1,8 +1,10 @@
 import { meHandlers } from "./services/me";
 import { commandHandlers } from "./services/command";
 import { applicationHandlers } from "./services/application";
+import { deploymentHandlers } from "./services/deployment";
 export const handlers = [
   ...meHandlers,
   ...commandHandlers,
   ...applicationHandlers,
+  ...deploymentHandlers,
 ];
