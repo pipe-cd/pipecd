9c67e2bc77d65f873151d836e15f7ef72f664b37
Ryo Narita <mail.ryonarita@gmail.com>
2020-11-12 18:56:15 +0900

    Remove an old error toast

--- pkg/app/web/src/constants/toast-text.ts
+++ pkg/app/web/src/constants/toast-text.ts
@@ -1,6 +1,5 @@
 export const UPDATE_STATIC_ADMIN_INFO_SUCCESS = `Successfully updated Static Admin configurations.`;
 export const UPDATE_STATIC_ADMIN_INFO_FAILED = `Failed to update Static Admin configurations.`;
 export const UPDATE_RBAC_SUCCESS = `Successfully updated RBAC configurations.`;
-export const UPDATE_RBAC_FAILED = `Failed to update RBAC configurations.`;
 export const UPDATE_SSO_SUCCESS = `Successfully updated SSO configurations.`;
 export const UPDATE_SSO_FAILED = `Failed to update SSO configurations.`;
