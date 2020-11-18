0910acecfc4db78d5c84b62c694b2e6fbf0b481a
Le Van Nghia <nghialv2607@gmail.com>
2020-11-18 18:08:54 +0900

    Refactor terraform executor (#1116)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/piped/executor/terraform/deploy.go
@@ -0,0 +1,183 @@
+// Copyright 2020 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package terraform
+
+import (
+	"context"
+
+	provider "github.com/pipe-cd/pipe/pkg/app/piped/cloudprovider/terraform"
+	"github.com/pipe-cd/pipe/pkg/app/piped/executor"
+	"github.com/pipe-cd/pipe/pkg/config"
+	"github.com/pipe-cd/pipe/pkg/model"
+)
+
+type deployExecutor struct {
+	executor.Input
+
+	repoDir       string
+	appDir        string
+	vars          []string
+	terraformPath string
+	deployCfg     *config.TerraformDeploymentSpec
+}
+
+func (e *deployExecutor) Execute(sig executor.StopSignal) model.StageStatus {
+	_, cloudProviderCfg, found := findCloudProvider(&e.Input)
+	if !found {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	ctx := sig.Context()
+	ds, err := e.TargetDSP.Get(ctx, e.LogPersister)
+	if err != nil {
+		e.LogPersister.Errorf("Failed to prepare target deploy source data (%v)", err)
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	e.deployCfg = ds.DeploymentConfig.TerraformDeploymentSpec
+	if e.deployCfg == nil {
+		e.LogPersister.Error("Malformed deployment configuration: missing TerraformDeploymentSpec")
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	e.repoDir = ds.RepoDir
+	e.appDir = ds.AppDir
+
+	e.vars = make([]string, 0, len(cloudProviderCfg.Vars)+len(e.deployCfg.Input.Vars))
+	e.vars = append(e.vars, cloudProviderCfg.Vars...)
+	e.vars = append(e.vars, e.deployCfg.Input.Vars...)
+
+	var (
+		originalStatus = e.Stage.Status
+		status         model.StageStatus
+	)
+
+	var ok bool
+	e.terraformPath, ok = findTerraform(ctx, e.deployCfg.Input.TerraformVersion, e.LogPersister)
+	if !ok {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	switch model.Stage(e.Stage.Name) {
+	case model.StageTerraformSync:
+		status = e.ensureSync(ctx)
+
+	case model.StageTerraformPlan:
+		status = e.ensurePlan(ctx)
+
+	case model.StageTerraformApply:
+		status = e.ensureApply(ctx)
+
+	default:
+		e.LogPersister.Errorf("Unsupported stage %s for cloudrun application", e.Stage.Name)
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	return executor.DetermineStageStatus(sig.Signal(), originalStatus, status)
+}
+
+func (e *deployExecutor) ensureSync(ctx context.Context) model.StageStatus {
+	cmd := provider.NewTerraform(e.terraformPath, e.appDir, e.vars, e.deployCfg.Input.VarFiles)
+
+	if ok := showUsingVersion(ctx, cmd, e.LogPersister); !ok {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if err := cmd.Init(ctx, e.LogPersister); err != nil {
+		e.LogPersister.Errorf("Failed to init (%v)", err)
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if ok := selectWorkspace(ctx, cmd, e.deployCfg.Input.Workspace, e.LogPersister); !ok {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	planResult, err := cmd.Plan(ctx, e.LogPersister)
+	if err != nil {
+		e.LogPersister.Errorf("Failed to plan (%v)", err)
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if planResult.NoChanges() {
+		e.LogPersister.Info("No changes to apply")
+		return model.StageStatus_STAGE_SUCCESS
+	}
+
+	e.LogPersister.Infof("Detected %d add, %d change, %d destroy. Those changes will be applied automatically.", planResult.Adds, planResult.Changes, planResult.Destroys)
+
+	if err := cmd.Apply(ctx, e.LogPersister); err != nil {
+		e.LogPersister.Errorf("Failed to apply changes (%v)", err)
+		return model.StageStatus_STAGE_SUCCESS
+	}
+
+	e.LogPersister.Success("Successfully applied changes")
+	return model.StageStatus_STAGE_SUCCESS
+}
+
+func (e *deployExecutor) ensurePlan(ctx context.Context) model.StageStatus {
+	cmd := provider.NewTerraform(e.terraformPath, e.appDir, e.vars, e.deployCfg.Input.VarFiles)
+
+	if ok := showUsingVersion(ctx, cmd, e.LogPersister); !ok {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if err := cmd.Init(ctx, e.LogPersister); err != nil {
+		e.LogPersister.Errorf("Failed to init (%v)", err)
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if ok := selectWorkspace(ctx, cmd, e.deployCfg.Input.Workspace, e.LogPersister); !ok {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	planResult, err := cmd.Plan(ctx, e.LogPersister)
+	if err != nil {
+		e.LogPersister.Errorf("Failed to plan (%v)", err)
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if planResult.NoChanges() {
+		e.LogPersister.Success("No changes to apply")
+		return model.StageStatus_STAGE_SUCCESS
+	}
+
+	e.LogPersister.Successf("Detected %d add, %d change, %d destroy.", planResult.Adds, planResult.Changes, planResult.Destroys)
+	return model.StageStatus_STAGE_SUCCESS
+}
+
+func (e *deployExecutor) ensureApply(ctx context.Context) model.StageStatus {
+	cmd := provider.NewTerraform(e.terraformPath, e.appDir, e.vars, e.deployCfg.Input.VarFiles)
+
+	if ok := showUsingVersion(ctx, cmd, e.LogPersister); !ok {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if err := cmd.Init(ctx, e.LogPersister); err != nil {
+		e.LogPersister.Errorf("Failed to init (%v)", err)
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if ok := selectWorkspace(ctx, cmd, e.deployCfg.Input.Workspace, e.LogPersister); !ok {
+		return model.StageStatus_STAGE_FAILURE
+	}
+
+	if err := cmd.Apply(ctx, e.LogPersister); err != nil {
+		e.LogPersister.Errorf("Failed to apply changes (%v)", err)
+		return model.StageStatus_STAGE_SUCCESS
+	}
+
+	e.LogPersister.Success("Successfully applied changes")
+	return model.StageStatus_STAGE_SUCCESS
+}
--- pkg/app/piped/executor/terraform/terraform.go
+++ pkg/app/piped/executor/terraform/terraform.go
@@ -18,24 +18,11 @@ import (
 	"context"
 
 	provider "github.com/pipe-cd/pipe/pkg/app/piped/cloudprovider/terraform"
-	"github.com/pipe-cd/pipe/pkg/app/piped/deploysource"
 	"github.com/pipe-cd/pipe/pkg/app/piped/executor"
 	"github.com/pipe-cd/pipe/pkg/app/piped/toolregistry"
 	"github.com/pipe-cd/pipe/pkg/config"
 	"github.com/pipe-cd/pipe/pkg/model"
 )
-
-type Executor struct {
-	executor.Input
-
-	cloudProviderConfig *config.CloudProviderTerraformConfig
-
-	repoDir       string
-	appDir        string
-	config        *config.TerraformDeploymentSpec
-	terraformPath string
-	vars          []string
-}
 
 type registerer interface {
 	Register(stage model.Stage, f executor.Factory) error
@@ -44,7 +31,7 @@ type registerer interface {
 
 func Register(r registerer) {
 	f := func(in executor.Input) executor.Executor {
-		return &Executor{
+		return &deployExecutor{
 			Input: in,
 		}
 	}
@@ -52,116 +39,61 @@ func Register(r registerer) {
 	r.Register(model.StageTerraformPlan, f)
 	r.Register(model.StageTerraformApply, f)
 
-	r.RegisterRollback(model.ApplicationKind_TERRAFORM, f)
+	r.RegisterRollback(model.ApplicationKind_TERRAFORM, func(in executor.Input) executor.Executor {
+		return &rollbackExecutor{
+			Input: in,
+		}
+	})
 }
 
-func (e *Executor) Execute(sig executor.StopSignal) model.StageStatus {
-	cloudProviderName := e.Application.CloudProvider
-	if cloudProviderName == "" {
-		e.LogPersister.Error("This application configuration was missing CloudProvider name")
-		return model.StageStatus_STAGE_FAILURE
-	}
-
-	cpConfig, ok := e.PipedConfig.FindCloudProvider(cloudProviderName, model.CloudProviderTerraform)
-	if !ok {
-		e.LogPersister.Errorf("The specified cloud provider %q was not found in piped configuration", cloudProviderName)
-		return model.StageStatus_STAGE_FAILURE
-	}
-	e.cloudProviderConfig = cpConfig.TerraformConfig
-
-	var ds *deploysource.DeploySource
-	var err error
-	ctx := sig.Context()
-
-	if model.Stage(e.Stage.Name) == model.StageRollback {
-		ds, err = e.RunningDSP.Get(ctx, e.LogPersister)
-		if err != nil {
-			e.LogPersister.Errorf("Failed to prepare running deploy source data (%v)", err)
-			return model.StageStatus_STAGE_FAILURE
-		}
-	} else {
-		ds, err = e.TargetDSP.Get(ctx, e.LogPersister)
-		if err != nil {
-			e.LogPersister.Errorf("Failed to prepare target deploy source data (%v)", err)
-			return model.StageStatus_STAGE_FAILURE
-		}
-	}
-
-	e.repoDir = ds.RepoDir
-	e.appDir = ds.AppDir
-	e.config = ds.DeploymentConfig.TerraformDeploymentSpec
-	if e.config == nil {
-		e.LogPersister.Error("Malformed deployment configuration: missing TerraformDeploymentSpec")
-		return model.StageStatus_STAGE_FAILURE
-	}
-
-	e.vars = make([]string, 0, len(e.cloudProviderConfig.Vars)+len(e.config.Input.Vars))
-	e.vars = append(e.vars, e.cloudProviderConfig.Vars...)
-	e.vars = append(e.vars, e.config.Input.Vars...)
-
-	var (
-		originalStatus = e.Stage.Status
-		status         model.StageStatus
-	)
-
-	execPath, ok := e.findTerraform(ctx, e.config.Input.TerraformVersion)
-	if !ok {
-		return model.StageStatus_STAGE_FAILURE
-	}
-	e.terraformPath = execPath
-
-	switch model.Stage(e.Stage.Name) {
-	case model.StageTerraformSync:
-		status = e.ensureSync(ctx)
-
-	case model.StageTerraformPlan:
-		status = e.ensurePlan(ctx)
-
-	case model.StageTerraformApply:
-		status = e.ensureApply(ctx)
-
-	case model.StageRollback:
-		status = e.ensureRollback(ctx)
-
-	default:
-		e.LogPersister.Errorf("Unsupported stage %s for cloudrun application", e.Stage.Name)
-		return model.StageStatus_STAGE_FAILURE
-	}
-
-	return executor.DetermineStageStatus(sig.Signal(), originalStatus, status)
-}
-
-func (e *Executor) showUsingVersion(ctx context.Context, cmd *provider.Terraform) bool {
+func showUsingVersion(ctx context.Context, cmd *provider.Terraform, lp executor.LogPersister) bool {
 	version, err := cmd.Version(ctx)
 	if err != nil {
-		e.LogPersister.Errorf("Failed to check terraform version (%v)", err)
+		lp.Errorf("Failed to check terraform version (%v)", err)
 		return false
 	}
-	e.LogPersister.Infof("Using terraform version %q to execute the terraform commands", version)
+	lp.Infof("Using terraform version %q to execute the terraform commands", version)
 	return true
 }
 
-func (e *Executor) selectWorkspace(ctx context.Context, cmd *provider.Terraform) bool {
-	workspace := e.config.Input.Workspace
+func selectWorkspace(ctx context.Context, cmd *provider.Terraform, workspace string, lp executor.LogPersister) bool {
 	if workspace == "" {
 		return true
 	}
 	if err := cmd.SelectWorkspace(ctx, workspace); err != nil {
-		e.LogPersister.Errorf("Failed to select workspace %q (%v). You might need to create the workspace before using by command %q", workspace, err, "terraform workspace new "+workspace)
+		lp.Errorf("Failed to select workspace %q (%v). You might need to create the workspace before using by command %q", workspace, err, "terraform workspace new "+workspace)
 		return false
 	}
-	e.LogPersister.Infof("Selected workspace %q", workspace)
+	lp.Infof("Selected workspace %q", workspace)
 	return true
 }
 
-func (e *Executor) findTerraform(ctx context.Context, version string) (string, bool) {
+func findTerraform(ctx context.Context, version string, lp executor.LogPersister) (string, bool) {
 	path, installed, err := toolregistry.DefaultRegistry().Terraform(ctx, version)
 	if err != nil {
-		e.LogPersister.Errorf("Unable to find required terraform %q (%v)", version, err)
+		lp.Errorf("Unable to find required terraform %q (%v)", version, err)
 		return "", false
 	}
 	if installed {
-		e.LogPersister.Infof("Terraform %q has just been installed to %q because of no pre-installed binary for that version", version, path)
+		lp.Infof("Terraform %q has just been installed to %q because of no pre-installed binary for that version", version, path)
 	}
 	return path, true
+}
+
+func findCloudProvider(in *executor.Input) (name string, cfg *config.CloudProviderTerraformConfig, found bool) {
+	name = in.Application.CloudProvider
+	if name == "" {
+		in.LogPersister.Error("Missing the CloudProvider name in the application configuration")
+		return
+	}
+
+	cp, ok := in.PipedConfig.FindCloudProvider(name, model.CloudProviderTerraform)
+	if !ok {
+		in.LogPersister.Errorf("The specified cloud provider %q was not found in piped configuration", name)
+		return
+	}
+
+	cfg = cp.TerraformConfig
+	found = true
+	return
 }
