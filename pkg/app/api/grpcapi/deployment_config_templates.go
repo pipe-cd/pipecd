58e05be5f8f6c837c66064a879314c8698bc588a
nghialv <nghialv2607@gmail.com>
2020-11-26 22:51:54 +0900

    Rename package pkg/app/api/api to grpcapi

--- pkg/app/api/grpcapi/deployment_config_templates.go
+++ pkg/app/api/grpcapi/deployment_config_templates.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package api
+package grpcapi
 
 import (
 	"github.com/pipe-cd/pipe/pkg/app/api/service/webservice"
