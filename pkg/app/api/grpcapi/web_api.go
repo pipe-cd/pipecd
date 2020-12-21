21966ba54fbcd85282a69e18df5c04c2160a00c6
sanposhiho <handbomusic@gmail.com>
2020-12-21 18:32:14 +0900

    Add todo comment about cache

--- pkg/app/api/grpcapi/web_api.go
+++ pkg/app/api/grpcapi/web_api.go
@@ -1319,6 +1319,7 @@ func (a *WebAPI) GetInsightData(ctx context.Context, req *webservice.GetInsightD
 	count := int(req.DataPointCount)
 	from := time.Unix(req.RangeFrom, 0)
 
+	// TODO: caching the insight data inside the cache service
 	chunks, err := a.insightstore.LoadChunks(ctx, claims.Role.ProjectId, req.ApplicationId, req.MetricsKind, req.Step, from, count)
 	if err != nil {
 		a.logger.Error("failed to load chunks from insightstore", zap.Error(err))
