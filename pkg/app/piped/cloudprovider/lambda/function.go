cf52a91d57a534ba9efa681307d12ec84e958a66
khanhtc1202 <khanhtc1202@gmail.com>
2021-01-05 23:37:43 +0900

    Add FunctionManifest

---
+++ pkg/app/piped/cloudprovider/lambda/function.go
@@ -0,0 +1,84 @@
+// Copyright 2020 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lambda
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/yaml"
+)
+
+type FunctionManifest struct {
+	Name string
+	u    *unstructured.Unstructured
+}
+
+func loadFunctionManifest(path string) (FunctionManifest, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return FunctionManifest{}, err
+	}
+	return parseFunctionManifest(data)
+}
+
+func parseFunctionManifest(data []byte) (FunctionManifest, error) {
+	var obj unstructured.Unstructured
+	if err := yaml.Unmarshal(data, &obj); err != nil {
+		return FunctionManifest{}, err
+	}
+
+	return FunctionManifest{
+		Name: obj.GetName(),
+		u:    &obj,
+	}, nil
+}
+
+func DecideRevisionName(fm FunctionManifest, commit string) (string, error) {
+	tag, err := FindImageTag(fm)
+	if err != nil {
+		return "", err
+	}
+	tag = strings.ReplaceAll(tag, ".", "")
+
+	if len(commit) > 7 {
+		commit = commit[:7]
+	}
+	return fmt.Sprintf("%s-%s-%s", fm.Name, tag, commit), nil
+}
+
+func FindImageTag(fm FunctionManifest) (string, error) {
+	imageURI, ok, err := unstructured.NestedString(fm.u.Object, "spec", "template", "spec", "image")
+	if err != nil {
+		return "", err
+	}
+	if !ok || imageURI == "" {
+		return "", fmt.Errorf("spec.template.spec.image is missing")
+	}
+	_, tag := parseContainerImage(imageURI)
+	return tag, nil
+}
+
+func parseContainerImage(image string) (name, tag string) {
+	parts := strings.Split(image, ":")
+	if len(parts) == 2 {
+		tag = parts[1]
+	}
+	paths := strings.Split(parts[0], "/")
+	name = paths[len(paths)-1]
+	return
+}
