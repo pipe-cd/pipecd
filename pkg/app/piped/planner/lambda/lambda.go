3f11c309c98cb4b6ca649cba373904f413bd7388
khanhtc1202 <khanhtc1202@gmail.com>
2021-01-08 14:29:34 +0900

    QuickSync strategy planner

--- pkg/app/piped/planner/lambda/lambda.go
+++ pkg/app/piped/planner/lambda/lambda.go
@@ -16,11 +16,18 @@ package lambda
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"time"
 
+	"go.uber.org/zap"
+
+	provider "github.com/pipe-cd/pipe/pkg/app/piped/cloudprovider/lambda"
 	"github.com/pipe-cd/pipe/pkg/app/piped/planner"
 	"github.com/pipe-cd/pipe/pkg/model"
 )
 
+// Planner plans the deployment pipeline for Lambda application.
 type Planner struct {
 }
 
@@ -33,6 +40,64 @@ func Register(r registerer) {
 	r.Register(model.ApplicationKind_LAMBDA, &Planner{})
 }
 
+// Plan decides which pipeline should be used for the given input.
 func (p *Planner) Plan(ctx context.Context, in planner.Input) (out planner.Output, err error) {
+	ds, err := in.TargetDSP.Get(ctx, ioutil.Discard)
+	if err != nil {
+		err = fmt.Errorf("error while preparing deploy source data (%v)", err)
+		return
+	}
+
+	cfg := ds.DeploymentConfig.LambdaDeploymentSpec
+	if cfg == nil {
+		err = fmt.Errorf("missing LambdaDeploymentSpec in deployment configuration")
+		return
+	}
+
+	// Determine application version from the manifest
+	if version, err := determineVersion(ds.AppDir, cfg.Input.FunctionManifestFile); err == nil {
+		out.Version = version
+	} else {
+		out.Version = "unknown"
+		in.Logger.Warn("unable to determine target version", zap.Error(err))
+	}
+
+	// If the deployment was triggered by forcing via web UI,
+	// we rely on the user's decision.
+	switch in.Deployment.Trigger.SyncStrategy {
+	case model.SyncStrategy_QUICK_SYNC:
+		out.Stages = buildQuickSyncPipeline(cfg.Input.AutoRollback, time.Now())
+		out.Summary = fmt.Sprintf("Quick sync to deploy image %s and configure all traffic to it (forced via web)", out.Version)
+		return
+	case model.SyncStrategy_PIPELINE:
+		err = fmt.Errorf("Pipeline sync for lambda application is not yet implemented")
+		return
+	}
+
+	// If this is the first time to deploy this application or it was unable to retrieve last successful commit,
+	// we perform the quick sync strategy.
+	if in.MostRecentSuccessfulCommitHash == "" {
+		out.Stages = buildQuickSyncPipeline(cfg.Input.AutoRollback, time.Now())
+		out.Summary = fmt.Sprintf("Quick sync to deploy image %s and configure all traffic to it (it seems this is the first deployment)", out.Version)
+		return
+	}
+
+	// When no pipeline was configured, perform the quick sync.
+	if cfg.Pipeline == nil || len(cfg.Pipeline.Stages) == 0 {
+		out.Stages = buildQuickSyncPipeline(cfg.Input.AutoRollback, time.Now())
+		out.Summary = fmt.Sprintf("Quick sync to deploy image %s and configure all traffic to it (pipeline was not configured)", out.Version)
+		return
+	}
+
+	err = fmt.Errorf("Currently only QUICK_SYNC strategy deployement is supported")
 	return
+}
+
+func determineVersion(appDir, functionManifestFile string) (string, error) {
+	fm, err := provider.LoadFunctionManifest(appDir, functionManifestFile)
+	if err != nil {
+		return "", err
+	}
+
+	return provider.FindImageTag(fm)
 }
