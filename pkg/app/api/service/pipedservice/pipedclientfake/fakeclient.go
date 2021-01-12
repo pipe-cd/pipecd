7c142845df9ab815800143d90cd4a4079a598eba
nghialv <nghialv2607@gmail.com>
2021-01-12 13:44:06 +0900

    Fix lints

--- pkg/app/api/service/pipedservice/pipedclientfake/fakeclient.go
+++ pkg/app/api/service/pipedservice/pipedclientfake/fakeclient.go
@@ -159,7 +159,7 @@ func (c *fakeClient) ReportApplicationSyncState(ctx context.Context, req *pipeds
 }
 
 // ReportApplicationDeployingStatus is used to report whether the specified application is deploying or not.
-func (c *fakeClient) ReportApplicationDeployingStatus(ctx context.Context, req *pipedservice.ReportApplicationDeployingStatusRequest, opts ...grpc.CallOption) (*pipedservice.ReportApplicationDeployingStatusResponse, error) {
+func (c *fakeClient) ReportApplicationDeployingStatus(_ context.Context, req *pipedservice.ReportApplicationDeployingStatusRequest, _ ...grpc.CallOption) (*pipedservice.ReportApplicationDeployingStatusResponse, error) {
 	c.logger.Info("fake client received ReportApplicationDeployingStatus rpc", zap.Any("request", req))
 	c.mu.RLock()
 	defer c.mu.RUnlock()
