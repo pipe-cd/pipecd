a988ce646170d9179a040b3fb0d550e9c9d9d83a
Ryo Narita <mail.ryonarita@gmail.com>
2021-01-27 18:45:18 +0900

    Add web tests

--- pkg/app/web/src/__fixtures__/dummy-environment.ts
+++ pkg/app/web/src/__fixtures__/dummy-environment.ts
@@ -1,5 +1,16 @@
 import { Environment } from "../modules/environments";
 
+export const createEnvFromObject = (o: Environment.AsObject): Environment => {
+  const env = new Environment();
+  env.setCreatedAt(o.createdAt);
+  env.setDesc(o.desc);
+  env.setName(o.name);
+  env.setProjectId(o.projectId);
+  env.setUpdatedAt(o.updatedAt);
+  env.setId(o.id);
+  return env;
+};
+
 export const dummyEnv: Environment.AsObject = {
   createdAt: 0,
   desc: "",
--- pkg/app/web/src/modules/environments.test.ts
+++ pkg/app/web/src/modules/environments.test.ts
@@ -1,5 +1,31 @@
-import { dummyEnv } from "../__fixtures__/dummy-environment";
-import { environmentsSlice, fetchEnvironments } from "./environments";
+import {
+  ListEnvironmentsResponse,
+  AddEnvironmentResponse,
+} from "pipe/pkg/app/web/api_client/service_pb";
+import { createStore } from "../../test-utils";
+import { createHandler } from "../mocks/create-handler";
+import { server } from "../mocks/server";
+import {
+  createEnvFromObject,
+  dummyEnv,
+} from "../__fixtures__/dummy-environment";
+import {
+  environmentsSlice,
+  fetchEnvironments,
+  addEnvironment,
+} from "./environments";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
 
 describe("environmentsSlice reducer", () => {
   it("should return the initial state", () => {
@@ -25,5 +51,50 @@ describe("environmentsSlice reducer", () => {
         ids: [dummyEnv.id],
       });
     });
+  });
+});
+
+describe("async actions", () => {
+  test("fetchEnvironments", async () => {
+    const store = createStore();
+
+    server.use(
+      createHandler<ListEnvironmentsResponse>("/ListEnvironments", () => {
+        const res = new ListEnvironmentsResponse();
+        res.setEnvironmentsList([createEnvFromObject(dummyEnv)]);
+        return res;
+      })
+    );
+
+    await store.dispatch(fetchEnvironments());
+    expect(store.getActions()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ type: fetchEnvironments.pending.type }),
+        expect.objectContaining({
+          type: fetchEnvironments.fulfilled.type,
+          payload: [dummyEnv],
+        }),
+      ])
+    );
+  });
+
+  test("addEnvironment", async () => {
+    const store = createStore();
+
+    server.use(
+      createHandler<AddEnvironmentResponse>("/AddEnvironment", () => {
+        return new AddEnvironmentResponse();
+      })
+    );
+
+    await store.dispatch(addEnvironment({ name: "env", desc: "description" }));
+    expect(store.getActions()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ type: addEnvironment.pending.type }),
+        expect.objectContaining({
+          type: addEnvironment.fulfilled.type,
+        }),
+      ])
+    );
   });
 });
