52d17c0a3da19e0ca670a654b8c0f8f851dec162
Ryo Narita <mail.ryonarita@gmail.com>
2021-01-29 15:52:58 +0900

    Refactor generated api key dialog
    * use text component that have copy button

---
+++ pkg/app/web/src/components/text-with-copy-button/text-with-copy-button.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useCallback } from "react";
+import { Box, IconButton, makeStyles } from "@material-ui/core";
+import CopyIcon from "@material-ui/icons/FileCopyOutlined";
+import copy from "copy-to-clipboard";
+
+const useStyles = makeStyles((theme) => ({
+  root: { backgroundColor: theme.palette.background.paper },
+  input: { border: "none", fontSize: 14, flex: 1, textOverflow: "ellipsis" },
+}));
+
+interface Props {
+  label: string;
+  value: string;
+  onCopy: () => void;
+}
+
+export const TextWithCopyButton: FC<Props> = ({ label, value, onCopy }) => {
+  const classes = useStyles();
+  const handleCopy = useCallback(() => {
+    copy(value);
+    onCopy();
+  }, [value, onCopy]);
+  return (
+    <Box
+      display="flex"
+      p={1}
+      border={1}
+      borderColor="divider"
+      className={classes.root}
+    >
+      <input readOnly value={value} className={classes.input} />
+      <IconButton
+        size="small"
+        style={{ marginLeft: 8 }}
+        aria-label={label}
+        onClick={handleCopy}
+      >
+        <CopyIcon style={{ fontSize: 20 }} />
+      </IconButton>
+    </Box>
+  );
+};
