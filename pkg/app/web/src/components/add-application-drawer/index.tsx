b9fd846dee882712dc3f5b5d8918ebfb69339c15
Ryo Narita <mail.ryonarita@gmail.com>
2021-02-05 11:11:36 +0900

    Add confirm dialog for adding application (#1550)
    **What this PR does / why we need it**:
    
    ![image](https://user-images.githubusercontent.com/6136383/106889122-eb5d9c00-672a-11eb-96c3-d6d001e7a378.png)
    
    
    **Which issue(s) this PR fixes**:
    
    Fixes #168 
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    Show confirm dialog when quit adding application if the user already filled some fields
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/web/src/components/add-application-drawer/index.tsx
+++ pkg/app/web/src/components/add-application-drawer/index.tsx
@@ -1,7 +1,15 @@
-import { Drawer } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Drawer,
+} from "@material-ui/core";
 import { useFormik } from "formik";
-import React, { FC, memo, useCallback } from "react";
+import React, { FC, memo, useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { UI_TEXT_CANCEL, UI_TEXT_DISCARD } from "../../constants/ui-text";
 import { AppState } from "../../modules";
 import { addApplication, fetchApplications } from "../../modules/applications";
 import { selectProjectName } from "../../modules/me";
@@ -18,9 +26,14 @@ export interface AddApplicationDrawerProps {
   onClose: () => void;
 }
 
+const CONFIRM_DIALOG_TITLE = "Quit adding application?";
+const CONFIRM_DIALOG_DESCRIPTION =
+  "Form values inputs so far will not be saved.";
+
 export const AddApplicationDrawer: FC<AddApplicationDrawerProps> = memo(
   function AddApplicationDrawer({ open, onClose }) {
     const dispatch = useDispatch<AppDispatch>();
+    const [showConfirm, setShowConfirm] = useState(false);
     const formik = useFormik<ApplicationFormValue>({
       initialValues: emptyFormValues,
       validateOnMount: true,
@@ -30,7 +43,6 @@ export const AddApplicationDrawer: FC<AddApplicationDrawerProps> = memo(
         await dispatch(addApplication(values));
         dispatch(fetchApplications());
         onClose();
-        formik.resetForm();
       },
     });
 
@@ -39,23 +51,48 @@ export const AddApplicationDrawer: FC<AddApplicationDrawerProps> = memo(
     );
 
     const handleClose = useCallback(() => {
-      onClose();
-      formik.resetForm();
+      if (formik.dirty) {
+        setShowConfirm(true);
+      } else {
+        onClose();
+        formik.resetForm();
+      }
     }, [onClose, formik]);
 
     return (
-      <Drawer
-        anchor="right"
-        open={open}
-        onClose={handleClose}
-        ModalProps={{ disableBackdropClick: formik.isSubmitting }}
-      >
-        <ApplicationForm
-          {...formik}
-          title={`Add a new application to "${projectName}" project`}
+      <>
+        <Drawer
+          anchor="right"
+          open={open}
           onClose={handleClose}
-        />
-      </Drawer>
+          ModalProps={{ disableBackdropClick: formik.isSubmitting }}
+        >
+          <ApplicationForm
+            {...formik}
+            title={`Add a new application to "${projectName}" project`}
+            onClose={handleClose}
+          />
+        </Drawer>
+        <Dialog open={showConfirm}>
+          <DialogTitle>{CONFIRM_DIALOG_TITLE}</DialogTitle>
+          <DialogContent>{CONFIRM_DIALOG_DESCRIPTION}</DialogContent>
+          <DialogActions>
+            <Button onClick={() => setShowConfirm(false)}>
+              {UI_TEXT_CANCEL}
+            </Button>
+            <Button
+              color="primary"
+              onClick={() => {
+                setShowConfirm(false);
+                onClose();
+                formik.resetForm();
+              }}
+            >
+              {UI_TEXT_DISCARD}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
     );
   }
 );
