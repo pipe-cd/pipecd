c57cc25b6d880df426594550df015e42e4279bbd
nakabonne <ryo@nakao.dev>
2021-02-25 11:28:30 +0900

    Treat context deadline exceeded error as a success

--- pkg/app/piped/analysisprovider/metrics/prometheus/prometheus.go
+++ pkg/app/piped/analysisprovider/metrics/prometheus/prometheus.go
@@ -16,6 +16,7 @@ package prometheus
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math"
 	"time"
@@ -73,6 +74,10 @@ func (p *Provider) RunQuery(ctx context.Context, query string, expected config.A
 	p.logger.Info("run query", zap.String("query", query))
 	// TODO: Use HTTP Basic Authentication with the username and password when needed.
 	response, warnings, err := p.api.Query(ctx, query, time.Now())
+	if errors.Is(err, context.DeadlineExceeded) {
+		// Treat as a success because query duration is already ended.
+		return true, nil
+	}
 	if err != nil {
 		return false, err
 	}
