2cb5ec119f73c0a8a2b02155d3281118f307e177
Ryo Narita <mail.ryonarita@gmail.com>
2021-02-26 20:04:08 +0900

    Use echarts for building chart component instead of recharts

--- pkg/app/web/src/pages/insight/index.tsx
+++ pkg/app/web/src/pages/insight/index.tsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { DeploymentFrequencyChart } from "../../components/deployment-frequency-chart";
 import { InsightHeader } from "../../components/insight-header";
 import { AppState } from "../../modules";
-import { fetchApplications } from "../../modules/applications";
+import { fetchApplications, selectById } from "../../modules/applications";
 import { InsightDataPoint } from "../../modules/insight";
 
 export const InsightIndexPage: FC = memo(function InsightIndexPage() {
@@ -14,6 +14,17 @@ export const InsightIndexPage: FC = memo(function InsightIndexPage() {
     AppState,
     InsightDataPoint.AsObject[]
   >((state) => state.deploymentFrequency.data);
+  const selectedAppName = useSelector<AppState, string | undefined>((state) =>
+    state.insight.applicationId
+      ? selectById(state.applications, state.insight.applicationId)?.name
+      : undefined
+  );
+
+  const data: { name: string; points: InsightDataPoint.AsObject[] }[] = [];
+
+  if (deploymentFrequency.length > 0) {
+    data.push({ name: selectedAppName || "All", points: deploymentFrequency });
+  }
 
   useEffect(() => {
     dispatch(fetchApplications());
@@ -28,7 +39,7 @@ export const InsightIndexPage: FC = memo(function InsightIndexPage() {
         gridTemplateColumns="repeat(2, 1fr)"
         mt={2}
       >
-        <DeploymentFrequencyChart data={deploymentFrequency} />
+        <DeploymentFrequencyChart data={data} />
       </Box>
     </Box>
   );
