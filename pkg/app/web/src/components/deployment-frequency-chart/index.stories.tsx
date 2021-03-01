5c6d35bcb3ed7890c61c63eed8129a3e47f5061d
Ryo Narita <mail.ryonarita@gmail.com>
2021-03-01 11:33:03 +0900

    Use echarts for building chart component instead of recharts (#1628)
    **What this PR does / why we need it**:
    
    https://echarts.apache.org/en/index.html
    
    `echars` can draw more graphical charts.
    It is well documented and easy to customize.
    So I switch library for render chats to this.
    
    
    ![image](https://user-images.githubusercontent.com/6136383/109293540-489ec600-786f-11eb-9e9d-2b871c2c6430.png)
    
    
    ![image](https://user-images.githubusercontent.com/6136383/109293514-3e7cc780-786f-11eb-9bbd-a8a7b360fb8f.png)
    
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    
    Insight is under development and is not yet visible to users.
    So this is maybe `NONE`.
    
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/web/src/components/deployment-frequency-chart/index.stories.tsx
+++ pkg/app/web/src/components/deployment-frequency-chart/index.stories.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@material-ui/core";
+import { Story } from "@storybook/react/types-6-0";
 import React from "react";
-import { DeploymentFrequencyChart } from "./";
+import { DeploymentFrequencyChart, DeploymentFrequencyChartProps } from "./";
 
 export default {
   title: "insights/DeploymentFrequencyChart",
@@ -12,13 +13,38 @@ const randData = Array.from(new Array(20)).map((_, v) => ({
   timestamp: new Date(`2020/10/${v + 5}`).getTime(),
 }));
 
-export const overview: React.FC = () => (
+const Template: Story<DeploymentFrequencyChartProps> = (args) => (
   <Box width={800}>
-    <DeploymentFrequencyChart data={randData} />
+    <DeploymentFrequencyChart data={args.data} />
   </Box>
 );
-export const noData: React.FC = () => (
-  <Box width={800}>
-    <DeploymentFrequencyChart data={[]} />
-  </Box>
-);
+export const Overview = Template.bind({});
+Overview.args = {
+  data: [{ name: "application-1", points: randData }],
+};
+
+export const MultipleApplication = Template.bind({});
+MultipleApplication.args = {
+  data: [
+    { name: "application-1", points: randData },
+    {
+      name: "application-2",
+      points: randData.map((v) => ({
+        ...v,
+        value: Math.floor(Math.random() * 30 + 5),
+      })),
+    },
+    {
+      name: "application-3",
+      points: randData.map((v) => ({
+        ...v,
+        value: Math.floor(Math.random() * 10 + 15),
+      })),
+    },
+  ],
+};
+
+export const NoData = Template.bind({});
+NoData.args = {
+  data: [],
+};
--- pkg/app/web/src/components/deployment-frequency-chart/index.tsx
+++ pkg/app/web/src/components/deployment-frequency-chart/index.tsx
@@ -1,22 +1,29 @@
-import React, { FC } from "react";
 import { Box, makeStyles, Paper, Typography } from "@material-ui/core";
-import { InsightDataPoint } from "pipe/pkg/app/web/model/insight_pb";
-import {
-  LineChart,
-  Line,
-  YAxis,
-  XAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
-import dayjs from "dayjs";
-import { theme } from "../../theme";
 import { WarningOutlined } from "@material-ui/icons";
+import dayjs from "dayjs";
+import { LineChart } from "echarts/charts";
+import {
+  GridComponent,
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+} from "echarts/components";
+import * as echarts from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { InsightDataPoint } from "pipe/pkg/app/web/model/insight_pb";
+import React, { FC, useEffect, useState } from "react";
+
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LineChart,
+  CanvasRenderer,
+  LegendComponent,
+]);
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: theme.spacing(3),
     minWidth: 600,
   },
   noDataMessage: {
@@ -25,14 +32,15 @@ const useStyles = makeStyles((theme) => ({
   noDataMessageIcon: {
     marginRight: theme.spacing(1),
   },
+  title: {
+    padding: theme.spacing(3),
+    paddingBottom: 0,
+  },
 }));
 
 export interface DeploymentFrequencyChartProps {
-  data: InsightDataPoint.AsObject[];
+  data: { name: string; points: InsightDataPoint.AsObject[] }[];
 }
-
-const tickFormatter = (time: number | string): string =>
-  dayjs(time).format("MMM DD");
 
 const labelFormatter = (time: number | string): string =>
   dayjs(time).format("YYYY MMM DD");
@@ -43,10 +51,40 @@ export const DeploymentFrequencyChart: FC<DeploymentFrequencyChartProps> = ({
   data,
 }) => {
   const classes = useStyles();
+  const [chart, setChart] = useState<echarts.ECharts | null>(null);
+
+  useEffect(() => {
+    if (chart && data.length !== 0) {
+      chart.setOption({
+        legend: { data: data.map((v) => v.name) },
+        xAxis: [
+          {
+            type: "category",
+            boundaryGap: false,
+            data: data[0].points.map((data) => labelFormatter(data.timestamp)),
+          },
+        ],
+        tooltip: {
+          trigger: "axis",
+        },
+        yAxis: [{ type: "value" }],
+        series: data.map((v) => ({
+          name: v.name,
+          type: "line",
+          stack: "deployments",
+          data: v.points.map((point) => point.value),
+          areaStyle: {},
+          emphasis: {
+            focus: "series",
+          },
+        })),
+      });
+    }
+  }, [chart, data]);
 
   return (
     <Paper elevation={1} className={classes.root}>
-      <Typography variant="h6" component="div">
+      <Typography variant="h6" component="div" className={classes.title}>
         Deployment Frequency
       </Typography>
       {data.length === 0 ? (
@@ -66,29 +104,14 @@ export const DeploymentFrequencyChart: FC<DeploymentFrequencyChartProps> = ({
           </Typography>
         </Box>
       ) : (
-        <ResponsiveContainer width="100%" aspect={2}>
-          <LineChart
-            data={data}
-            margin={{
-              top: 48,
-              right: 24,
-              left: 8,
-              bottom: 8,
-            }}
-          >
-            <CartesianGrid vertical={false} />
-            <Line
-              dataKey="value"
-              stroke={theme.palette.primary.main}
-              strokeWidth={2}
-              isAnimationActive={false}
-              dot={{ fill: theme.palette.primary.main }}
-            />
-            <YAxis axisLine={false} tickLine={false} />
-            <XAxis dataKey="timestamp" tickFormatter={tickFormatter} />
-            <Tooltip labelFormatter={labelFormatter} />
-          </LineChart>
-        </ResponsiveContainer>
+        <div
+          style={{ width: "100%", height: 400 }}
+          ref={(ref) => {
+            if (ref) {
+              setChart(echarts.init(ref));
+            }
+          }}
+        />
       )}
     </Paper>
   );
