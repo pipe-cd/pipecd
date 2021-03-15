b73e0c712bdf10c227e21ac80053cd9b8ee96f10
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2021-03-15 15:34:03 +0900

    Add a planner for ECS deployment (#1698)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Ref #1568
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/piped/planner/ecs/pipeline_test.go
@@ -0,0 +1,38 @@
+package ecs
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pipe-cd/pipe/pkg/model"
+)
+
+func TestBuildQuickSyncPipeline(t *testing.T) {
+	tests := []struct {
+		name             string
+		wantAutoRollback bool
+	}{
+		{
+			name:             "want auto rollback stage",
+			wantAutoRollback: true,
+		},
+		{
+			name:             "don't want auto rollback stage",
+			wantAutoRollback: true,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			stages := buildQuickSyncPipeline(tc.wantAutoRollback, time.Now())
+			var autoRollback bool
+			for _, stage := range stages {
+				if stage.Name == string(model.StageRollback) {
+					autoRollback = true
+				}
+			}
+			assert.Equal(t, tc.wantAutoRollback, autoRollback)
+		})
+	}
+}
