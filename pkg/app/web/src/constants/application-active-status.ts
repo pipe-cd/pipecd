b019851f56a485bc8e0c649d424d8a30667d7df1
Ryo Narita <mail.ryonarita@gmail.com>
2021-03-16 19:48:34 +0900

    Add application counts module (#1729)
    **What this PR does / why we need it**:
    
    Add application counts module to fetch and manage application counts data.
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/web/src/constants/application-active-status.ts
@@ -0,0 +1,10 @@
+import { ApplicationActiveStatus } from "../modules/applications";
+
+export const APPLICATION_ACTIVE_STATUS_NAME: Record<
+  ApplicationActiveStatus,
+  string
+> = {
+  [ApplicationActiveStatus.ENABLED]: "ENABLED",
+  [ApplicationActiveStatus.DISABLED]: "DISABLED",
+  [ApplicationActiveStatus.DELETED]: "DELETED",
+};
