6ce6fc63c05437e1ec08f2ed0ccd2d29058125c7
khanhtc1202 <khanhtc1202@gmail.com>
2021-03-22 19:10:08 +0900

    Remove unnecessary NOT NULL check for omitempty fields

--- pkg/app/ops/mysqlensurer/schema.sql
+++ pkg/app/ops/mysqlensurer/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS Application (
   Id BINARY(16) PRIMARY KEY,
   Data JSON NOT NULL,
   ProjectId VARCHAR(50) GENERATED ALWAYS AS (data->>"$.project_id") STORED NOT NULL,
-  Disabled BOOL GENERATED ALWAYS AS (data->>"$.disabled") STORED NOT NULL,
+  Disabled BOOL GENERATED ALWAYS AS (data->>"$.disabled") STORED,
   CreatedAt INT(11) GENERATED ALWAYS AS (data->>"$.created_at") STORED NOT NULL,
   UpdatedAt INT(11) GENERATED ALWAYS AS (data->>"$.updated_at") STORED NOT NULL,
   Extra VARCHAR(100) GENERATED ALWAYS AS (data->>"$._extra") STORED
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS Piped (
   Id BINARY(16) PRIMARY KEY,
   Data JSON NOT NULL,
   ProjectId VARCHAR(50) GENERATED ALWAYS AS (data->>"$.project_id") STORED NOT NULL,
-  Disabled BOOL GENERATED ALWAYS AS (data->>"$.disabled") STORED NOT NULL,
+  Disabled BOOL GENERATED ALWAYS AS (data->>"$.disabled") STORED,
   CreatedAt INT(11) GENERATED ALWAYS AS (data->>"$.created_at") STORED NOT NULL,
   UpdatedAt INT(11) GENERATED ALWAYS AS (data->>"$.updated_at") STORED NOT NULL,
   Extra VARCHAR(100) GENERATED ALWAYS AS (data->>"$._extra") STORED
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS APIKey (
   Id BINARY(16) PRIMARY KEY,
   Data JSON NOT NULL,
   ProjectId VARCHAR(50) GENERATED ALWAYS AS (data->>"$.project_id") STORED NOT NULL,
-  Disabled BOOL GENERATED ALWAYS AS (data->>"$.disabled") STORED NOT NULL,
+  Disabled BOOL GENERATED ALWAYS AS (data->>"$.disabled") STORED,
   CreatedAt INT(11) GENERATED ALWAYS AS (data->>"$.created_at") STORED NOT NULL,
   UpdatedAt INT(11) GENERATED ALWAYS AS (data->>"$.updated_at") STORED NOT NULL,
   Extra VARCHAR(100) GENERATED ALWAYS AS (data->>"$._extra") STORED
