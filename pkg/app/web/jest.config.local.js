428296a495237c3ddc3ec25a58aefa2e4c3b6822
Ryo Narita <mail.ryonarita@gmail.com>
2021-03-30 20:45:58 +0900

    Improve web test running speed

--- pkg/app/web/jest.config.local.js
+++ pkg/app/web/jest.config.local.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": "@swc/jest",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$":
       "<rootDir>/file-transformer.js",
   },
