06e0cea7a7051aede1f8f4ef1c90898159674817
Ryo Narita <mail.ryonarita@gmail.com>
2021-05-14 13:56:52 +0900

    Use native clipboard API for copy text on the web (#1964)
    **What this PR does / why we need it**:
    
    Use `Clipboard API` instead of `copy-to-clipboard` to copy text.
    
    https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText
    
    Browser compatibility seems to be fine.
    
    ![image](https://user-images.githubusercontent.com/6136383/118108852-767dba80-b41b-11eb-98dc-6050fe4fcc70.png)
    
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/web/src/components/copy-icon-button/index.test.tsx
@@ -0,0 +1,24 @@
+import { waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { render, screen, createStore } from "../../../test-utils";
+import { addToast } from "../../modules/toasts";
+import { CopyIconButton } from "./";
+
+test("copy text", async () => {
+  const store = createStore();
+  jest.spyOn(navigator.clipboard, "writeText");
+  render(<CopyIconButton name="ID" value="id" />, { store });
+  userEvent.click(screen.getByRole("button"));
+
+  expect(navigator.clipboard.writeText).toHaveBeenCalledWith("id");
+  await waitFor(() =>
+    expect(store.getActions()).toEqual([
+      {
+        payload: {
+          message: "ID copied to clipboard.",
+        },
+        type: addToast.type,
+      },
+    ])
+  );
+});
