20f6400f6b76fcd9e41ec22fae10c17289bd716b
Ryo Narita <mail.ryonarita@gmail.com>
2021-05-26 19:08:52 +0900

    Add web tests for the Application Counts (#2006)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/web/src/modules/application-counts.test.ts
+++ pkg/app/web/src/modules/application-counts.test.ts
@@ -1,6 +1,9 @@
 import { setupServer } from "msw/node";
-import { createReduxStore } from "../../test-utils";
-import { getInsightApplicationCountHandler } from "../mocks/services/insight";
+import { createReduxStore, createStore } from "../../test-utils";
+import {
+  getInsightApplicationCountHandler,
+  getInsightApplicationCountNotFound,
+} from "../mocks/services/insight";
 import {
   applicationCountsSlice,
   fetchApplicationCount,
@@ -26,37 +29,35 @@ describe("applicationCountsSlice reducer", () => {
       applicationCountsSlice.reducer(undefined, {
         type: "TEST_ACTION",
       })
-    ).toMatchInlineSnapshot(`
-      Object {
-        "counts": Object {
-          "CLOUDRUN": Object {
-            "DISABLED": 0,
-            "ENABLED": 0,
-          },
-          "CROSSPLANE": Object {
-            "DISABLED": 0,
-            "ENABLED": 0,
-          },
-          "ECS": Object {
-            "DISABLED": 0,
-            "ENABLED": 0,
-          },
-          "KUBERNETES": Object {
-            "DISABLED": 0,
-            "ENABLED": 0,
-          },
-          "LAMBDA": Object {
-            "DISABLED": 0,
-            "ENABLED": 0,
-          },
-          "TERRAFORM": Object {
-            "DISABLED": 0,
-            "ENABLED": 0,
-          },
+    ).toEqual({
+      counts: {
+        CLOUDRUN: {
+          DISABLED: 0,
+          ENABLED: 0,
         },
-        "updatedAt": 0,
-      }
-    `);
+        CROSSPLANE: {
+          DISABLED: 0,
+          ENABLED: 0,
+        },
+        ECS: {
+          DISABLED: 0,
+          ENABLED: 0,
+        },
+        KUBERNETES: {
+          DISABLED: 0,
+          ENABLED: 0,
+        },
+        LAMBDA: {
+          DISABLED: 0,
+          ENABLED: 0,
+        },
+        TERRAFORM: {
+          DISABLED: 0,
+          ENABLED: 0,
+        },
+      },
+      updatedAt: 0,
+    });
   });
 });
 
@@ -95,4 +96,19 @@ test("fetchApplicationCount", async () => {
       },
     })
   );
+});
+
+it("should be fulfilled if the API error is Not Found", async () => {
+  server.use(getInsightApplicationCountNotFound);
+  const store = createStore();
+
+  await store.dispatch(fetchApplicationCount());
+  expect(store.getActions()).toEqual([
+    expect.objectContaining({
+      type: fetchApplicationCount.pending.type,
+    }),
+    expect.objectContaining({
+      type: fetchApplicationCount.fulfilled.type,
+    }),
+  ]);
 });
