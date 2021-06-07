4c00bb85da0373f62d6bb6ac48634889e8e63524
nakabonne <ryo@nakao.dev>
2021-06-07 15:34:13 +0900

    Use MEMBER OF

--- pkg/datastore/mysql/query.go
+++ pkg/datastore/mysql/query.go
@@ -74,8 +74,8 @@ func buildWhereClause(filters []datastore.ListFilter) string {
 			// Make string of (?,...) which contains the number of `?` equal to the element number of filter.Value
 			valLength := reflect.ValueOf(filter.Value).Len()
 			conds[i] = fmt.Sprintf("%s %s (?%s)", filter.Field, filter.Operator, strings.Repeat(",?", valLength-1))
-		case "JSON_CONTAINS":
-			conds[i] = fmt.Sprintf("%s(%s, ?, '$')", filter.Operator, filter.Field)
+		case "MEMBER OF":
+			conds[i] = fmt.Sprintf("? %s (%s)", filter.Operator, filter.Field)
 		default:
 			conds[i] = fmt.Sprintf("%s %s ?", filter.Field, filter.Operator)
 		}
@@ -215,7 +215,7 @@ func refineFiltersOperator(filters []datastore.ListFilter) ([]datastore.ListFilt
 		case datastore.OperatorNotIn:
 			filter.Operator = "NOT IN"
 		case datastore.OperatorContains:
-			filter.Operator = "JSON_CONTAINS"
+			filter.Operator = "MEMBER OF"
 		case datastore.OperatorNotEqual,
 			datastore.OperatorGreaterThan,
 			datastore.OperatorGreaterThanOrEqual,
