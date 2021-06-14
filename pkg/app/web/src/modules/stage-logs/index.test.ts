e32ed44dcf059a1e9b7a28ec966711de5d329e91
Ryo Narita <mail.ryonarita@gmail.com>
2021-06-14 23:41:28 +0900

    Fix alerting error for stage log fetching

--- pkg/app/web/src/modules/stage-logs/index.test.ts
+++ pkg/app/web/src/modules/stage-logs/index.test.ts
@@ -5,11 +5,147 @@ import {
   StageLog,
   LogSeverity,
 } from "./";
+import { setupServer } from "msw/node";
+import { createReduxStore } from "../../../test-utils";
+import { dummyDeployment } from "../../__fixtures__/dummy-deployment";
+import {
+  getStageLogHandler,
+  getStageLogNotFoundHandler,
+  getStageLogInternalErrorHandler,
+} from "../../mocks/services/stage-log";
+import { dummyLogBlocks } from "../../__fixtures__/dummy-stage-log";
+import { StageStatus } from "../deployments";
+
+const server = setupServer();
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
 
 test("createActiveStageKey", () => {
   expect(
     createActiveStageKey({ deploymentId: "deployment-1", stageId: "stage-1" })
   ).toBe("deployment-1stage-1");
+});
+
+describe("async actions", () => {
+  describe("fetchStageLog", () => {
+    it("should store fetched stage data to state ", async () => {
+      server.use(getStageLogHandler);
+      const store = createReduxStore({
+        deployments: {
+          canceling: {},
+          cursor: "",
+          hasMore: false,
+          loading: {},
+          status: "idle",
+          ids: [dummyDeployment.id],
+          entities: { [dummyDeployment.id]: dummyDeployment },
+        },
+      });
+
+      await store.dispatch(
+        fetchStageLog({
+          deploymentId: dummyDeployment.id,
+          offsetIndex: 0,
+          stageId: dummyDeployment.stagesList[0].id,
+          retriedCount: 0,
+        })
+      );
+
+      expect(store.getState().stageLogs).toEqual({
+        [`${dummyDeployment.id}${dummyDeployment.stagesList[0].id}`]: {
+          deploymentId: dummyDeployment.id,
+          stageId: dummyDeployment.stagesList[0].id,
+          logBlocks: dummyLogBlocks,
+        },
+      });
+    });
+
+    it("should return initialState if API returns NOT_FOUND when stage is running", async () => {
+      server.use(getStageLogNotFoundHandler);
+      const deployment = {
+        ...dummyDeployment,
+        stagesList: [
+          {
+            ...dummyDeployment.stagesList[0],
+            status: StageStatus.STAGE_RUNNING,
+          },
+        ],
+      };
+
+      const store = createReduxStore({
+        deployments: {
+          canceling: {},
+          cursor: "",
+          hasMore: false,
+          loading: {},
+          status: "idle",
+          ids: [deployment.id],
+          entities: { [deployment.id]: deployment },
+        },
+      });
+
+      await store.dispatch(
+        fetchStageLog({
+          deploymentId: deployment.id,
+          offsetIndex: 0,
+          stageId: deployment.stagesList[0].id,
+          retriedCount: 0,
+        })
+      );
+
+      expect(store.getState().stageLogs).toEqual({
+        [`${deployment.id}${deployment.stagesList[0].id}`]: {
+          deploymentId: deployment.id,
+          stageId: deployment.stagesList[0].id,
+          logBlocks: [],
+        },
+      });
+      expect(store.getState().toasts).toEqual({ ids: [], entities: {} });
+    });
+
+    it("should add error toast if API return error code", async () => {
+      server.use(getStageLogInternalErrorHandler);
+      const store = createReduxStore({
+        deployments: {
+          canceling: {},
+          cursor: "",
+          hasMore: false,
+          loading: {},
+          status: "idle",
+          ids: [dummyDeployment.id],
+          entities: { [dummyDeployment.id]: dummyDeployment },
+        },
+      });
+
+      await store.dispatch(
+        fetchStageLog({
+          deploymentId: dummyDeployment.id,
+          offsetIndex: 0,
+          stageId: dummyDeployment.stagesList[0].id,
+          retriedCount: 0,
+        })
+      );
+
+      expect(store.getState().stageLogs).toEqual({
+        [`${dummyDeployment.id}${dummyDeployment.stagesList[0].id}`]: {
+          deploymentId: dummyDeployment.id,
+          stageId: dummyDeployment.stagesList[0].id,
+          logBlocks: [],
+        },
+      });
+      expect(store.getState().toasts).not.toEqual({ ids: [], entities: {} });
+    });
+  });
 });
 
 describe("stageLogsSlice reducer", () => {
@@ -35,7 +171,6 @@ describe("stageLogsSlice reducer", () => {
           stageId: "stage-1",
           deploymentId: "deployment-1",
           logBlocks: [],
-          completed: false,
         },
       });
     });
@@ -47,7 +182,6 @@ describe("stageLogsSlice reducer", () => {
         logBlocks: [
           { createdAt: 0, index: 0, log: "log", severity: LogSeverity.SUCCESS },
         ],
-        completed: false,
       };
       expect(
         stageLogsSlice.reducer(
@@ -56,7 +190,6 @@ describe("stageLogsSlice reducer", () => {
               stageId: "stage-1",
               deploymentId: "deployment-1",
               logBlocks: [],
-              completed: false,
             },
           },
           {
@@ -80,7 +213,6 @@ describe("stageLogsSlice reducer", () => {
               stageId: "stage-1",
               deploymentId: "deployment-1",
               logBlocks: [],
-              completed: false,
             },
           },
           {
@@ -95,7 +227,6 @@ describe("stageLogsSlice reducer", () => {
           stageId: "stage-1",
           deploymentId: "deployment-1",
           logBlocks: [],
-          completed: true,
         },
       });
     });
--- pkg/app/web/src/modules/stage-logs/index.ts
+++ pkg/app/web/src/modules/stage-logs/index.ts
@@ -1,17 +1,17 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { LogBlock } from "pipe/pkg/app/web/model/logblock_pb";
-import { DeploymentStatus } from "pipe/pkg/app/web/model/deployment_pb";
 import { getStageLog } from "../../api/stage-log";
-import { selectById as selectDeploymentById } from "../deployments";
+import {
+  selectById as selectDeploymentById,
+  StageStatus,
+} from "../deployments";
 import { AppState } from "../../store";
-
-export { LogSeverity } from "pipe/pkg/app/web/model/logblock_pb";
+import { StatusCode } from "grpc-web";
 
 export type StageLog = {
   deploymentId: string;
   stageId: string;
   logBlocks: LogBlock.AsObject[];
-  completed: boolean;
 };
 
 // NOTE: Use deploymentId + stageId as record key.
@@ -37,22 +37,25 @@ export const fetchStageLog = createAsyncThunk<
   async ({ deploymentId, offsetIndex, retriedCount, stageId }, thunkAPI) => {
     const s = thunkAPI.getState();
     const deployment = selectDeploymentById(s.deployments, deploymentId);
+    const initialLogs: StageLog = {
+      stageId,
+      deploymentId,
+      logBlocks: [],
+    };
 
     if (!deployment) {
       throw new Error(`Deployment: ${deploymentId} is not exists in state.`);
     }
 
-    // When the Deployment Status is `Pending` and `Planned`, the log doesn't exist, so it returns an empty log instead of requesting it.
-    if (
-      deployment.status === DeploymentStatus.DEPLOYMENT_PLANNED ||
-      deployment.status === DeploymentStatus.DEPLOYMENT_PENDING
-    ) {
-      return {
-        stageId,
-        deploymentId,
-        logBlocks: [],
-        completed: false,
-      };
+    const stage = deployment.stagesList.find((stage) => stage.id === stageId);
+    if (!stage) {
+      throw new Error(
+        `Stage (ID: ${stageId}) is not found in application state.`
+      );
+    }
+
+    if (stage.status === StageStatus.STAGE_NOT_STARTED_YET) {
+      return initialLogs;
     }
 
     const response = await getStageLog({
@@ -60,13 +63,27 @@ export const fetchStageLog = createAsyncThunk<
       offsetIndex,
       retriedCount,
       stageId,
+    }).catch((e: { code: number }) => {
+      // If status is running and error code is NOT_FOUND, it is maybe first state of deployment log.
+      // So we ignore this error and then return initialLogs below code.
+      if (
+        e.code === StatusCode.NOT_FOUND &&
+        stage.status === StageStatus.STAGE_RUNNING
+      ) {
+        return;
+      }
+
+      throw e;
     });
+
+    if (!response) {
+      return initialLogs;
+    }
 
     return {
       stageId,
       deploymentId,
       logBlocks: response.blocksList,
-      completed: response.completed,
     };
   }
 );
@@ -79,25 +96,17 @@ export const stageLogsSlice = createSlice({
     builder
       .addCase(fetchStageLog.pending, (state, action) => {
         const id = createActiveStageKey(action.meta.arg);
-        if (state[id]) {
-          state[id].completed = false;
-        } else {
+        if (!state[id]) {
           state[id] = {
             stageId: action.meta.arg.stageId,
             deploymentId: action.meta.arg.deploymentId,
             logBlocks: [],
-            completed: false,
           };
         }
       })
       .addCase(fetchStageLog.fulfilled, (state, action) => {
         const id = createActiveStageKey(action.meta.arg);
         state[id] = action.payload;
-        state[id].completed = true;
-      })
-      .addCase(fetchStageLog.rejected, (state, action) => {
-        const id = createActiveStageKey(action.meta.arg);
-        state[id].completed = true;
       });
   },
 });
@@ -112,4 +121,4 @@ export const selectStageLogById = (
   return state[createActiveStageKey(props)];
 };
 
-export { LogBlock } from "pipe/pkg/app/web/model/logblock_pb";
+export { LogBlock, LogSeverity } from "pipe/pkg/app/web/model/logblock_pb";
