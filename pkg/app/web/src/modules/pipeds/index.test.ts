2ecb52433f94f9b845fa73aa4ddfb7c4b53e0e91
Ryo Narita <mail.ryonarita@gmail.com>
2021-06-14 22:48:22 +0900

    Change the directory structure of the redux module to make it easier to read (#2074)
    **What this PR does / why we need it**:
    
    **BEFORE**
    ```
    └── modules
       ├── module-name.ts
       └── module-name.test.ts
    ```
    
    **AFTER**
    ```
    └── modules
       └── module-name
          ├── index.ts
          └── index.test.ts
    ```
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/web/src/modules/pipeds/index.test.ts
@@ -0,0 +1,168 @@
+import { dummyPiped } from "../../__fixtures__/dummy-piped";
+import {
+  pipedsSlice,
+  clearRegisteredPipedInfo,
+  addPiped,
+  fetchPipeds,
+  addNewPipedKey,
+  selectPipedsByEnv,
+  Piped,
+  editPiped,
+} from "./";
+
+const baseState = {
+  entities: {},
+  ids: [],
+  registeredPiped: null,
+  updating: false,
+};
+
+test("selectPipedsByEnv", () => {
+  const disabledPiped: Piped.AsObject = {
+    ...dummyPiped,
+    id: "piped-2",
+    disabled: true,
+  };
+  expect(selectPipedsByEnv({ entities: {}, ids: [] }, "env-1")).toEqual([]);
+  expect(
+    selectPipedsByEnv(
+      {
+        entities: {
+          [dummyPiped.id]: dummyPiped,
+          [disabledPiped.id]: disabledPiped,
+        },
+        ids: [dummyPiped.id, disabledPiped.id],
+      },
+      dummyPiped.envIdsList[0]
+    )
+  ).toEqual([dummyPiped]);
+});
+
+describe("pipedsSlice reducer", () => {
+  it("should return the initial state", () => {
+    expect(
+      pipedsSlice.reducer(undefined, {
+        type: "TEST_ACTION",
+      })
+    ).toEqual(baseState);
+  });
+
+  it(`should handle ${clearRegisteredPipedInfo.type}`, () => {
+    expect(
+      pipedsSlice.reducer(
+        {
+          ...baseState,
+          registeredPiped: {
+            id: "piped-1",
+            key: "piped-key",
+            isNewKey: false,
+          },
+        },
+        {
+          type: clearRegisteredPipedInfo.type,
+        }
+      )
+    ).toEqual(baseState);
+  });
+
+  describe("addPiped", () => {
+    it(`should handle ${addPiped.fulfilled.type}`, () => {
+      expect(
+        pipedsSlice.reducer(baseState, {
+          type: addPiped.fulfilled.type,
+          payload: {
+            id: "piped-1",
+            key: "piped-key",
+            isNewKey: false,
+          },
+        })
+      ).toEqual({
+        ...baseState,
+        registeredPiped: {
+          id: "piped-1",
+          key: "piped-key",
+          isNewKey: false,
+        },
+      });
+    });
+  });
+
+  describe("fetchPipeds", () => {
+    it(`should handle ${fetchPipeds.fulfilled.type}`, () => {
+      expect(
+        pipedsSlice.reducer(baseState, {
+          type: fetchPipeds.fulfilled.type,
+          payload: [dummyPiped],
+        })
+      ).toEqual({
+        ...baseState,
+        entities: { [dummyPiped.id]: dummyPiped },
+        ids: [dummyPiped.id],
+      });
+    });
+  });
+
+  describe("recreatePipedKey", () => {
+    it(`should handle ${addNewPipedKey.fulfilled.type}`, () => {
+      expect(
+        pipedsSlice.reducer(baseState, {
+          type: addNewPipedKey.fulfilled.type,
+          payload: "add-new-piped-key",
+          meta: {
+            arg: {
+              pipedId: "piped-1",
+            },
+          },
+        })
+      ).toEqual({
+        ...baseState,
+        registeredPiped: {
+          id: "piped-1",
+          key: "add-new-piped-key",
+          isNewKey: true,
+        },
+      });
+    });
+  });
+
+  describe("editPiped", () => {
+    it(`should handle ${editPiped.pending.type}`, () => {
+      expect(
+        pipedsSlice.reducer(baseState, {
+          type: editPiped.pending.type,
+        })
+      ).toEqual({
+        ...baseState,
+        updating: true,
+      });
+    });
+
+    it(`should handle ${editPiped.rejected.type}`, () => {
+      expect(
+        pipedsSlice.reducer(
+          { ...baseState, updating: true },
+          {
+            type: editPiped.rejected.type,
+          }
+        )
+      ).toEqual({
+        ...baseState,
+        updating: false,
+      });
+    });
+
+    it(`should handle ${editPiped.fulfilled.type}`, () => {
+      expect(
+        pipedsSlice.reducer(
+          { ...baseState, updating: true },
+          {
+            type: editPiped.fulfilled.type,
+          }
+        )
+      ).toEqual({
+        ...baseState,
+        updating: false,
+      });
+    });
+  });
+});
---
+++ pkg/app/web/src/modules/stage-logs/index.ts
@@ -0,0 +1,115 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { LogBlock } from "pipe/pkg/app/web/model/logblock_pb";
+import { DeploymentStatus } from "pipe/pkg/app/web/model/deployment_pb";
+import { getStageLog } from "../../api/stage-log";
+import { selectById as selectDeploymentById } from "../deployments";
+import { AppState } from "../../store";
+
+export { LogSeverity } from "pipe/pkg/app/web/model/logblock_pb";
+
+export type StageLog = {
+  deploymentId: string;
+  stageId: string;
+  logBlocks: LogBlock.AsObject[];
+  completed: boolean;
+};
+
+// NOTE: Use deploymentId + stageId as record key.
+type StageLogs = Record<string, StageLog>;
+const initialState: StageLogs = {};
+
+export const createActiveStageKey = (props: {
+  deploymentId: string;
+  stageId: string;
+}): string => `${props.deploymentId}${props.stageId}`;
+
+export const fetchStageLog = createAsyncThunk<
+  StageLog,
+  {
+    deploymentId: string;
+    stageId: string;
+    offsetIndex: number;
+    retriedCount: number;
+  },
+  { state: AppState }
+>(
+  "stage-logs/fetch",
+  async ({ deploymentId, offsetIndex, retriedCount, stageId }, thunkAPI) => {
+    const s = thunkAPI.getState();
+    const deployment = selectDeploymentById(s.deployments, deploymentId);
+
+    if (!deployment) {
+      throw new Error(`Deployment: ${deploymentId} is not exists in state.`);
+    }
+
+    // When the Deployment Status is `Pending` and `Planned`, the log doesn't exist, so it returns an empty log instead of requesting it.
+    if (
+      deployment.status === DeploymentStatus.DEPLOYMENT_PLANNED ||
+      deployment.status === DeploymentStatus.DEPLOYMENT_PENDING
+    ) {
+      return {
+        stageId,
+        deploymentId,
+        logBlocks: [],
+        completed: false,
+      };
+    }
+
+    const response = await getStageLog({
+      deploymentId,
+      offsetIndex,
+      retriedCount,
+      stageId,
+    });
+
+    return {
+      stageId,
+      deploymentId,
+      logBlocks: response.blocksList,
+      completed: response.completed,
+    };
+  }
+);
+
+export const stageLogsSlice = createSlice({
+  name: "stageLogs",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchStageLog.pending, (state, action) => {
+        const id = createActiveStageKey(action.meta.arg);
+        if (state[id]) {
+          state[id].completed = false;
+        } else {
+          state[id] = {
+            stageId: action.meta.arg.stageId,
+            deploymentId: action.meta.arg.deploymentId,
+            logBlocks: [],
+            completed: false,
+          };
+        }
+      })
+      .addCase(fetchStageLog.fulfilled, (state, action) => {
+        const id = createActiveStageKey(action.meta.arg);
+        state[id] = action.payload;
+        state[id].completed = true;
+      })
+      .addCase(fetchStageLog.rejected, (state, action) => {
+        const id = createActiveStageKey(action.meta.arg);
+        state[id].completed = true;
+      });
+  },
+});
+
+export const selectStageLogById = (
+  state: StageLogs,
+  props: {
+    deploymentId: string;
+    stageId: string;
+  }
+): StageLog | null => {
+  return state[createActiveStageKey(props)];
+};
+
+export { LogBlock } from "pipe/pkg/app/web/model/logblock_pb";
