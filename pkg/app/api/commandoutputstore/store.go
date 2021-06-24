862b2e3fdb5abb13de82babf1b165d5a9ca6e224
nghialv <nghialv2607@gmail.com>
2021-06-24 11:26:01 +0900

    Implement RPC to retrieve planpreview results

---
+++ pkg/app/api/commandoutputstore/store.go
@@ -0,0 +1,71 @@
+// Copyright 2021 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package commandoutputstore
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/pipe-cd/pipe/pkg/filestore"
+)
+
+var (
+	ErrNotFound = errors.New("Not Found")
+)
+
+type Store interface {
+	Get(ctx context.Context, commandID string) ([]byte, error)
+	Put(ctx context.Context, commandID string, data []byte) error
+}
+
+type store struct {
+	backend filestore.Store
+	logger  *zap.Logger
+}
+
+func NewStore(fs filestore.Store, logger *zap.Logger) Store {
+	return &store{
+		backend: fs,
+		logger:  logger.Named("command-output-store"),
+	}
+}
+
+func (s *store) Get(ctx context.Context, commandID string) ([]byte, error) {
+	path := dataPath(commandID)
+	obj, err := s.backend.GetObject(ctx, path)
+	if err != nil {
+		if err == filestore.ErrNotFound {
+			return nil, ErrNotFound
+		}
+		s.logger.Error("failed to get command ouput from filestore",
+			zap.String("command", commandID),
+			zap.Error(err),
+		)
+		return nil, err
+	}
+	return obj.Content, nil
+}
+
+func (s *store) Put(ctx context.Context, commandID string, data []byte) error {
+	path := dataPath(commandID)
+	return s.backend.PutObject(ctx, path, data)
+}
+
+func dataPath(commandID string) string {
+	return fmt.Sprintf("command-output/%s.json", commandID)
+}
--- pkg/app/api/commandoutputstore/store_test.go
+++ pkg/app/api/commandoutputstore/store_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The PipeCD Authors.
+// Copyright 2021 The PipeCD Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,15 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package model
-
-import "context"
-
-type ReportableCommand struct {
-	*Command
-	Report func(ctx context.Context, status CommandStatus, metadata map[string]string) error
-}
-
-func (c *Command) IsHandled() bool {
-	return c.Status != CommandStatus_COMMAND_NOT_HANDLED_YET
-}
+package commandoutputstore
