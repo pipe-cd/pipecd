362aa121d736f46e8c1c3936360d26cb545fe019
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2021-07-13 21:28:58 +0900

    Use declared TTL for stat cache instead of default value for normal cache (#2233)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- cmd/pipecd/server.go
+++ cmd/pipecd/server.go
@@ -65,6 +65,7 @@ var (
 
 const (
 	defaultPipedStatHashKey = "HASHKEY:PIPED:STATS"
+	pipedStatTTL            = 2 * time.Minute
 )
 
 type httpHandler interface {
@@ -189,7 +190,7 @@ func (s *server) run(ctx context.Context, t cli.Telemetry) error {
 	cmds := commandstore.NewStore(ds, cache, t.Logger)
 	is := insightstore.NewStore(fs)
 	cmdOutputStore := commandoutputstore.NewStore(fs, t.Logger)
-	statCache := rediscache.NewTTLHashCache(rd, cfg.Cache.TTLDuration(), defaultPipedStatHashKey)
+	statCache := rediscache.NewTTLHashCache(rd, pipedStatTTL, defaultPipedStatHashKey)
 
 	// Start a gRPC server for handling PipedAPI requests.
 	{
