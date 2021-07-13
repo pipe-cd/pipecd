ee9c5bc55779dfc68d586269de4164d646cfcedb
nghialv <nghialv2607@gmail.com>
2021-07-13 18:21:26 +0900

    Move source code of action-plan-preview to dockers dir

---
+++ dockers/action-plan-preview/go.mod
@@ -0,0 +1,8 @@
+module github.com/pipe-cd/actions-plan-preview
+
+go 1.16
+
+require (
+	github.com/google/go-github/v36 v36.0.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
---
+++ dockers/action-plan-preview/planpreview.go
@@ -0,0 +1,204 @@
+// Copyright 2021 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type PlanPreviewResult struct {
+	Applications        []ApplicationResult
+	FailureApplications []FailureApplication
+	FailurePipeds       []FailurePiped
+}
+
+func (r *PlanPreviewResult) HasError() bool {
+	return len(r.FailureApplications)+len(r.FailurePipeds) > 0
+}
+
+func (r *PlanPreviewResult) NoChange() bool {
+	return len(r.Applications)+len(r.FailureApplications)+len(r.FailurePipeds) == 0
+}
+
+type ApplicationResult struct {
+	ApplicationInfo
+	SyncStrategy string // QUICK_SYNC, PIPELINE
+	PlanSummary  string
+	PlanDetails  string
+}
+
+type FailurePiped struct {
+	PipedInfo
+	Reason string
+}
+
+type FailureApplication struct {
+	ApplicationInfo
+	Reason      string
+	PlanDetails string
+}
+
+type PipedInfo struct {
+	PipedID  string
+	PipedURL string
+}
+
+type ApplicationInfo struct {
+	ApplicationID        string
+	ApplicationName      string
+	ApplicationURL       string
+	EnvID                string
+	EnvName              string
+	EnvURL               string
+	ApplicationKind      string // KUBERNETES, TERRAFORM, CLOUDRUN, LAMBDA, ECS
+	ApplicationDirectory string
+}
+
+func retrievePlanPreview(
+	ctx context.Context,
+	remoteURL,
+	baseBranch,
+	headBranch,
+	headCommit,
+	address,
+	apiKey string,
+	timeout time.Duration,
+) (*PlanPreviewResult, error) {
+
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a temporary directory (%w)", err)
+	}
+	outPath := filepath.Join(dir, "result.json")
+
+	args := []string{
+		"plan-preview",
+		"--repo-remote-url", remoteURL,
+		"--base-branch", baseBranch,
+		"--head-branch", headBranch,
+		"--head-commit", headCommit,
+		"--address", address,
+		"--api-key", apiKey,
+		"--timeout", timeout.String(),
+		"--out", outPath,
+	}
+	cmd := exec.CommandContext(ctx, "pipectl", args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute pipectl command (%w) (%s)", err, string(out))
+	}
+
+	log.Println(string(out))
+
+	data, err := ioutil.ReadFile(outPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read result file (%w)", err)
+	}
+
+	var r PlanPreviewResult
+	if err := json.Unmarshal(data, &r); err != nil {
+		return nil, fmt.Errorf("failed to parse result file (%w)", err)
+	}
+
+	return &r, nil
+}
+
+const (
+	successBadgeURL = `<!-- pipecd-plan-preview-->
+[![PLAN_PREVIEW](https://img.shields.io/static/v1?label=PipeCD&message=Plan_Preview&color=success&style=flat)](https://pipecd.dev/docs/user-guide/plan-preview/)
+
+`
+	failureBadgeURL = `<!-- pipecd-plan-preview-->
+[![PLAN_PREVIEW](https://img.shields.io/static/v1?label=PipeCD&message=Plan_Preview&color=orange&style=flat)](https://pipecd.dev/docs/user-guide/plan-preview/)
+
+`
+
+	noChangeTitleFormat  = "Ran plan-preview against head commit %s of this pull request. PipeCD detected `0` updated application. It means no deployment will be triggered once this pull request got merged.\n"
+	hasChangeTitleFormat = "Ran plan-preview against head commit %s of this pull request. PipeCD detected `%d` updated applications and here are their plan results. Once this pull request got merged their deployments will be triggered to run as these estimations.\n"
+	detailsFormat        = "<details>\n<summary>Details (Click me)</summary>\n<p>\n\n``` %s\n%s\n```\n</p>\n</details>\n"
+)
+
+func makeCommentBody(event *githubEvent, r *PlanPreviewResult) string {
+	var b strings.Builder
+
+	if !r.HasError() {
+		b.WriteString(successBadgeURL)
+	} else {
+		b.WriteString(failureBadgeURL)
+	}
+
+	if event.IsComment {
+		b.WriteString(fmt.Sprintf("@%s ", event.SenderLogin))
+	}
+
+	if r.NoChange() {
+		fmt.Fprintf(&b, noChangeTitleFormat, event.HeadCommit)
+		return b.String()
+	}
+
+	b.WriteString(fmt.Sprintf(hasChangeTitleFormat, event.HeadCommit, len(r.Applications)))
+
+	for _, app := range r.Applications {
+		fmt.Fprintf(&b, "\n## app: [%s](%s), env: [%s](%s), kind: %s\n", app.ApplicationName, app.ApplicationURL, app.EnvName, app.EnvURL, strings.ToLower(app.ApplicationKind))
+		fmt.Fprintf(&b, "Sync strategy: %s\n", app.SyncStrategy)
+		fmt.Fprintf(&b, "Summary: %s\n\n", app.PlanSummary)
+
+		var lang string = "diff"
+		if app.ApplicationKind == "TERRAFORM" {
+			lang = "hcl"
+		}
+		fmt.Fprintf(&b, detailsFormat, lang, app.PlanDetails)
+	}
+
+	if !r.HasError() {
+		return b.String()
+	}
+
+	fmt.Fprintf(&b, "\n---\n\n## NOTE\n\n")
+
+	if len(r.FailureApplications) > 0 {
+		fmt.Fprintf(&b, "**An error occurred while building plan-preview for the following applications**\n")
+
+		for _, app := range r.FailureApplications {
+			fmt.Fprintf(&b, "## app: [%s](%s), env: [%s](%s), kind: %s\n", app.ApplicationName, app.ApplicationURL, app.EnvName, app.EnvURL, strings.ToLower(app.ApplicationKind))
+			fmt.Fprintf(&b, "Reason: %s\n\n", app.Reason)
+
+			var lang string = "diff"
+			if app.ApplicationKind == "TERRAFORM" {
+				lang = "hcl"
+			}
+			fmt.Fprintf(&b, detailsFormat, lang, app.PlanDetails)
+		}
+	}
+
+	if len(r.FailurePipeds) > 0 {
+		fmt.Fprintf(&b, "**An error occurred while building plan-preview for applications of the following Pipeds**\n")
+
+		for _, piped := range r.FailurePipeds {
+			fmt.Fprintf(&b, "## piped: [%s](%s)\n", piped.PipedID, piped.PipedURL)
+			fmt.Fprintf(&b, "Reason: %s\n\n", piped.Reason)
+		}
+	}
+
+	return b.String()
+}
