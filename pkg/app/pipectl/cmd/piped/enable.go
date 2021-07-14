aa7ce81d2a825dc4d545d9565c7061e67f485a3c
Le Van Nghia <nghialv2607@gmail.com>
2021-07-14 12:02:28 +0900

    Add ability to enable/disable Piped by using pipectl (#2238)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    Add ability to enable/disable Piped by using pipectl
    ```
    
    This PR was merged by Kapetanios.

---
+++ pkg/app/pipectl/cmd/piped/enable.go
@@ -0,0 +1,70 @@
+// Copyright 2021 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package piped
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/pipe-cd/pipe/pkg/app/api/service/apiservice"
+	"github.com/pipe-cd/pipe/pkg/cli"
+)
+
+type enable struct {
+	root *command
+
+	pipedID string
+	stdout  io.Writer
+}
+
+func newEnableCommand(root *command) *cobra.Command {
+	c := &enable{
+		root:   root,
+		stdout: os.Stdout,
+	}
+	cmd := &cobra.Command{
+		Use:   "enable",
+		Short: "Enable a given Piped.",
+		RunE:  cli.WithContext(c.run),
+	}
+
+	cmd.Flags().StringVar(&c.pipedID, "piped-id", c.pipedID, "The Piped ID.")
+	cmd.MarkFlagRequired("piped-id")
+
+	return cmd
+}
+
+func (c *enable) run(ctx context.Context, t cli.Telemetry) error {
+	cli, err := c.root.clientOptions.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to initialize client: %w", err)
+	}
+	defer cli.Close()
+
+	req := &apiservice.EnablePipedRequest{
+		PipedId: c.pipedID,
+	}
+	if _, err := cli.EnablePiped(ctx, req); err != nil {
+		fmt.Fprintf(c.stdout, "Failed to enable Piped %s (%v)\n", c.pipedID, err)
+		return err
+	}
+
+	fmt.Fprintf(c.stdout, "Successfully enabled Piped %s\n", c.pipedID)
+	return nil
+}
