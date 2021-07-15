863b0c98f28f207303d149b7b6eefffc14614a99
nghialv <nghialv2607@gmail.com>
2021-07-15 10:49:49 +0900

    Add examples for secret-management feature

---
+++ examples/terraform/secret-management/main.tf
@@ -0,0 +1,22 @@
+variable "project" {}
+
+provider "google" {
+  project     = var.project
+  credentials = ".credentials/service-account.json"
+}
+
+terraform {
+  backend "gcs" {
+    bucket      = "pipecd-terraform-examples"
+    prefix      = "tfstates/simple"
+    credentials = ".credentials/service-account.json"
+  }
+}
+
+variable "content" {}
+
+resource "google_storage_bucket_object" "object" {
+  name    = "examples/simple/${terraform.workspace}.txt"
+  bucket  = "pipecd-terraform-examples"
+  content = var.content
+}
