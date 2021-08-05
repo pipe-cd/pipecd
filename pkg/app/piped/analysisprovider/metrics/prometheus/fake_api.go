e9b0616d85d44c6bf849832d636f4ca526c6eb40
nakabonne <ryo@nakao.dev>
2021-08-05 15:19:15 +0900

    Rename receiver

--- pkg/app/piped/analysisprovider/metrics/prometheus/fake_api.go
+++ pkg/app/piped/analysisprovider/metrics/prometheus/fake_api.go
@@ -14,90 +14,90 @@ type fakeAPI struct {
 	warnings v1.Warnings
 }
 
-func (m fakeAPI) Query(_ context.Context, _ string, _ time.Time) (model.Value, v1.Warnings, error) {
-	if m.err != nil {
-		return nil, m.warnings, m.err
+func (f fakeAPI) Query(_ context.Context, _ string, _ time.Time) (model.Value, v1.Warnings, error) {
+	if f.err != nil {
+		return nil, f.warnings, f.err
 	}
-	return m.value, m.warnings, nil
+	return f.value, f.warnings, nil
 }
 
-func (m fakeAPI) QueryRange(_ context.Context, _ string, _ v1.Range) (model.Value, v1.Warnings, error) {
-	if m.err != nil {
-		return nil, m.warnings, m.err
+func (f fakeAPI) QueryRange(_ context.Context, _ string, _ v1.Range) (model.Value, v1.Warnings, error) {
+	if f.err != nil {
+		return nil, f.warnings, f.err
 	}
-	return m.value, m.warnings, nil
+	return f.value, f.warnings, nil
 }
 
 // Below methods are required to meet the interface.
 
-func (y fakeAPI) Alerts(ctx context.Context) (v1.AlertsResult, error) {
+func (f fakeAPI) Alerts(ctx context.Context) (v1.AlertsResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) AlertManagers(ctx context.Context) (v1.AlertManagersResult, error) {
+func (f fakeAPI) AlertManagers(ctx context.Context) (v1.AlertManagersResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) CleanTombstones(ctx context.Context) error {
+func (f fakeAPI) CleanTombstones(ctx context.Context) error {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Config(ctx context.Context) (v1.ConfigResult, error) {
+func (f fakeAPI) Config(ctx context.Context) (v1.ConfigResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) DeleteSeries(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) error {
+func (f fakeAPI) DeleteSeries(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) error {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Flags(ctx context.Context) (v1.FlagsResult, error) {
+func (f fakeAPI) Flags(ctx context.Context) (v1.FlagsResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) LabelNames(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]string, v1.Warnings, error) {
+func (f fakeAPI) LabelNames(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]string, v1.Warnings, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) LabelValues(ctx context.Context, label string, matches []string, startTime time.Time, endTime time.Time) (model.LabelValues, v1.Warnings, error) {
+func (f fakeAPI) LabelValues(ctx context.Context, label string, matches []string, startTime time.Time, endTime time.Time) (model.LabelValues, v1.Warnings, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) QueryExemplars(ctx context.Context, query string, startTime time.Time, endTime time.Time) ([]v1.ExemplarQueryResult, error) {
+func (f fakeAPI) QueryExemplars(ctx context.Context, query string, startTime time.Time, endTime time.Time) ([]v1.ExemplarQueryResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Buildinfo(ctx context.Context) (v1.BuildinfoResult, error) {
+func (f fakeAPI) Buildinfo(ctx context.Context) (v1.BuildinfoResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Runtimeinfo(ctx context.Context) (v1.RuntimeinfoResult, error) {
+func (f fakeAPI) Runtimeinfo(ctx context.Context) (v1.RuntimeinfoResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Series(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]model.LabelSet, v1.Warnings, error) {
+func (f fakeAPI) Series(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]model.LabelSet, v1.Warnings, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Snapshot(ctx context.Context, skipHead bool) (v1.SnapshotResult, error) {
+func (f fakeAPI) Snapshot(ctx context.Context, skipHead bool) (v1.SnapshotResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Rules(ctx context.Context) (v1.RulesResult, error) {
+func (f fakeAPI) Rules(ctx context.Context) (v1.RulesResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Targets(ctx context.Context) (v1.TargetsResult, error) {
+func (f fakeAPI) Targets(ctx context.Context) (v1.TargetsResult, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) TargetsMetadata(ctx context.Context, matchTarget string, metric string, limit string) ([]v1.MetricMetadata, error) {
+func (f fakeAPI) TargetsMetadata(ctx context.Context, matchTarget string, metric string, limit string) ([]v1.MetricMetadata, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) Metadata(ctx context.Context, metric string, limit string) (map[string][]v1.Metadata, error) {
+func (f fakeAPI) Metadata(ctx context.Context, metric string, limit string) (map[string][]v1.Metadata, error) {
 	panic("this method doesn't expect to be called")
 }
 
-func (y fakeAPI) TSDB(ctx context.Context) (v1.TSDBResult, error) {
+func (f fakeAPI) TSDB(ctx context.Context) (v1.TSDBResult, error) {
 	panic("this method doesn't expect to be called")
 }
