b43c7d548b2d825e7c0f34b83341371480cdce8a
khanhtc1202 <khanhtc1202@gmail.com>
2021-09-16 18:11:53 +0900

    Typo

--- pkg/app/web/src/components/application-detail-page/application-state-view/index.tsx
+++ pkg/app/web/src/components/application-detail-page/application-state-view/index.tsx
@@ -62,12 +62,12 @@ export const ApplicationStateView: FC<ApplicationStateViewProps> = memo(
 
     useInterval(
       () => {
-        // Only fetch kubernest application.
+        // Only fetch kubernetes application.
         if (app && app.kind === ApplicationKind.KUBERNETES) {
           dispatch(fetchApplicationStateById(app.id));
         }
       },
-      // Only fetch kubernest application.
+      // Only fetch kubernetes application.
       app && app.kind === ApplicationKind.KUBERNETES && hasError === false ? FETCH_INTERVAL : null
     );
 
