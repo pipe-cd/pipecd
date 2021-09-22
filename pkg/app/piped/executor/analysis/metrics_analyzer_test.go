d5fc8f7748aa89bc3078bbfad362c7635343b150
nakabonne <ryo@nakao.dev>
2021-09-22 18:59:58 +0900

    Enable ADA stage to compare two samples using Mann-Whitney U test

--- pkg/app/piped/executor/analysis/metrics_analyzer_test.go
+++ pkg/app/piped/executor/analysis/metrics_analyzer_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package analysis
 
 import (
@@ -121,6 +135,80 @@ func Test_metricsAnalyzer_analyzeWithThreshold(t *testing.T) {
 			got, err := tc.metricsAnalyzer.analyzeWithThreshold(context.Background())
 			assert.Equal(t, tc.wantErr, err != nil)
 			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_compare(t *testing.T) {
+	type args struct {
+		experiment []float64
+		control    []float64
+		deviation  string
+	}
+	testcases := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "no significance",
+			args: args{
+				experiment: []float64{0.1, 0.2, 0.3, 0.4, 0.5},
+				control:    []float64{0.1, 0.2, 0.3, 0.4, 0.5},
+				deviation:  "EITHER",
+			},
+			wantErr: false,
+		},
+		{
+			name: "deviation on high direction as expected",
+			args: args{
+				experiment: []float64{10.1, 10.2, 10.3, 10.4, 10.5},
+				control:    []float64{0.1, 0.2, 0.3, 0.4, 0.5},
+				deviation:  "LOW",
+			},
+			wantErr: false,
+		},
+		{
+			name: "deviation on low direction as expected",
+			args: args{
+				experiment: []float64{0.1, 0.2, 0.3, 0.4, 0.5},
+				control:    []float64{10.1, 10.2, 10.3, 10.4, 10.5},
+				deviation:  "HIGH",
+			},
+			wantErr: false,
+		},
+		{
+			name: "deviation on high direction as unexpected",
+			args: args{
+				experiment: []float64{10.1, 10.2, 10.3, 10.4, 10.5},
+				control:    []float64{0.1, 0.2, 0.3, 0.4, 0.5},
+				deviation:  "HIGH",
+			},
+			wantErr: true,
+		},
+		{
+			name: "deviation on low direction as unexpected",
+			args: args{
+				experiment: []float64{0.1, 0.2, 0.3, 0.4, 0.5},
+				control:    []float64{10.1, 10.2, 10.3, 10.4, 10.5},
+				deviation:  "LOW",
+			},
+			wantErr: true,
+		},
+		{
+			name: "deviation as unexpected",
+			args: args{
+				experiment: []float64{0.1, 0.2, 5.3, 0.2, 0.5},
+				control:    []float64{0.1, 0.1, 0.1, 0.1, 0.1},
+				deviation:  "EITHER",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := compare(tc.args.experiment, tc.args.control, tc.args.deviation)
+			assert.Equal(t, tc.wantErr, err != nil)
 		})
 	}
 }
