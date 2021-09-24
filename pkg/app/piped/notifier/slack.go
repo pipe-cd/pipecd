accc83265ac921c1fce8631d82bb8fffd3e69d55
Naoto Ono <nono19@students.desu.edu>
2021-09-24 10:54:38 +0900

    Use slice instead of string concatenation
    Co-authored-by: Le Van Nghia <nghialv2607@gmail.com>

--- pkg/app/piped/notifier/slack.go
+++ pkg/app/piped/notifier/slack.go
@@ -173,11 +173,13 @@ func (s *slack) buildSlackMessage(event model.NotificationEvent, webURL string) 
 	case model.NotificationEventType_EVENT_DEPLOYMENT_WAIT_APPROVAL:
 		md := event.Metadata.(*model.NotificationEventDeploymentWaitApproval)
 		title = fmt.Sprintf("Deployment for %q is waiting for an approval", md.Deployment.ApplicationName)
-		approvers := ""
-		for _, v := range md.Approver {
-			approvers += " <@" + v + ">"
+                 if len(md.Approvers) > 0 {
+		    approvers := make([]string, 0, len(md.Approvers))
+		    for _, a := range md.Approvers {
+			  approvers = append(approvers, fmt.Sprintf("<@%s>", a))
+		    }
+		    text = fmt.Sprintf("Approvable users: %s", strings.Join(approvers, " "))
 		}
-		text = fmt.Sprintf("Approval is required from %s", approvers)
 		generateDeploymentEventData(md.Deployment, md.EnvName)
 
 	case model.NotificationEventType_EVENT_DEPLOYMENT_SUCCEEDED:
