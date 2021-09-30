64189aff489fed6625215cec3fdc32e57ecc4e01
小野 直人 <nono19@students.desu.edu>
2021-09-30 14:19:10 +0900

    Add id field in Piped notification

--- pkg/app/piped/notifier/notifier.go
+++ pkg/app/piped/notifier/notifier.go
@@ -102,6 +102,7 @@ func (n *Notifier) Run(ctx context.Context) error {
 	n.Notify(model.NotificationEvent{
 		Type: model.NotificationEventType_EVENT_PIPED_STARTED,
 		Metadata: &model.NotificationEventPipedStarted{
+			Id:      n.config.PipedID,
 			Name:    n.config.Name,
 			Version: version.Get().Version,
 		},
@@ -117,6 +118,7 @@ func (n *Notifier) Run(ctx context.Context) error {
 	n.Notify(model.NotificationEvent{
 		Type: model.NotificationEventType_EVENT_PIPED_STOPPED,
 		Metadata: &model.NotificationEventPipedStopped{
+			Id:      n.config.PipedID,
 			Name:    n.config.Name,
 			Version: version.Get().Version,
 		},
