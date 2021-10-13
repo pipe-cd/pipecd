cc1310590c28fb2a8eeaf3a589a3471466cd5a1c
Le Van Nghia <nghialv2607@gmail.com>
2021-10-13 17:33:29 +0900

    Update docs/assets/js/navbar-version-selector.js
    Co-authored-by: Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>

--- docs/assets/js/navbar-version-selector.js
+++ docs/assets/js/navbar-version-selector.js
@@ -5,7 +5,7 @@
         init: function () {
             $(document).ready(function () {
                 const paths = window.location.pathname.split("/").filter(p => p)
-                if (paths.length == 0) {
+                if (paths.length === 0) {
                     return
                 }
                 const version = paths[0].replace('docs', '').replace("-", '');
