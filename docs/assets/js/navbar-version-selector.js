76c0d39387f36fc5a41ec8f16f297c3d59f399cd
khanhtc1202 <khanhtc1202@gmail.com>
2021-10-14 02:09:56 +0900

    Fix wrong version selector value

--- docs/assets/js/navbar-version-selector.js
+++ docs/assets/js/navbar-version-selector.js
@@ -4,7 +4,7 @@
     var Selector = {
         init: function () {
             $(document).ready(function () {
-                const paths = window.location.pathname.split("/").filter(p => p)
+                const paths = window.location.pathname.split("/").filter(p => p.startsWith('docs'))
                 if (paths.length === 0) {
                     return
                 }
