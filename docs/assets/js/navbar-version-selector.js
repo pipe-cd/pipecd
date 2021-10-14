a8adc5e5132b9c06dd27742685215d1b38a428a6
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2021-10-14 08:22:42 +0700

    Fix display wrong value for version selector box (#2630)
    **What this PR does / why we need it**:
    
    Fix version selector value 
    
    <img width="1200" alt="Screen Shot 2021-10-14 at 2 00 17" src="https://user-images.githubusercontent.com/32532742/137181066-b89345a0-5116-45b6-b392-334879bb4df1.png">
    
    **Which issue(s) this PR fixes**:
    
    Follow #2628 
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- docs/assets/js/navbar-version-selector.js
+++ docs/assets/js/navbar-version-selector.js
@@ -4,11 +4,11 @@
     var Selector = {
         init: function () {
             $(document).ready(function () {
-                const paths = window.location.pathname.split("/").filter(p => p)
+                const paths = window.location.pathname.split("/").filter(p => p.startsWith('docs-'))
                 if (paths.length === 0) {
                     return
                 }
-                const version = paths[0].replace('docs', '').replace("-", '');
+                const version = paths[0].replace('docs-', '');
                 if (version) {
                     $('.navbar-version-menu')[0].text = version;
                 };
