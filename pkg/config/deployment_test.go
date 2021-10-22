f1ec0481f5304b12d42015c1f950308501123d48
Naoto Ono <nono19@students.desu.edu>
2021-10-22 12:09:42 +0900

    Fix bug that NotificationMention is not validated and add some test cases (#2675)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/config/deployment_test.go
+++ pkg/config/deployment_test.go
@@ -71,3 +71,41 @@ func TestHasStage(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateMentions(t *testing.T) {
+	testcases := []struct {
+		name    string
+		event   string
+		slack   []string
+		wantErr bool
+	}{
+		{
+			name:    "valid",
+			event:   "DEPLOYMENT_TRIGGERED",
+			slack:   []string{"user-1", "user-2"},
+			wantErr: false,
+		},
+		{
+			name:    "invalid because of non-existent event",
+			event:   "event-1",
+			slack:   []string{"user-1", "user-2"},
+			wantErr: true,
+		},
+		{
+			name:    "invalid because of missing event",
+			event:   "",
+			slack:   []string{"user-1", "user-2"},
+			wantErr: true,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			m := &NotificationMention{
+				Event: tc.event,
+				Slack: tc.slack,
+			}
+			err := m.Validate()
+			assert.Equal(t, tc.wantErr, err != nil)
+		})
+	}
+}
