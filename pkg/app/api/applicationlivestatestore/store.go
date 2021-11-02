795eb16a656245ea17088dea303d6b759eda63d1
nakabonne <ryo@nakao.dev>
2021-11-02 16:12:09 +0900

    Remove warning

--- pkg/app/api/applicationlivestatestore/store.go
+++ pkg/app/api/applicationlivestatestore/store.go
@@ -63,14 +63,9 @@ func (s *store) GetStateSnapshot(ctx context.Context, applicationID string) (*mo
 
 	fileResp, err := s.backend.Get(ctx, applicationID)
 	if err != nil {
-		if errors.Is(err, filestore.ErrNotFound) {
-			s.logger.Warn("application live state is not found",
-				zap.String("app-id", applicationID),
-				zap.Error(err),
-			)
-		} else {
+		if !errors.Is(err, filestore.ErrNotFound) {
 			s.logger.Error("failed to get application live state from filestore",
-				zap.String("app-id", applicationID),
+				zap.String("application-id", applicationID),
 				zap.Error(err),
 			)
 		}
