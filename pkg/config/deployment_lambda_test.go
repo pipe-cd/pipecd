a5b256e6013b4f62f91c0bfc52711721bd030114
khanhtc1202 <khanhtc1202@gmail.com>
2021-11-08 19:46:24 +0900

    Revise lambda application configuration test data

--- pkg/config/deployment_lambda_test.go
+++ pkg/config/deployment_lambda_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pipe-cd/pipe/pkg/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -37,6 +38,79 @@ func TestLambdaDeploymentConfig(t *testing.T) {
 			expectedSpec: &LambdaDeploymentSpec{
 				GenericDeploymentSpec: GenericDeploymentSpec{
 					Timeout: Duration(6 * time.Hour),
+				},
+				Input: LambdaDeploymentInput{
+					FunctionManifestFile: "function.yaml",
+					AutoRollback:         true,
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			fileName:           "testdata/application/lambda-app-canary.yaml",
+			expectedKind:       KindLambdaApp,
+			expectedAPIVersion: "pipecd.dev/v1beta1",
+			expectedSpec: &LambdaDeploymentSpec{
+				GenericDeploymentSpec: GenericDeploymentSpec{
+					Timeout: Duration(6 * time.Hour),
+					Pipeline: &DeploymentPipeline{
+						Stages: []PipelineStage{
+							{
+								Name:                            model.StageLambdaCanaryRollout,
+								LambdaCanaryRolloutStageOptions: &LambdaCanaryRolloutStageOptions{},
+							},
+							{
+								Name: model.StageLambdaPromote,
+								LambdaPromoteStageOptions: &LambdaPromoteStageOptions{
+									Percent: Percentage{
+										Number:    10,
+										HasSuffix: false,
+									},
+								},
+							},
+							{
+								Name: model.StageLambdaPromote,
+								LambdaPromoteStageOptions: &LambdaPromoteStageOptions{
+									Percent: Percentage{
+										Number:    100,
+										HasSuffix: false,
+									},
+								},
+							},
+						},
+					},
+				},
+				Input: LambdaDeploymentInput{
+					FunctionManifestFile: "function.yaml",
+					AutoRollback:         true,
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			fileName:           "testdata/application/lambda-app-bluegreen.yaml",
+			expectedKind:       KindLambdaApp,
+			expectedAPIVersion: "pipecd.dev/v1beta1",
+			expectedSpec: &LambdaDeploymentSpec{
+				GenericDeploymentSpec: GenericDeploymentSpec{
+					Timeout: Duration(6 * time.Hour),
+					Pipeline: &DeploymentPipeline{
+						Stages: []PipelineStage{
+							{
+								Name:                            model.StageLambdaCanaryRollout,
+								LambdaCanaryRolloutStageOptions: &LambdaCanaryRolloutStageOptions{},
+							},
+							{
+								Name: model.StageLambdaPromote,
+								LambdaPromoteStageOptions: &LambdaPromoteStageOptions{
+									Percent: Percentage{
+										Number:    100,
+										HasSuffix: false,
+									},
+								},
+							},
+						},
+					},
 				},
 				Input: LambdaDeploymentInput{
 					FunctionManifestFile: "function.yaml",
