517c4882fcda918108b80785b3e83e17005fdcbb
Naoto Ono <nono19@students.desu.edu>
2021-11-25 12:42:09 +0900

    Fix small part in state.go (#2841)
    **What this PR does / why we need it**:
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/piped/cloudprovider/kubernetes/state.go
+++ pkg/app/piped/cloudprovider/kubernetes/state.go
@@ -355,6 +355,9 @@ func determineJobHealth(obj *unstructured.Unstructured) (status model.Kubernetes
 			completed = true
 			message = condition.Message
 		}
+		if failed {
+			break
+		}
 	}
 
 	switch {
@@ -450,7 +453,6 @@ func determineIngressHealth(obj *unstructured.Unstructured) (status model.Kubern
 	status = model.KubernetesResourceState_OTHER
 	desc = fmt.Sprintf("Unexpected error while calculating: unable to convert %T to neither %T, %T, nor %T: %v", obj, v1Ingress, betaIngress, extensionIngress, err)
 	return
-
 }
 
 func determineServiceHealth(obj *unstructured.Unstructured) (status model.KubernetesResourceState_HealthStatus, desc string) {
@@ -499,12 +501,11 @@ func determinePersistentVolumeHealth(obj *unstructured.Unstructured) (status mod
 	case corev1.VolumeBound, corev1.VolumeAvailable:
 		status = model.KubernetesResourceState_HEALTHY
 		desc = pv.Status.Message
-		return
 	default:
 		status = model.KubernetesResourceState_OTHER
 		desc = pv.Status.Message
-		return
 	}
+	return
 }
 
 func determinePVCHealth(obj *unstructured.Unstructured) (status model.KubernetesResourceState_HealthStatus, desc string) {
