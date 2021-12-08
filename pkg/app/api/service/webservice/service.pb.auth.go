2385a2e2da8effe605e5fa1f159b26365d0eb435
Ryo Nakao <ryo@nakao.dev>
2021-12-08 13:12:09 +0900

    Allow Viewer to call ListUnregisteredApplications (#2903)
    **What this PR does / why we need it**:
    Without it, no one can add an application from Git.
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/api/service/webservice/service.pb.auth.go
+++ pkg/app/api/service/webservice/service.pb.auth.go
@@ -130,6 +130,8 @@ func (a *authorizer) Authorize(method string, r model.Role) bool {
 		return isAdmin(r) || isEditor(r) || isViewer(r)
 	case "/pipe.api.service.webservice.WebService/GetInsightApplicationCount":
 		return isAdmin(r) || isEditor(r) || isViewer(r)
+	case "/pipe.api.service.webservice.WebService/ListUnregisteredApplications":
+		return isAdmin(r) || isEditor(r) || isViewer(r)
 	}
 
 	return false
