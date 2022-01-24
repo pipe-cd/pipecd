b850e8ae6fd5a51545ccd093b5d8e3d2d16d7673
knanao <50069775+knanao@users.noreply.github.com>
2022-01-24 14:15:40 +0900

    Add labels getter method for ServiceManifest (#3098)
    **What this PR does / why we need it**:
    - Implement a Labels() Getter to retrieve the appID.
    - Add service manifes test
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- pkg/app/piped/cloudprovider/cloudrun/servicemanifest_test.go
+++ pkg/app/piped/cloudprovider/cloudrun/servicemanifest_test.go
@@ -13,3 +13,242 @@
 // limitations under the License.
 
 package cloudrun
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const manifest = `
+apiVersion: serving.knative.dev/v1
+kind: Service
+metadata:
+  name: helloworld
+  labels:
+    cloud.googleapis.com/location: asia-northeast1
+    pipecd-dev-managed-by: piped
+  annotations:
+    run.googleapis.com/ingress: all
+    run.googleapis.com/ingress-status: all
+spec:
+  template:
+    metadata:
+      name: helloworld-v010-1234567
+      annotations:
+        autoscaling.knative.dev/maxScale: '1'
+    spec:
+      containerConcurrency: 80
+      timeoutSeconds: 300
+      containers:
+      - image: gcr.io/pipecd/helloworld:v0.1.0
+        args:
+        - server
+        ports:
+        - name: http1
+          containerPort: 9085
+        resources:
+          limits:
+            cpu: 1000m
+            memory: 128Mi
+  traffic:
+  - revisionName: helloworld-v010-1234567
+    percent: 100
+`
+
+func TestServiceManifest(t *testing.T) {
+	sm, err := ParseServiceManifest([]byte(manifest))
+	require.NoError(t, err)
+	require.NotEmpty(t, sm)
+
+	// SetRevision
+	err = sm.SetRevision("helloworld-v010-1234567")
+	require.NoError(t, err)
+
+	// UpdateTraffic
+	traffics := []RevisionTraffic{
+		{
+			RevisionName: "helloworld-v010-1234567",
+			Percent:      50,
+		},
+		{
+			RevisionName: "helloworld-v011-2345678",
+			Percent:      50,
+		},
+	}
+	err = sm.UpdateTraffic(traffics)
+	require.NoError(t, err)
+
+	// YamlBytes
+	data, err := sm.YamlBytes()
+	require.NoError(t, err)
+	require.NotEmpty(t, data)
+
+	// AddLabels
+	labels := map[string]string{
+		LabelPiped:       "hoge",
+		LabelApplication: "foo",
+	}
+	sm.AddLabels(labels)
+
+	// Labels
+	require.Len(t, sm.Labels(), 4)
+}
+
+func TestLoadServiceManifest(t *testing.T) {
+	// Success
+	sm, err := loadServiceManifest("testdata/new_manifest.yaml")
+	require.NoError(t, err)
+	require.NotEmpty(t, sm)
+
+	// Failure
+	_, err = loadServiceManifest("testdata/not_found")
+	require.Error(t, err)
+}
+
+func TestParseServiceManifest(t *testing.T) {
+	// Success
+	data := []byte(manifest)
+	sm, err := ParseServiceManifest(data)
+	require.NoError(t, err)
+	require.Equal(t, "helloworld", sm.Name)
+
+	// Failure
+	data = []byte("error")
+	_, err = ParseServiceManifest(data)
+	require.Error(t, err)
+}
+
+func TestDecideRevisionName(t *testing.T) {
+	data := []byte(manifest)
+	sm, err := ParseServiceManifest(data)
+	require.NoError(t, err)
+
+	name, err := DecideRevisionName(sm, "12345678912345678")
+	require.NoError(t, err)
+	require.Equal(t, "helloworld-v010-1234567", name)
+}
+
+func TestFindImageTag(t *testing.T) {
+	testcases := []struct {
+		name     string
+		manifest string
+		want     string
+		wantErr  bool
+	}{
+		{
+			name: "ok",
+			manifest: `
+apiVersion: serving.knative.dev/v1
+kind: Service
+metadata:
+  name: helloworld
+  labels:
+    cloud.googleapis.com/location: asia-northeast1
+    pipecd-dev-managed-by: piped
+  annotations:
+    run.googleapis.com/ingress: all
+    run.googleapis.com/ingress-status: all
+spec:
+  template:
+    metadata:
+      name: helloworld-v010-1234567
+      annotations:
+        autoscaling.knative.dev/maxScale: '1'
+    spec:
+      containerConcurrency: 80
+      timeoutSeconds: 300
+      containers:
+      - image: gcr.io/pipecd/helloworld:v0.1.0
+        args:
+        - server
+        ports:
+        - name: http1
+          containerPort: 9085
+        resources:
+          limits:
+            cpu: 1000m
+            memory: 128Mi
+  traffic:
+  - revisionName: helloworld-v010-1234567
+    percent: 100
+`,
+			want:    "v0.1.0",
+			wantErr: false,
+		},
+		{
+			name: "err: containers missing",
+			manifest: `
+apiVersion: serving.knative.dev/v1
+kind: Service
+metadata:
+  name: helloworld
+spec:
+  template:
+    metadata:
+      name: helloworld-v010-1234567
+      annotations:
+        autoscaling.knative.dev/maxScale: '1'
+    spec:
+      containerConcurrency: 80
+      timeoutSeconds: 300
+`,
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "err: image missing",
+			manifest: `
+apiVersion: serving.knative.dev/v1
+kind: Service
+metadata:
+  name: helloworld
+  labels:
+    cloud.googleapis.com/location: asia-northeast1
+    pipecd-dev-managed-by: piped
+  annotations:
+    run.googleapis.com/ingress: all
+    run.googleapis.com/ingress-status: all
+spec:
+  template:
+    metadata:
+      name: helloworld-v010-1234567
+      annotations:
+        autoscaling.knative.dev/maxScale: '1'
+    spec:
+      containerConcurrency: 80
+      timeoutSeconds: 300
+      containers:
+      - args:
+        - server
+        ports:
+        - name: http1
+          containerPort: 9085
+        resources:
+          limits:
+            cpu: 1000m
+            memory: 128Mi
+  traffic:
+  - revisionName: helloworld-v010-1234567
+    percent: 100
+`,
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			data := []byte(tc.manifest)
+			sm, err := ParseServiceManifest(data)
+			require.NoError(t, err)
+
+			got, err := FindImageTag(sm)
+			if tc.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
