24c1ee449022d90d7455b7eb445d3b96ed68fdd8
nghialv <nghialv2607@gmail.com>
2022-01-27 15:50:32 +0900

    Fix wrong repository to push binaries by asset-publisher

--- dockers/asset-publisher/main.go
+++ dockers/asset-publisher/main.go
@@ -30,7 +30,7 @@ import (
 
 var (
 	org             = flag.String("org", "pipe-cd", "The name of GitHub organization")
-	repo            = flag.String("repo", "pipe", "The name of GitHub repository")
+	repo            = flag.String("repo", "pipecd", "The name of GitHub repository")
 	releaseTag      = flag.String("release-tag", "", "The release tag where asset should be uploaded to")
 	assetNameSuffix = flag.String("asset-name-suffix", "", "The suffix name of the asset")
 	assetFile       = flag.String("asset-file", "", "The path to the asset file")
