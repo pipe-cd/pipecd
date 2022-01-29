7ac58da739e03b2877b9397a534ebdfc0c048aa7
khanhtc1202 <khanhtc1202@gmail.com>
2022-01-29 22:13:32 +0900

    Add filedb package

---
+++ pkg/datastore/filedb/filedb.go
@@ -0,0 +1,69 @@
+// Copyright 2022 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filedb
+
+import (
+	"context"
+
+	"github.com/pipe-cd/pipecd/pkg/datastore"
+	"github.com/pipe-cd/pipecd/pkg/filestore"
+	"go.uber.org/zap"
+)
+
+type FileDB struct {
+	backend filestore.Store
+	logger  *zap.Logger
+}
+
+type Option func(*FileDB)
+
+func WithLogger(logger *zap.Logger) Option {
+	return func(m *FileDB) {
+		m.logger = logger
+	}
+}
+
+func NewFileDB(fs filestore.Store, opts ...Option) (*FileDB, error) {
+	fd := &FileDB{
+		logger: zap.NewNop(),
+	}
+	for _, opt := range opts {
+		opt(fd)
+	}
+
+	fd.backend = fs
+
+	return fd, nil
+}
+
+func (f *FileDB) Find(ctx context.Context, col datastore.Collection, opts datastore.ListOptions) (datastore.Iterator, error) {
+	return nil, datastore.ErrUnimplemented
+}
+
+func (f *FileDB) Get(ctx context.Context, col datastore.Collection, id string, v interface{}) error {
+	return datastore.ErrUnimplemented
+}
+
+func (f *FileDB) Create(ctx context.Context, col datastore.Collection, id string, entity interface{}) error {
+	return datastore.ErrUnimplemented
+}
+
+func (f *FileDB) Update(ctx context.Context, col datastore.Collection, id string, updater datastore.Updater) error {
+	return datastore.ErrUnimplemented
+}
+
+func (f *FileDB) Close() error {
+	return f.backend.Close()
+}
