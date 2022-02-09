b9e4d1fbc89a643d3564c504e84e82a687569863
knanao <nao.7ken@gmail.com>
2022-02-09 15:22:14 +0900

    Chore to make gazelle

---
+++ pkg/app/piped/cloudprovider/cloudrun/state.go
@@ -0,0 +1,81 @@
+// Copyright 2022 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudrun
+
+import (
+	"sort"
+	"time"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/pipe-cd/pipecd/pkg/model"
+)
+
+func MakeResourceStates(svc *Service, revs []*Revision, now time.Time) []*model.CloudRunResourceState {
+	states := make([]*model.CloudRunResourceState, 0, len(revs)+1)
+
+	// Set service state.
+	sm, err := svc.ServiceManifest()
+	if err == nil {
+		status, desc := svc.HealthStatus()
+		states = append(states, makeResourceState(sm.u, status, desc, now))
+	}
+
+	// Set active revision states.
+	for _, r := range revs {
+		rm, err := r.RevisionManifest()
+		if err != nil {
+			continue
+		}
+
+		var (
+			status, desc = r.HealthStatus()
+			state        = makeResourceState(rm.u, status, desc, now)
+		)
+		states = append(states, state)
+	}
+	return states
+}
+
+func makeResourceState(obj *unstructured.Unstructured, status model.CloudRunResourceState_HealthStatus, desc string, now time.Time) *model.CloudRunResourceState {
+	var (
+		owners       = obj.GetOwnerReferences()
+		ownerIDs     = make([]string, 0, len(owners))
+		creationTime = obj.GetCreationTimestamp()
+	)
+
+	for _, owner := range owners {
+		ownerIDs = append(ownerIDs, string(owner.UID))
+	}
+	sort.Strings(ownerIDs)
+
+	state := &model.CloudRunResourceState{
+		Id:         string(obj.GetUID()),
+		OwnerIds:   ownerIDs,
+		ParentIds:  ownerIDs,
+		Name:       obj.GetName(),
+		ApiVersion: obj.GetAPIVersion(),
+		Kind:       obj.GetKind(),
+		Namespace:  obj.GetNamespace(),
+
+		HealthStatus:      status,
+		HealthDescription: desc,
+
+		CreatedAt: creationTime.Unix(),
+		UpdatedAt: now.Unix(),
+	}
+
+	return state
+}
