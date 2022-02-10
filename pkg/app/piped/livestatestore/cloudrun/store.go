96c44ec1031c203cf6d2d8f93b23372753a33460
knanao <nao.7ken@gmail.com>
2022-02-10 16:20:49 +0900

    Add application state into livestatestore

--- pkg/app/piped/livestatestore/cloudrun/store.go
+++ pkg/app/piped/livestatestore/cloudrun/store.go
@@ -18,10 +18,12 @@ import (
 	"context"
 	"fmt"
 	"sync/atomic"
+	"time"
 
 	"go.uber.org/zap"
 
 	provider "github.com/pipe-cd/pipecd/pkg/app/piped/cloudprovider/cloudrun"
+	"github.com/pipe-cd/pipecd/pkg/model"
 )
 
 type store struct {
@@ -30,10 +32,17 @@ type store struct {
 	client provider.Client
 }
 
+type app struct {
+	service provider.ServiceManifest
+	// The states of service and all its active revsions which may handle the traffic.
+	states  []*model.CloudRunResourceState
+	version model.ApplicationLiveStateVersion
+}
+
 func (s *store) run(ctx context.Context) error {
 	const maxLimit = 500
 	var cursor string
-	svc := make([]*provider.Service, 0, maxLimit)
+	svcs := make([]*provider.Service, 0, maxLimit)
 	for {
 		ops := &provider.ListOptions{
 			Limit:         maxLimit,
@@ -46,52 +55,115 @@ func (s *store) run(ctx context.Context) error {
 		if err != nil {
 			return fmt.Errorf("failed to list cloudrun services: %w", err)
 		}
-		svc = append(svc, v...)
+		svcs = append(svcs, v...)
 		if next == "" {
 			break
 		}
 		cursor = next
 	}
 
+	revs := make(map[string][]*provider.Revision, len(svcs))
+	for i := range svcs {
+		id, ok := svcs[i].UID()
+		if !ok {
+			continue
+		}
+		names := svcs[i].ActiveRevisionNames()
+		if rs := s.getMultiRevisions(ctx, names); len(rs) > 0 {
+			revs[id] = rs
+		}
+	}
+
 	// Update apps to the latest.
-	s.setApps(svc)
+	s.setApps(ctx, svcs, revs)
 
 	return nil
 }
 
-func (s *store) setApps(svc []*provider.Service) {
-	apps := make(map[string]provider.ServiceManifest, len(svc))
-	for i := range svc {
-		sm, err := svc[i].ServiceManifest()
+func (s *store) setApps(ctx context.Context, svcs []*provider.Service, revs map[string][]*provider.Revision) {
+	apps := make(map[string]*app, len(svcs))
+	for i := range svcs {
+		sm, err := svcs[i].ServiceManifest()
 		if err != nil {
 			s.logger.Error("failed to load cloudrun service into service manifest", zap.Error(err))
 			continue
 		}
-		appID := sm.Labels()[provider.LabelApplication]
-		apps[appID] = sm
+
+		appID, ok := sm.AppID()
+		if !ok {
+			continue
+		}
+
+		id, _ := svcs[i].UID()
+		rs, ok := revs[id]
+		if !ok {
+			apps[appID] = &app{service: sm}
+			continue
+		}
+
+		now := time.Now()
+		apps[appID] = &app{
+			service: sm,
+			states:  provider.MakeResourceStates(svcs[i], rs, now),
+			version: model.ApplicationLiveStateVersion{
+				Timestamp: now.Unix(),
+			},
+		}
 	}
 	s.apps.Store(apps)
 }
 
-func (s *store) loadApps() map[string]provider.ServiceManifest {
+func (s *store) getMultiRevisions(ctx context.Context, names []string) []*provider.Revision {
+	ret := make([]*provider.Revision, len(names))
+	for i := range names {
+		v, err := s.client.GetRevision(ctx, names[i])
+		if err != nil {
+			s.logger.Error("failed to get cloudrun revision", zap.Error(err))
+			continue
+		}
+		ret = append(ret, v)
+	}
+	return ret
+}
+
+func (s *store) loadApps() map[string]*app {
 	apps := s.apps.Load()
 	if apps == nil {
 		return nil
 	}
-	return apps.(map[string]provider.ServiceManifest)
+	return apps.(map[string]*app)
 }
 
-func (s *store) GetServiceManifest(appID string) (provider.ServiceManifest, bool) {
+func (s *store) getServiceManifest(appID string) (provider.ServiceManifest, bool) {
 	apps := s.loadApps()
 	if apps == nil {
 		s.logger.Error("failed to load cloudrun apps")
 		return provider.ServiceManifest{}, false
 	}
 
-	sm, ok := apps[appID]
+	app, ok := apps[appID]
 	if !ok {
 		return provider.ServiceManifest{}, false
 	}
 
-	return sm, true
+	return app.service, true
+}
+
+func (s *store) getState(appID string) (State, bool) {
+	apps := s.loadApps()
+	if apps == nil {
+		s.logger.Error("failed to load cloudrun apps")
+		return State{}, false
+	}
+
+	app, ok := apps[appID]
+	if !ok {
+		return State{}, false
+	}
+
+	state := State{
+		Resources: app.states,
+		Version:   app.version,
+	}
+	return state, true
 }
