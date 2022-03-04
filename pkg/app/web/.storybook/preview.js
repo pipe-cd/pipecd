9a9f0c38bae77c5b08b735ae95c46af918cc95f4
nghialv <nghialv2607@gmail.com>
2022-03-04 14:15:51 +0900

    Remove storybook from web code

--- pkg/app/web/.storybook/preview.js
+++
@@ -1,7 +0,0 @@
-import { addDecorator } from "@storybook/react";
-import { ThemeDecorator } from "./ThemeDecorator";
-import { setupDayjs } from "../src/utils/setup-dayjs";
-
-setupDayjs();
-
-addDecorator(ThemeDecorator);
--- pkg/app/web/.storybook/redux-decorator.tsx
+++
@@ -1,8 +0,0 @@
-import { DeepPartial } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import type { AppState } from "../src/store";
-import { createStore } from "test-utils";
-
-export const createDecoratorRedux = (initialState: DeepPartial<AppState>) => (
-  storyFn: any
-) => <Provider store={createStore(initialState)}>{storyFn()}</Provider>;
