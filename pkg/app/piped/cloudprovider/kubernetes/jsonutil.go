a35ad8b03f1e615cf18885695ec97a5f58a3ebc3
Hosshii <sao_heath6147.wistre@icloud.com>
2022-03-09 16:22:53 +0900

    add license

--- pkg/app/piped/cloudprovider/kubernetes/jsonutil.go
+++ pkg/app/piped/cloudprovider/kubernetes/jsonutil.go
@@ -1,3 +1,17 @@
+// Copyright 2022 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package kubernetes
 
 // This file is copied from argocd/gitops-engine and modified
@@ -10,16 +24,14 @@ func removeFields(config, live interface{}) interface{} {
 		l, ok := live.(map[string]interface{})
 		if ok {
 			return removeMapFields(c, l)
-		} else {
-			return live
 		}
+		return live
 	case []interface{}:
 		l, ok := live.([]interface{})
 		if ok {
 			return removeListFields(c, l)
-		} else {
-			return live
 		}
+		return live
 	default:
 		return live
 	}
