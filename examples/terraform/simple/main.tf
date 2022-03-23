868a5498594b5beb4d58207f0a7daa9a13f5c0f4
nghialv <nghialv2607@gmail.com>
2022-03-23 15:12:34 +0900

    Update terraform example

--- examples/terraform/simple/main.tf
+++ examples/terraform/simple/main.tf
@@ -1,22 +1,21 @@
+terraform {
+  backend "gcs" {
+    bucket      = "pipecd-play-terraform-examples-backend"
+    prefix      = "simple"
+    credentials = ".credentials/service-account.json"
+  }
+}
+
 variable "project" {}
+variable "content" {}
 
 provider "google" {
   project     = var.project
   credentials = ".credentials/service-account.json"
 }
 
-terraform {
-  backend "gcs" {
-    bucket      = "pipecd-terraform-examples"
-    prefix      = "tfstates/simple"
-    credentials = ".credentials/service-account.json"
-  }
-}
-
-variable "content" {}
-
 resource "google_storage_bucket_object" "object" {
-  name    = "examples/simple/${terraform.workspace}.txt"
-  bucket  = "pipecd-terraform-examples"
+  name    = "simple/${terraform.workspace}.txt"
+  bucket  = "pipecd-play-terraform-examples"
   content = var.content
 }
