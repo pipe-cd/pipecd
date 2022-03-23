e69680a0c865c15eb8e0e1f2eab0b3a7e0431dc3
Hosshii <sao_heath6147.wistre@icloud.com>
2022-03-23 15:29:18 +0900

    use new proto type in getinsightData

--- web/src/api/insight.ts
+++ web/src/api/insight.ts
@@ -1,28 +1,26 @@
 import { apiClient, apiRequest } from "./client";
 import {
-  // GetInsightDataRequest,
-  // GetInsightDataResponse,
+  GetInsightDataRequest,
+  GetInsightDataResponse,
   GetInsightApplicationCountRequest,
   GetInsightApplicationCountResponse,
 } from "pipecd/web/api_client/service_pb";
 
-// export const getInsightData = ({
-//   applicationId,
-//   dataPointCount,
-//   metricsKind,
-//   rangeFrom,
-//   step,
-// }: GetInsightDataRequest.AsObject): Promise<
-//   GetInsightDataResponse.AsObject
-// > => {
-//   const req = new GetInsightDataRequest();
-//   req.setApplicationId(applicationId);
-//   req.setDataPointCount(dataPointCount);
-//   req.setMetricsKind(metricsKind);
-//   req.setRangeFrom(rangeFrom);
-//   req.setStep(step);
-//   return apiRequest(req, apiClient.getInsightData);
-// };
+export const getInsightData = ({
+  applicationId,
+  metricsKind,
+  rangeFrom,
+  rangeTo,
+}: GetInsightDataRequest.AsObject): Promise<
+  GetInsightDataResponse.AsObject
+> => {
+  const req = new GetInsightDataRequest();
+  req.setApplicationId(applicationId);
+  req.setMetricsKind(metricsKind);
+  req.setRangeFrom(rangeFrom);
+  req.setRangeTo(rangeTo);
+  return apiRequest(req, apiClient.getInsightData);
+};
 
 export const getApplicationCount = (): Promise<
   GetInsightApplicationCountResponse.AsObject
