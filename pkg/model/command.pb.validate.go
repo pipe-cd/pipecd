070f9958d1316d0266c295c8697e423b6c4c48a0
knanao <nao.7ken@gmail.com>
2022-04-07 15:26:16 +0900

    Add new command type and stage status for skipping stage

--- pkg/model/command.pb.validate.go
+++ pkg/model/command.pb.validate.go
@@ -288,6 +288,35 @@ func (m *Command) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetSkipStage()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, CommandValidationError{
+					field:  "SkipStage",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, CommandValidationError{
+					field:  "SkipStage",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetSkipStage()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return CommandValidationError{
+				field:  "SkipStage",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if m.GetCreatedAt() <= 0 {
 		err := CommandValidationError{
 			field:  "CreatedAt",
@@ -1155,3 +1184,127 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = Command_ChainSyncApplicationValidationError{}
+
+// Validate checks the field values on Command_SkipStage with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *Command_SkipStage) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Command_SkipStage with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// Command_SkipStageMultiError, or nil if none found.
+func (m *Command_SkipStage) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Command_SkipStage) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if utf8.RuneCountInString(m.GetDeploymentId()) < 1 {
+		err := Command_SkipStageValidationError{
+			field:  "DeploymentId",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if utf8.RuneCountInString(m.GetStageId()) < 1 {
+		err := Command_SkipStageValidationError{
+			field:  "StageId",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if len(errors) > 0 {
+		return Command_SkipStageMultiError(errors)
+	}
+
+	return nil
+}
+
+// Command_SkipStageMultiError is an error wrapping multiple validation errors
+// returned by Command_SkipStage.ValidateAll() if the designated constraints
+// aren't met.
+type Command_SkipStageMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m Command_SkipStageMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m Command_SkipStageMultiError) AllErrors() []error { return m }
+
+// Command_SkipStageValidationError is the validation error returned by
+// Command_SkipStage.Validate if the designated constraints aren't met.
+type Command_SkipStageValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e Command_SkipStageValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e Command_SkipStageValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e Command_SkipStageValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e Command_SkipStageValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e Command_SkipStageValidationError) ErrorName() string {
+	return "Command_SkipStageValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e Command_SkipStageValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sCommand_SkipStage.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = Command_SkipStageValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = Command_SkipStageValidationError{}
