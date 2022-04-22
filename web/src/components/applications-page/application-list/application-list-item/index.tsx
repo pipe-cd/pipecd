98287dcd2bc6b076a793ce42d911598552704126
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2022-04-22 10:42:15 +0700

    Limit height for possible multiple rows column on application list page (#3567)
    **What this PR does / why we need it**:
    
    https://user-images.githubusercontent.com/32532742/164584887-6675527b-4165-405f-9f73-6d646a821881.mp4
    
    **Which issue(s) this PR fixes**:
    
    Fixes #
    
    **Does this PR introduce a user-facing change?**:
    <!--
    If no, just write "NONE" in the release-note block below.
    -->
    ```release-note
    NONE
    ```
    
    This PR was merged by Kapetanios.

--- web/src/components/applications-page/application-list/application-list-item/index.tsx
+++ web/src/components/applications-page/application-list/application-list-item/index.tsx
@@ -30,8 +30,14 @@ const useStyles = makeStyles((theme) => ({
   disabled: {
     background: theme.palette.grey[200],
   },
+  labels: {
+    maxHeight: 200,
+    overflowY: "scroll",
+  },
   version: {
     maxWidth: 300,
+    maxHeight: 200,
+    overflowY: "scroll",
     wordBreak: "break-word",
   },
   deployedBy: {
@@ -140,56 +146,60 @@ export const ApplicationListItem: FC<ApplicationListItemProps> = memo(
           </TableCell>
           <TableCell>{APPLICATION_KIND_TEXT[app.kind]}</TableCell>
           <TableCell>
-            {app.labelsMap.length !== 0
-              ? app.labelsMap.map(([key, value]) => (
-                  <>
-                    <span>{key + ": " + value}</span>
-                    <br />
-                  </>
-                ))
-              : "-"}
+            <div className={classes.labels}>
+              {app.labelsMap.length !== 0
+                ? app.labelsMap.map(([key, value]) => (
+                    <>
+                      <span>{key + ": " + value}</span>
+                      <br />
+                    </>
+                  ))
+                : "-"}
+            </div>
           </TableCell>
           {recentlyDeployment ? (
             <>
-              <TableCell className={classes.version}>
-                {recentlyDeployment.versionsList.length !== 0 ? (
-                  recentlyDeployment.versionsList.map((v) =>
-                    v.name == "" ? (
-                      <>
-                        <span>{v.version}</span>
-                        <br />
-                      </>
-                    ) : (
-                      <>
-                        <Link
-                          href={v.url.includes("://") ? v.url : `//${v.url}`}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          {/* Trim first 7 characters like commit hash in case it is too long */}
-                          {v.name}:
-                          {v.version.length > 7
-                            ? `${v.version.slice(0, 7)}...`
-                            : v.version}
-                          <OpenInNewIcon className={classes.linkIcon} />
-                        </Link>
-                        <br />
-                      </>
+              <TableCell>
+                <div className={classes.version}>
+                  {recentlyDeployment.versionsList.length !== 0 ? (
+                    recentlyDeployment.versionsList.map((v) =>
+                      v.name === "" ? (
+                        <>
+                          <span>{v.version}</span>
+                          <br />
+                        </>
+                      ) : (
+                        <>
+                          <Link
+                            href={v.url.includes("://") ? v.url : `//${v.url}`}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            {/* Trim first 7 characters like commit hash in case it is too long */}
+                            {v.name}:
+                            {v.version.length > 7
+                              ? `${v.version.slice(0, 7)}...`
+                              : v.version}
+                            <OpenInNewIcon className={classes.linkIcon} />
+                          </Link>
+                          <br />
+                        </>
+                      )
                     )
-                  )
-                ) : recentlyDeployment.version.includes(",") ? (
-                  recentlyDeployment.version
-                    .split(",")
-                    .filter((item, index, arr) => arr.indexOf(item) === index)
-                    .map((v) => (
-                      <>
-                        <span>{v}</span>
-                        <br />
-                      </>
-                    ))
-                ) : (
-                  <span>{recentlyDeployment.version}</span>
-                )}
+                  ) : recentlyDeployment.version.includes(",") ? (
+                    recentlyDeployment.version
+                      .split(",")
+                      .filter((item, index, arr) => arr.indexOf(item) === index)
+                      .map((v) => (
+                        <>
+                          <span>{v}</span>
+                          <br />
+                        </>
+                      ))
+                  ) : (
+                    <span>{recentlyDeployment.version}</span>
+                  )}
+                </div>
               </TableCell>
               {displayAllProperties && (
                 <TableCell>
