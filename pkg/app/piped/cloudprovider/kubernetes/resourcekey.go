955e6361401c9a404b9bee34c575fa3cd74956d3
kondo takeshi <chaspy@users.noreply.github.com>
2022-04-24 15:18:38 +0900

    Support PodDisruptionBudget

--- pkg/app/piped/cloudprovider/kubernetes/resourcekey.go
+++ pkg/app/piped/cloudprovider/kubernetes/resourcekey.go
@@ -77,6 +77,7 @@ const (
 	KindClusterRole              = "ClusterRole"
 	KindClusterRoleBinding       = "ClusterRoleBinding"
 	KindNameSpace                = "NameSpace"
+	KindPodDisruptionBudget      = "PodDisruptionBudget"
 	KindCustomResourceDefinition = "CustomResourceDefinition"
 
 	DefaultNamespace = "default"
