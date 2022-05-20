2a558804113a86f36dc0d2af43400648f8bb4396
Tsubasa Umeuchi <umeuchi_tsubasa@cyberagent.co.jp>
2022-05-20 18:51:36 +0900

    Add read perm

--- tool/piped-base/dockertest/check-helm.sh
+++ tool/piped-base/dockertest/check-helm.sh
@@ -38,13 +38,9 @@ declare -A versioncases
 #versioncases["helm-2.16.7"]="Client: \&version.Version{SemVer:\"v2.16.7\", GitCommit:\"5f2584fd3d35552c4af26036f0c464191287986b\", GitTreeState:\"clean\"}"
 versioncases["helm"]="version.BuildInfo{Version:\"v3.8.2\", GitCommit:\"6e3701edea09e5d55a8ca2aae03a68917630e91b\", GitTreeState:\"clean\", GoVersion:\"go1.17.5\"}"
 
-ls -al /builder/home/.docker
-echo $HOME
-
-
 for h in "${!versioncases[@]}"
 do
-    helm env
+    chmod +r -R $HOME
     got=$($h version --client --registry-config ${HELM_REGISTRY_CONFIG})
     want=${versioncases[$h]}
     if [[ ${got} == ${want} ]]; then
