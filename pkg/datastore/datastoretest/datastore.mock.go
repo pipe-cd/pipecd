c41d0a0afee7f070b16381d3ac2db9f036c01208
Tsubasa Umeuchi <umeuchi_tsubasa@cyberagent.co.jp>
2022-06-01 17:41:28 +0900

    Exec gen/code

--- pkg/datastore/datastoretest/datastore.mock.go
+++ pkg/datastore/datastoretest/datastore.mock.go
@@ -374,6 +374,20 @@ func (mr *MockPipedStoreMockRecorder) List(arg0, arg1 interface{}) *gomock.Call 
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "List", reflect.TypeOf((*MockPipedStore)(nil).List), arg0, arg1)
 }
 
+// RestartPiped mocks base method.
+func (m *MockPipedStore) RestartPiped(arg0 context.Context, arg1 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RestartPiped", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// RestartPiped indicates an expected call of RestartPiped.
+func (mr *MockPipedStoreMockRecorder) RestartPiped(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RestartPiped", reflect.TypeOf((*MockPipedStore)(nil).RestartPiped), arg0, arg1)
+}
+
 // UpdateDesiredVersion mocks base method.
 func (m *MockPipedStore) UpdateDesiredVersion(arg0 context.Context, arg1, arg2 string) error {
 	m.ctrl.T.Helper()
