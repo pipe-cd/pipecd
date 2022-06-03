ef36955a167db967bbe09748895a824eb300dc59
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2022-06-03 11:39:42 +0700

    Revert "Revert "Add feature to show piped config on web console (#3673)" (#3716)"
    This reverts commit 32193625e079d390138390477cc735a22d0eb9f9.

--- web/src/constants/ui-text.ts
+++ web/src/constants/ui-text.ts
@@ -18,3 +18,4 @@ export const UI_TEXT_UPGRADE = "UPGRADE";
 // piped
 export const UI_TEXT_ADD_NEW_KEY = "Add new Key";
 export const UI_TEXT_DELETE_OLD_KEY = "Delete old Key";
+export const UI_TEXT_VIEW_THE_CONFIGURATION = "View the configuration";
