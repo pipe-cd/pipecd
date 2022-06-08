b0932de679688e04269bc7a8f6d4ce2162d3a4ed
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2022-06-08 17:08:41 +0700

    Sort the application suggestion name in application filter form (#3740)

--- web/src/utils/unique-array.test.ts
+++
@@ -1,7 +0,0 @@
-import { uniqueArray } from "./unique-array";
-
-test("uniqueArray", () => {
-  expect(uniqueArray([])).toEqual([]);
-  expect(uniqueArray([1, 2, 3])).toEqual([1, 2, 3]);
-  expect(uniqueArray([1, 2, 2, 3])).toEqual([1, 2, 3]);
-});
--- web/src/utils/unique-array.ts
+++
@@ -1,1 +0,0 @@
-export const uniqueArray = <T>(arr: Array<T>): Array<T> => [...new Set(arr)];
