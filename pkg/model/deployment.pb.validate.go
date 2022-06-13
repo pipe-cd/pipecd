8fce1415411b1d7f8d790b6ec833237f27e4a0cd
khanhtc1202 <khanhtc1202@gmail.com>
2022-06-13 18:15:54 +0700

    Add PlatformProvider to deployment model

--- pkg/model/deployment.pb.validate.go
+++ pkg/model/deployment.pb.validate.go
@@ -163,16 +163,9 @@ func (m *Deployment) validate(all bool) error {
 		}
 	}
 
-	if utf8.RuneCountInString(m.GetCloudProvider()) < 1 {
-		err := DeploymentValidationError{
-			field:  "CloudProvider",
-			reason: "value length must be at least 1 runes",
-		}
-		if !all {
-			return err
-		}
-		errors = append(errors, err)
-	}
+	// no validation rules for CloudProvider
+
+	// no validation rules for PlatformProvider
 
 	// no validation rules for Labels
 
