62cb9ce63411d10a920aa5250a0717b33e9ff45a
khanhtc1202 <khanhtc1202@gmail.com>
2022-08-22 16:00:43 +0900

    Fix unable to click back in login form

--- web/src/components/login-page/login-form/index.tsx
+++ web/src/components/login-page/login-form/index.tsx
@@ -6,6 +6,7 @@ import {
   PAGE_PATH_LOGIN,
 } from "~/constants/path";
 import { MarkGithubIcon } from "@primer/octicons-react";
+import { LOGGING_IN_PROJECT } from "~/constants/localstorage";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -67,6 +68,7 @@ export const LoginForm: FC<LoginFormProps> = memo(function LoginForm({
   const classes = useStyles();
 
   const handleOnBack = (): void => {
+    localStorage.removeItem(LOGGING_IN_PROJECT);
     window.location.href = PAGE_PATH_LOGIN;
   };
 
