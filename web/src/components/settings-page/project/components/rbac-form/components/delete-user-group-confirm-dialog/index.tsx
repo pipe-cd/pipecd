6dd74775c2126b0f28b2910efa9c2d34b9db2498
knanao <nao.7ken@gmail.com>
2022-09-27 16:20:40 +0900

    Fix the project tab to manage a role and a user group for RBAC (#3919)
    * Feat the user-group tab
    
    * Add some toast texts
    
    * Fix RBAC section for the setting page
    
    * Remove the fix option for lint/web
    
    * Fix the table header for User Group
    
    * Fix the design for the project setting page
    
    * Add a delete user group confirm dialog

---
+++ web/src/components/settings-page/project/components/rbac-form/components/delete-user-group-confirm-dialog/index.tsx
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import { FC, memo } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  deleteTargetName: {
+    color: theme.palette.text.primary,
+    fontWeight: theme.typography.fontWeightMedium,
+  },
+  description: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+export interface DeleteUserGroupConfirmDialogProps {
+  ssoGroup: string | null;
+  onCancel: () => void;
+  onDelete: (ssoGroup: string) => void;
+}
+
+const DIALOG_TITLE = "Delete User Group";
+const DESCRIPTION = "Are you sure you want to delete the User Group?";
+
+export const DeleteUserGroupConfirmDialog: FC<DeleteUserGroupConfirmDialogProps> = memo(
+  function DeleteUserGroupConfirmDialog({ ssoGroup, onDelete, onCancel }) {
+    const classes = useStyles();
+
+    return (
+      <Dialog open={Boolean(ssoGroup)}>
+        <DialogTitle>{DIALOG_TITLE}</DialogTitle>
+        <DialogContent>
+          <Alert severity="warning" className={classes.description}>
+            {DESCRIPTION}
+          </Alert>
+          <Typography variant="caption">Group</Typography>
+          <Typography variant="body1" className={classes.deleteTargetName}>
+            {ssoGroup}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onCancel}>Cancel</Button>
+          <Button
+            color="primary"
+            onClick={() => {
+              if (ssoGroup) {
+                onDelete(ssoGroup);
+              }
+            }}
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+);
--- web/src/components/settings-page/project/components/rbac-form/index.tsx
+++ web/src/components/settings-page/project/components/rbac-form/index.tsx
@@ -1,69 +1,14 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  TextField,
-  Typography,
-} from "@material-ui/core";
-import EditIcon from "@material-ui/icons/Edit";
-import Skeleton from "@material-ui/lab/Skeleton/Skeleton";
-import * as React from "react";
-import { FC, memo, useState } from "react";
+import { Typography } from "@material-ui/core";
+import { FC, memo } from "react";
 import { RBAC_DESCRIPTION } from "~/constants/text";
-import { UPDATE_RBAC_SUCCESS } from "~/constants/toast-text";
-import { UI_TEXT_CANCEL, UI_TEXT_SAVE } from "~/constants/ui-text";
-import { useAppDispatch, useAppSelector } from "~/hooks/redux";
-import { fetchProject, Teams, updateRBAC } from "~/modules/project";
-import { addToast } from "~/modules/toasts";
 import { useProjectSettingStyles } from "~/styles/project-setting";
-import { ProjectSettingLabeledText } from "../project-setting-labeled-text";
+import { RoleTable } from "./components/role";
+import { UserGroupTable } from "./components/user-group";
 
 const SECTION_TITLE = "Role-Based Access Control";
-const DIALOG_TITLE = `Edit ${SECTION_TITLE}`;
-const TEAM_LABELS = {
-  ADMIN: "Admin Team",
-  EDITOR: "Editor Team",
-  VIEWER: "Viewer Team",
-};
 
 export const RBACForm: FC = memo(function RBACForm() {
   const projectSettingClasses = useProjectSettingStyles();
-  const teams = useAppSelector<Teams | null | undefined>(
-    (state) => state.project.teams
-  );
-  const dispatch = useAppDispatch();
-  const [isEdit, setIsEdit] = useState(false);
-  const [admin, setAdmin] = useState("");
-  const [editor, setEditor] = useState("");
-  const [viewer, setViewer] = useState("");
-
-  const handleClose = (): void => {
-    setIsEdit(false);
-  };
-  const handleSave = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    dispatch(updateRBAC({ admin, editor, viewer })).then((result) => {
-      if (updateRBAC.fulfilled.match(result)) {
-        dispatch(fetchProject());
-        dispatch(
-          addToast({
-            message: UPDATE_RBAC_SUCCESS,
-            severity: "success",
-          })
-        );
-      }
-    });
-    setIsEdit(false);
-  };
-
-  const isNotModified =
-    !!teams &&
-    teams.admin === admin &&
-    teams.editor === editor &&
-    teams.viewer === viewer;
 
   return (
     <>
@@ -84,94 +29,9 @@ export const RBACForm: FC = memo(function RBACForm() {
         {RBAC_DESCRIPTION}
       </Typography>
 
-      <div className={projectSettingClasses.valuesWrapper}>
-        {teams ? (
-          <>
-            <div className={projectSettingClasses.values}>
-              <ProjectSettingLabeledText
-                label={TEAM_LABELS.ADMIN}
-                value={teams.admin}
-              />
-              <ProjectSettingLabeledText
-                label={TEAM_LABELS.EDITOR}
-                value={teams.editor}
-              />
-              <ProjectSettingLabeledText
-                label={TEAM_LABELS.VIEWER}
-                value={teams.viewer}
-              />
-            </div>
-          </>
-        ) : (
-          <div className={projectSettingClasses.values}>
-            {teams === undefined ? (
-              <>
-                <Skeleton width={200} height={28} />
-                <Skeleton width={200} height={28} />
-                <Skeleton width={200} height={28} />
-              </>
-            ) : (
-              <>
-                <Typography variant="body1" color="textSecondary">
-                  Not Configured
-                </Typography>
-              </>
-            )}
-          </div>
-        )}
-        <div>
-          <IconButton onClick={() => setIsEdit(true)}>
-            <EditIcon />
-          </IconButton>
-        </div>
-      </div>
+      <RoleTable />
 
-      <Dialog
-        open={isEdit}
-        onEnter={() => {
-          setAdmin(teams?.admin ?? "");
-          setEditor(teams?.editor ?? "");
-          setViewer(teams?.viewer ?? "");
-        }}
-        onClose={handleClose}
-      >
-        <form onSubmit={handleSave}>
-          <DialogTitle>{DIALOG_TITLE}</DialogTitle>
-          <DialogContent>
-            <TextField
-              value={admin}
-              variant="outlined"
-              margin="dense"
-              label={TEAM_LABELS.ADMIN}
-              fullWidth
-              autoFocus
-              onChange={(e) => setAdmin(e.currentTarget.value)}
-            />
-            <TextField
-              value={editor}
-              variant="outlined"
-              margin="dense"
-              label={TEAM_LABELS.EDITOR}
-              fullWidth
-              onChange={(e) => setEditor(e.currentTarget.value)}
-            />
-            <TextField
-              value={viewer}
-              variant="outlined"
-              margin="dense"
-              label={TEAM_LABELS.VIEWER}
-              fullWidth
-              onChange={(e) => setViewer(e.currentTarget.value)}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>{UI_TEXT_CANCEL}</Button>
-            <Button type="submit" color="primary" disabled={isNotModified}>
-              {UI_TEXT_SAVE}
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>
+      <UserGroupTable />
     </>
   );
 });
