70a79cdf7dcc97f1eca1979d80c8f3418d663ae5
Tomoki Hori <50762864+TonkyH@users.noreply.github.com>
2022-12-27 16:30:10 +0900

    support standalone ECS task (#4084)
    Fixes https://github.com/pipe-cd/pipecd/issues/2734

--- pkg/app/piped/executor/ecs/ecs.go
+++ pkg/app/piped/executor/ecs/ecs.go
@@ -157,6 +157,34 @@ func applyServiceDefinition(ctx context.Context, cli provider.Client, serviceDef
 	return service, nil
 }
 
+func runStandaloneTask(
+	ctx context.Context,
+	in *executor.Input,
+	cloudProviderName string,
+	cloudProviderCfg *config.PlatformProviderECSConfig,
+	taskDefinition types.TaskDefinition,
+	ecsInput *config.ECSDeploymentInput,
+) bool {
+	client, err := provider.DefaultRegistry().Client(cloudProviderName, cloudProviderCfg, in.Logger)
+	if err != nil {
+		in.LogPersister.Errorf("Unable to create ECS client for the provider %s: %v", cloudProviderName, err)
+		return false
+	}
+
+	err = client.RunTask(
+		ctx,
+		taskDefinition,
+		ecsInput.ClusterArn,
+		ecsInput.LaunchType,
+		&ecsInput.AwsVpcConfiguration,
+	)
+	if err != nil {
+		in.LogPersister.Errorf("Failed to apply ECS task definition: %v", err)
+		return false
+	}
+	return true
+}
+
 func createPrimaryTaskSet(ctx context.Context, client provider.Client, service types.Service, taskDef types.TaskDefinition, targetGroup *types.LoadBalancer) error {
 	// Get current PRIMARY task set.
 	prevPrimaryTaskSet, err := client.GetPrimaryTaskSet(ctx, service)
