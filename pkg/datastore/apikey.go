9fe978896e7d6d323e688e7faea8cb3e242e6cf6
kevin55156 <nambayouhei@keio.jp>
2023-02-17 17:40:32 +0900

    update only enabled

--- pkg/datastore/apikey.go
+++ pkg/datastore/apikey.go
@@ -153,7 +153,9 @@ func (s *apiKeyStore) Disable(ctx context.Context, id, projectID string) error {
 func (s *apiKeyStore) UpdateLastUsedAt(ctx context.Context, id string, time int64) error {
 	return s.ds.Update(ctx, s.col, id, func(e interface{}) error {
 		k := e.(*model.APIKey)
-		k.LastUsedAt = time
+		if !k.Disabled {
+			k.LastUsedAt = time
+		}
 		return k.Validate()
 	})
 }
