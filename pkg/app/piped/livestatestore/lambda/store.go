817e91b8a580abb3f0adc027a75cd382bedaddb3
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2023-02-21 10:18:46 +0700

    Remove cloud-provider word in piped logs (#4205)

--- pkg/app/piped/livestatestore/lambda/store.go
+++ pkg/app/piped/livestatestore/lambda/store.go
@@ -36,7 +36,7 @@ type Getter interface {
 
 func NewStore(cfg *config.PlatformProviderLambdaConfig, platformProvider string, appLister applicationLister, logger *zap.Logger) *Store {
 	logger = logger.Named("lambda").
-		With(zap.String("cloud-provider", platformProvider))
+		With(zap.String("platform-provider", platformProvider))
 
 	return &Store{
 		logger: logger,
