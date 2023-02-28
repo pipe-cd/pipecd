44d2891ad04b6cb3f1fcd0ab30451fb0af4a0e65
funera1 <funeral7776@gmail.com>
2023-02-28 17:07:45 +0900

    Add ability to see invalid config errors from application-detail-page

--- web/src/components/application-detail-page/application-detail/index.tsx
+++ web/src/components/application-detail-page/application-detail/index.tsx
@@ -302,7 +302,7 @@ export const ApplicationDetail: FC<ApplicationDetailProps> = memo(
 
               {app.syncState && (
                 <SyncStateReason
-                  summary={app.syncState.shortReason}
+                  summary={app.syncState.shortReason ? app.syncState.shortReason : app.syncState.reason.slice(100)}
                   detail={app.syncState.reason}
                 />
               )}
