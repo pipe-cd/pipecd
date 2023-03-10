baa846f233e5a817aa04a57c187cdabf72af3614
funera1 <funeral7776@gmail.com>
2023-03-10 18:19:00 +0900

    Add clear button to piped fileter

--- web/src/components/applications-page/application-filter/piped-select.tsx
+++ web/src/components/applications-page/application-filter/piped-select.tsx
@@ -1,14 +1,31 @@
-import { Select, MenuItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { Select, MenuItem, IconButton } from "@material-ui/core";
+import  ClearIcon  from "@material-ui/icons/Clear";
 import { FC, useState } from "react";
 import { useAppSelector } from "~/hooks/redux";
 import { selectAllPipeds } from "~/modules/pipeds";
+import clsx from "clsx";
 
 interface Props {
   onChange: (value: string) => void;
 }
-const ALL_VALUE = "ALL";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "&:hover $clearIndicatorDirty, & .Mui-focused $clearIndicatorDirty": {
+      visibility: "visible"
+    }
+  },
+  clearIndicatorDirty: {},
+  clearIndicator: {
+    visibility: "hidden",
+    right: 20
+  }
+}));
 
 export const PipedSelect: FC<Props> = ({ onChange }) => {
+  const classes = useStyles()
+
   const ps = useAppSelector((state) => selectAllPipeds(state));
   const pipeds = ps
     .filter((piped) => !piped.disabled)
@@ -16,22 +33,35 @@ export const PipedSelect: FC<Props> = ({ onChange }) => {
   const [selectedPipedId, setSelectedPipedId] = useState(
     pipeds.length === 1 ? pipeds[0].id : ""
   );
+
   return (
     <Select
       labelId="filter-piped"
       id="filter-piped"
+      className={classes.root}
       label="Piped"
-      value={selectedPipedId ?? ALL_VALUE}
+      value={selectedPipedId}
       onChange={(e) => {
         setSelectedPipedId(e.target.value as string);
         onChange(
-          e.target.value === ALL_VALUE ? "" : (e.target.value as string)
+          e.target.value as string
         );
       }}
+      endAdornment={
+          <IconButton
+            className={clsx(classes.clearIndicator, {
+              [classes.clearIndicatorDirty]: setSelectedPipedId.length > 0
+            })}
+            size="small"
+            onClick={() => {
+                setSelectedPipedId("");
+                onChange("")
+            }}
+          >
+            <ClearIcon fontSize="small" />
+          </IconButton>
+      }
     >
-      <MenuItem value={ALL_VALUE}>
-        <em>All</em>
-      </MenuItem>
       {pipeds.map((e) => (
         <MenuItem value={e.id} key={`piped-${e.id}`}>
           {e.name} ({e.id})
