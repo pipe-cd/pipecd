ec9bfeaf302ca73de0df8052f03d11d5de012c04
funera1 <funeral7776@gmail.com>
2023-03-27 18:18:45 +0900

    Fix comment and error message

--- pkg/app/piped/driftdetector/kubernetes/detector.go
+++ pkg/app/piped/driftdetector/kubernetes/detector.go
@@ -195,11 +195,11 @@ func (d *detector) checkApplication(ctx context.Context, app *model.Application,
 
 	ignoredPathsOf := make(map[string][]string, 0)
 	if ddCfg != nil {
-		// ResourceKey#ignroePath
 		for _, ignoreField := range ddCfg.IgnoreFields {
+			// ignoreField is 'apiVersion:kind:namespace:name#fieldPath'
 			splited := strings.Split(ignoreField, "#")
 			if len(splited) != 2 {
-				return fmt.Errorf("It should input in the form of 'apiVersion:kind:namespace:name#fieldPath'")
+				return fmt.Errorf("It should be entered in the form of 'apiVersion:kind:namespace:name#fieldPath'")
 			}
 			key, ignoredPath := splited[0], splited[1]
 			ignoredPathsOf[key] = append(ignoredPathsOf[key], ignoredPath)
