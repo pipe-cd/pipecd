720d118fedd05b7c7ac6d8715651c3b91f7a8f01
sivchari <shibuuuu5@gmail.com>
2023-04-12 14:47:06 +0900

    improve: proto

--- pkg/app/server/service/apiservice/service.pb.validate.go
+++ pkg/app/server/service/apiservice/service.pb.validate.go
@@ -2218,6 +2218,94 @@ func (m *UpdateApplicationRequest) validate(all bool) error {
 
 	var errors []error
 
+	if utf8.RuneCountInString(m.GetApplicationId()) < 1 {
+		err := UpdateApplicationRequestValidationError{
+			field:  "ApplicationId",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if utf8.RuneCountInString(m.GetPipedId()) < 1 {
+		err := UpdateApplicationRequestValidationError{
+			field:  "PipedId",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if utf8.RuneCountInString(m.GetName()) < 1 {
+		err := UpdateApplicationRequestValidationError{
+			field:  "Name",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	// no validation rules for Description
+
+	// no validation rules for Labels
+
+	if utf8.RuneCountInString(m.GetPlatformProvider()) < 1 {
+		err := UpdateApplicationRequestValidationError{
+			field:  "PlatformProvider",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if m.GetGitPath() == nil {
+		err := UpdateApplicationRequestValidationError{
+			field:  "GitPath",
+			reason: "value is required",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if all {
+		switch v := interface{}(m.GetGitPath()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, UpdateApplicationRequestValidationError{
+					field:  "GitPath",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, UpdateApplicationRequestValidationError{
+					field:  "GitPath",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetGitPath()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return UpdateApplicationRequestValidationError{
+				field:  "GitPath",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if all {
 		switch v := interface{}(m.GetApplication()).(type) {
 		case interface{ ValidateAll() error }:
