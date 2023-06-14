d5217709e27d7a1de7c4abdfdd493529495fd00f
Kenta Kozuka <kenta.kozuka@gmail.com>
2023-06-14 21:40:33 +0900

    fix typo
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>

--- pkg/app/piped/platformprovider/terraform/terraform_test.go
+++ pkg/app/piped/platformprovider/terraform/terraform_test.go
@@ -16,13 +16,13 @@ func TestPlanHasChangeRegex(t *testing.T) {
 	}{
 		{
 			name:     "older than v1.5.0",
-			input:    "Plan: 0 to import, 1 to add, 2 to change, 3 to destroy.",
-			expected: []string{"Plan: 0 to import, 1 to add, 2 to change, 3 to destroy.", "1", "2", "3"},
+			input:    "Plan: 1 to add, 2 to change, 3 to destroy.",
+			expected: []string{"Plan: 1 to add, 2 to change, 3 to destroy.", "1", "2", "3"},
 		},
 		{
 			name:     "later than v1.5.0",
-			input:    "Plan: 1 to add, 2 to change, 3 to destroy.",
-			expected: []string{"Plan: 1 to add, 2 to change, 3 to destroy.", "1", "2", "3"},
+			input:    "Plan: 0 to import, 1 to add, 2 to change, 3 to destroy.",
+			expected: []string{"Plan: 0 to import, 1 to add, 2 to change, 3 to destroy.", "1", "2", "3"},
 		},
 	}
 
