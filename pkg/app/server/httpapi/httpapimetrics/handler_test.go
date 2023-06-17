e8aa5d62cd45e822569929f7c972aa818753b68b
yamasaki <yamasakipann0218@gmail.com>
2023-06-17 12:48:43 +0900

    fix : add License & t.parallel
    Signed-off-by: yamasaki <yamasakipann0218@gmail.com>

--- pkg/app/server/httpapi/httpapimetrics/handler_test.go
+++ pkg/app/server/httpapi/httpapimetrics/handler_test.go
@@ -1,3 +1,17 @@
+// Copyright 2023 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package httpapimetrics
 
 import (
@@ -8,6 +22,7 @@ import (
 )
 
 func TestLabels(t *testing.T) {
+	t.Parallel()
 
 	tests := []struct {
 		path      string
@@ -54,6 +69,8 @@ func TestLabels(t *testing.T) {
 }
 
 func TestSanitizeMethod(t *testing.T) {
+	t.Parallel()
+
 	tests := []struct {
 		input    string
 		expected string
@@ -85,6 +102,8 @@ func TestSanitizeMethod(t *testing.T) {
 }
 
 func TestSanitizeCode(t *testing.T) {
+	t.Parallel()
+
 	tests := []struct {
 		input    int
 		expected string
