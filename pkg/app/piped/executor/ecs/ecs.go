b4bc5958a23a262c4b016ac9d9a1376491d61f02
khanhtc1202 <khanhtc1202@gmail.com>
2023-07-05 21:46:10 +0700

    Update logs message for ECS routing stage executor
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>

--- pkg/app/piped/executor/ecs/ecs.go
+++ pkg/app/piped/executor/ecs/ecs.go
@@ -411,12 +411,12 @@ func routing(ctx context.Context, in *executor.Input, platformProviderName strin
 
 	currListenerArns, err := client.GetListenerArns(ctx, primaryTargetGroup)
 	if err != nil {
-		in.LogPersister.Errorf("Failed to get current active listener: %v", err)
+		in.LogPersister.Errorf("Failed to get current active listeners: %v", err)
 		return false
 	}
 
 	if err := client.ModifyListeners(ctx, currListenerArns, routingTrafficCfg); err != nil {
-		in.LogPersister.Errorf("Failed to routing traffic to CANARY variant: %v", err)
+		in.LogPersister.Errorf("Failed to routing traffic to PRIMARY/CANARY variants: %v", err)
 		return false
 	}
 
