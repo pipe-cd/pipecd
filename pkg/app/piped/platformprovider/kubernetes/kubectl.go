5efcfc0ce162ac2ff95e5a16a95bb36c6664643f
hungran <26101787+hungran@users.noreply.github.com>
2023-07-15 19:07:23 +0700

    not return err
    Signed-off-by: hungran <26101787+hungran@users.noreply.github.com>

--- pkg/app/piped/platformprovider/kubernetes/kubectl.go
+++ pkg/app/piped/platformprovider/kubernetes/kubectl.go
@@ -235,7 +235,10 @@ func (c *Kubectl) CreateNamespace(ctx context.Context, kubeconfig, namespace str
 	out, err := cmd.CombinedOutput()
 
 	if strings.Contains(string(out), "(AlreadyExists)") {
-		fmt.Printf("Namespace already exists: %s, %v", string(out), err)
+		_, err := fmt.Printf("Namespace already exists: %s, %v", string(out), err)
+		if err != nil {
+			return err
+		}
 	}
 	if err != nil {
 		return fmt.Errorf("failed to create namespace: %s, %v", string(out), err)
