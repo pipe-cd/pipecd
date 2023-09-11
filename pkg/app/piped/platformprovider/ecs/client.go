9a222d2675069b43ca1692ebc04c08222cea0eb7
moko-poi <mokopoi44@gmail.com>
2023-09-11 15:51:21 +0900

    feat: Upgraded the ModifyListeners to include listenerrules
    Signed-off-by: moko-poi <mokopoi44@gmail.com>

--- pkg/app/piped/platformprovider/ecs/client.go
+++ pkg/app/piped/platformprovider/ecs/client.go
@@ -341,9 +341,43 @@ func (c *client) getLoadBalancerArn(ctx context.Context, targetGroupArn string) 
 	return output.TargetGroups[0].LoadBalancerArns[0], nil
 }
 
-func (c *client) ModifyListeners(ctx context.Context, listenerArns []string, routingTrafficCfg RoutingTrafficConfig) error {
+func (c *client) ModifyListenerOrRule(ctx context.Context, listenerArns []string, listenerRuleArns []string, routingTrafficCfg RoutingTrafficConfig) error {
 	if len(routingTrafficCfg) != 2 {
 		return fmt.Errorf("invalid listener configuration: requires 2 target groups")
+	}
+
+	if len(listenerRuleArns) > 0 {
+		modifyListenerRule := func(ctx context.Context, listenerRuleArn string) error {
+			input := &elasticloadbalancingv2.ModifyRuleInput{
+				RuleArn: aws.String(listenerRuleArn),
+				Actions: []elbtypes.Action{
+					{
+						Type: elbtypes.ActionTypeEnumForward,
+						ForwardConfig: &elbtypes.ForwardActionConfig{
+							TargetGroups: []elbtypes.TargetGroupTuple{
+								{
+									TargetGroupArn: aws.String(routingTrafficCfg[0].TargetGroupArn),
+									Weight:         aws.Int32(int32(routingTrafficCfg[0].Weight)),
+								},
+								{
+									TargetGroupArn: aws.String(routingTrafficCfg[1].TargetGroupArn),
+									Weight:         aws.Int32(int32(routingTrafficCfg[1].Weight)),
+								},
+							},
+						},
+					},
+				},
+			}
+			_, err := c.elbClient.ModifyRule(ctx, input)
+			return err
+		}
+
+		for _, rule := range listenerRuleArns {
+			if err := modifyListenerRule(ctx, rule); err != nil {
+				return err
+			}
+		}
+		return nil
 	}
 
 	modifyListener := func(ctx context.Context, listenerArn string) error {
@@ -373,6 +407,44 @@ func (c *client) ModifyListeners(ctx context.Context, listenerArns []string, rou
 
 	for _, listener := range listenerArns {
 		if err := modifyListener(ctx, listener); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *client) ModifyListenerRules(ctx context.Context, listenerRuleArns []string, routingTrafficCfg RoutingTrafficConfig) error {
+	if len(routingTrafficCfg) != 2 {
+		return fmt.Errorf("invalid listener configuration: requires 2 target groups")
+	}
+
+	modifyListenerRule := func(ctx context.Context, listenerRuleArn string) error {
+		input := &elasticloadbalancingv2.ModifyRuleInput{
+			RuleArn: aws.String(listenerRuleArn),
+			Actions: []elbtypes.Action{
+				{
+					Type: elbtypes.ActionTypeEnumForward,
+					ForwardConfig: &elbtypes.ForwardActionConfig{
+						TargetGroups: []elbtypes.TargetGroupTuple{
+							{
+								TargetGroupArn: aws.String(routingTrafficCfg[0].TargetGroupArn),
+								Weight:         aws.Int32(int32(routingTrafficCfg[0].Weight)),
+							},
+							{
+								TargetGroupArn: aws.String(routingTrafficCfg[1].TargetGroupArn),
+								Weight:         aws.Int32(int32(routingTrafficCfg[1].Weight)),
+							},
+						},
+					},
+				},
+			},
+		}
+		_, err := c.elbClient.ModifyRule(ctx, input)
+		return err
+	}
+
+	for _, listener := range listenerRuleArns {
+		if err := modifyListenerRule(ctx, listener); err != nil {
 			return err
 		}
 	}
