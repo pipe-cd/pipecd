49f19a7e8fb76562b21f25718674cd469e1a4eb0
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2023-10-06 15:29:36 +0700

    [ECS] Fix remove all previous active tasksets on QuickSync (#4600)
    * Remove previous ACTIVE tasksets if present on quicksync
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>
    
    * Remove GetPrimaryTaskSet interface
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>
    
    ---------
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>
    Signed-off-by: moko-poi <mokopoi44@gmail.com>

--- pkg/app/piped/platformprovider/ecs/ecs.go
+++ pkg/app/piped/platformprovider/ecs/ecs.go
@@ -48,7 +48,6 @@ type ECS interface {
 	WaitServiceStable(ctx context.Context, service types.Service) error
 	RegisterTaskDefinition(ctx context.Context, taskDefinition types.TaskDefinition) (*types.TaskDefinition, error)
 	RunTask(ctx context.Context, taskDefinition types.TaskDefinition, clusterArn string, launchType string, awsVpcConfiguration *config.ECSVpcConfiguration, tags []types.Tag) error
-	GetPrimaryTaskSet(ctx context.Context, service types.Service) (*types.TaskSet, error)
 	GetServiceTaskSets(ctx context.Context, service types.Service) ([]*types.TaskSet, error)
 	CreateTaskSet(ctx context.Context, service types.Service, taskDefinition types.TaskDefinition, targetGroup *types.LoadBalancer, scale int) (*types.TaskSet, error)
 	DeleteTaskSet(ctx context.Context, taskSet types.TaskSet) error
