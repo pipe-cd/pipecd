cb6dab2759ee41037cbf82d2eec11900caebf071
t-kikuc <tkikuchi07f@gmail.com>
2024-01-09 14:59:48 +0900

    Remove stdinReader
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/app/pipectl/prompt/reader.go
+++ pkg/app/pipectl/prompt/reader.go
@@ -26,34 +26,20 @@ const (
 	maximum_retry = 5 // maximum number of retry for required fields
 )
 
-// Reader is an interface that reads input from user or mock.
-type Reader interface {
-	// ReadString reads a string with showing message. If the input is empty, it returns an empty string.
-	// If the input is more than one word, it returns an error.
-	ReadString(message string) (string, error)
-	// ReadStrings reads a string slice separated by blank space with showing message. If the input is empty, it returns an empty slice.
-	ReadStrings(message string) ([]string, error)
-	// ReadInt reads an integer with showing message. If the input is empty, it returns 0.
-	// If the input is not an integer, it returns an error.
-	ReadInt(message string) (int, error)
-	// ReadStringRequired reads a string with showing message. If the input is empty, it asks again.
-	// If number of retry exceeds maximum_retry, it returns an error.
-	// If the input is more than one word, it returns an error.
-	ReadStringRequired(message string) (string, error)
-}
-
-// stdReader is a Reader implementation that reads input from stdin.
-type stdinReader struct {
-	reader bufio.Reader
+// Reader reads input from user or mock.
+type Reader struct {
+	bufReader bufio.Reader
 }
 
 func NewReader(in io.Reader) Reader {
-	return &stdinReader{
-		reader: *bufio.NewReader(in),
+	return Reader{
+		bufReader: *bufio.NewReader(in),
 	}
 }
 
-func (r *stdinReader) ReadString(message string) (string, error) {
+// ReadString reads a string with showing message. If the input is empty, it returns an empty string.
+// If the input is more than one word, it returns an error.
+func (r *Reader) ReadString(message string) (string, error) {
 	s, e := r.ReadStrings(message)
 	if e != nil {
 		return "", e
@@ -69,16 +55,19 @@ func (r *stdinReader) ReadString(message string) (string, error) {
 	return s[0], nil
 }
 
-func (r *stdinReader) ReadStrings(message string) ([]string, error) {
+// ReadStrings reads a string slice separated by blank space with showing message. If the input is empty, it returns an empty slice.
+func (r *Reader) ReadStrings(message string) ([]string, error) {
 	fmt.Printf("%s ", message)
-	line, e := r.reader.ReadString('\n')
+	line, e := r.bufReader.ReadString('\n')
 	if e != nil {
 		return nil, e
 	}
 	return strings.Fields(line), nil
 }
 
-func (r *stdinReader) ReadInt(message string) (int, error) {
+// ReadInt reads an integer with showing message. If the input is empty, it returns 0.
+// If the input is not an integer, it returns an error.
+func (r *Reader) ReadInt(message string) (int, error) {
 	s, e := r.ReadString(message)
 	if e != nil {
 		return 0, e
@@ -93,7 +82,10 @@ func (r *stdinReader) ReadInt(message string) (int, error) {
 	return n, nil
 }
 
-func (r *stdinReader) ReadStringRequired(message string) (string, error) {
+// ReadStringRequired reads a string with showing message. If the input is empty, it asks again.
+// If number of retry exceeds maximum_retry, it returns an error.
+// If the input is more than one word, it returns an error.
+func (r *Reader) ReadStringRequired(message string) (string, error) {
 	for i := 0; i < maximum_retry; i++ {
 		s, e := r.ReadString(message)
 		if e != nil {
