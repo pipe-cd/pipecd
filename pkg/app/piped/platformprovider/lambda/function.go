e98705976f3516f395b0141291430f6ecf325b49
t-kikuc <tkikuchi07f@gmail.com>
2024-03-05 18:25:34 +0900

    Add comment of Layers
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/app/piped/platformprovider/lambda/function.go
+++ pkg/app/piped/platformprovider/lambda/function.go
@@ -76,6 +76,8 @@ type FunctionManifestSpec struct {
 	VPCConfig        *VPCConfig        `json:"vpcConfig,omitempty"`
 	// A list of function layers used in the function.
 	// Specify each layer by its ARN including the version.
+	// You can use layers only with Lambda functions deployed as a .zip file archive, not for a container image.
+	// See https://docs.aws.amazon.com/lambda/latest/dg/chapter-layers.html.
 	Layers []string `json:"layers,omitempty"`
 }
 
