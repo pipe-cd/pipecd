27a0d971a33b11862ae898ecca8e4d582f80a9ab
Tetsuya Kikuchi <97105818+t-kikuc@users.noreply.github.com>
2024-03-26 19:03:35 +0900

    Support deploying Lambda functions depending on Lambda layers (#4805)
    * draft: support Lambda layer
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * reset 'draft: support Lambda layer'
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Add Layers attribute
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Add comment of Layers
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * add a test
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Add comment of Layers
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Add comment of Layers
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    ---------
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/app/piped/platformprovider/lambda/function_test.go
+++ pkg/app/piped/platformprovider/lambda/function_test.go
@@ -130,6 +130,40 @@ func TestParseFunctionManifest(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "correct config for LambdaFunction with specifying layers",
+			data: `{
+  "apiVersion": "pipecd.dev/v1beta1",
+  "kind": "LambdaFunction",
+  "spec": {
+	  "name": "SimpleFunction",
+	  "role": "arn:aws:iam::xxxxx:role/lambda-role",
+	  "memory": 128,
+	  "timeout": 5,
+	  "image": "ecr.region.amazonaws.com/lambda-simple-function:v0.0.1",
+	  "layers": [
+		"arn:aws:lambda:REGION:ACCOUNT:layer:test-layer-1:1",
+		"arn:aws:lambda:REGION:ACCOUNT:layer:test-layer-2:2"
+	  ]
+  }
+}`,
+			wantSpec: FunctionManifest{
+				Kind:       "LambdaFunction",
+				APIVersion: "pipecd.dev/v1beta1",
+				Spec: FunctionManifestSpec{
+					Name:     "SimpleFunction",
+					Role:     "arn:aws:iam::xxxxx:role/lambda-role",
+					Memory:   128,
+					Timeout:  5,
+					ImageURI: "ecr.region.amazonaws.com/lambda-simple-function:v0.0.1",
+					Layers: []string{
+						"arn:aws:lambda:REGION:ACCOUNT:layer:test-layer-1:1",
+						"arn:aws:lambda:REGION:ACCOUNT:layer:test-layer-2:2",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
 			name: "correct config for LambdaFunction with specifying vpc config",
 			data: `{
   "apiVersion": "pipecd.dev/v1beta1",
