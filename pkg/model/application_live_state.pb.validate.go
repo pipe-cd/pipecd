e6b1ee65f8042575ed0f05656f3eb0bea65ab3ec
t-kikuc <tkikuchi07f@gmail.com>
2024-06-18 19:51:02 +0900

    Remove validation of ApiVersion of ECS
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/model/application_live_state.pb.validate.go
+++ pkg/model/application_live_state.pb.validate.go
@@ -1746,16 +1746,7 @@ func (m *ECSResourceState) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
-	if utf8.RuneCountInString(m.GetApiVersion()) < 1 {
-		err := ECSResourceStateValidationError{
-			field:  "ApiVersion",
-			reason: "value length must be at least 1 runes",
-		}
-		if !all {
-			return err
-		}
-		errors = append(errors, err)
-	}
+	// no validation rules for ApiVersion
 
 	if utf8.RuneCountInString(m.GetKind()) < 1 {
 		err := ECSResourceStateValidationError{
