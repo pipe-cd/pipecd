f20a270153800b7732cf95de354236a6dc570769
Yoshiki Fujikane <40124947+ffjlabo@users.noreply.github.com>
2024-07-01 11:39:19 +0900

    Add default env for script run (#5002)
    * Add default env for script run
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    * Add default env for script run rollback
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    * Make ContextInfo to use deployment info for script run stage
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    * Add docs
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    * Change env name
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    * Add test
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    * Add Summary value
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    * Set omitempty
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>
    
    ---------
    
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>

---
+++ pkg/app/piped/executor/scriptrun/script_run_test.go
@@ -0,0 +1,65 @@
+package scriptrun
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ContextInfo_BuildEnv(t *testing.T) {
+	tests := []struct {
+		name    string
+		ci      *ContextInfo
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name: "success",
+			ci: &ContextInfo{
+				DeploymentID:        "deployment-id",
+				ApplicationID:       "application-id",
+				ApplicationName:     "application-name",
+				TriggeredAt:         1234567890,
+				TriggeredCommitHash: "commit-hash",
+				RepositoryURL:       "repo-url",
+				Labels: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+				Summary: "summary",
+			},
+			want: map[string]string{
+				"SR_DEPLOYMENT_ID":         "deployment-id",
+				"SR_APPLICATION_ID":        "application-id",
+				"SR_APPLICATION_NAME":      "application-name",
+				"SR_TRIGGERED_AT":          "1234567890",
+				"SR_TRIGGERED_COMMIT_HASH": "commit-hash",
+				"SR_REPOSITORY_URL":        "repo-url",
+				"SR_SUMMARY":               "summary",
+				"SR_LABELS_KEY1":           "value1",
+				"SR_LABELS_KEY2":           "value2",
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.ci.BuildEnv()
+			assert.Equal(t, tt.wantErr, err != nil)
+
+			for k, v := range got {
+				if k == "SR_CONTEXT_RAW" {
+					continue
+				}
+				assert.Equal(t, tt.want[k], v)
+			}
+
+			var gotRaw ContextInfo
+			err = json.Unmarshal([]byte(got["SR_CONTEXT_RAW"]), &gotRaw)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.ci, &gotRaw)
+		})
+	}
+}
