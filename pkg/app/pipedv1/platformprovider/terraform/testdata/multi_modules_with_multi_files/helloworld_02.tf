eca1f46e0da2f9fd19509d428ed8fc4f12241768
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2024-07-17 17:50:26 +0900

    Delete platform specific codes from pipedv1 (#5051)
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- pkg/app/pipedv1/platformprovider/terraform/testdata/multi_modules_with_multi_files/helloworld_02.tf
+++
@@ -1,9 +0,0 @@
-provider "docker" {
-}
-
-module "helloworld_02" {
-  source = "helloworld"
-  version = "v0.9.0"
-  image_version = "v0.9.0"
-  external_port = 8081
-}
--- pkg/app/pipedv1/platformprovider/terraform/testdata/single_module/main.tf
+++
@@ -1,9 +0,0 @@
-provider "docker" {
-}
-
-module "helloworld" {
-  source = "helloworld"
-  version = "v1.0.0"
-  image_version = "v1.0.0"
-  external_port = 8080
-}
