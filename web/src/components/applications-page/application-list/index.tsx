49f7ec04f3c400173945b4bc58380ea79a52bffc
Yoshiki Fujikane <40124947+ffjlabo@users.noreply.github.com>
2024-08-15 11:03:31 +0900

    Revert "Cherry-pick #5029 #5096 #5097 #5099 #5108 #5113 #5114 #5115 #5116 #51…" (#5135)
    This reverts commit b1d9cd0144e68ac8904756b955bcdf1a3f25e7a8.
    
    Signed-off-by: Yoshiki Fujikane <40124947+ffjlabo@users.noreply.github.com>

--- web/src/components/applications-page/application-list/index.tsx
+++ web/src/components/applications-page/application-list/index.tsx
@@ -194,7 +194,7 @@ export const ApplicationList: FC<ApplicationListProps> = memo(
                   onPageChange={(_, newPage) => {
                     onPageChange(newPage + 1);
                   }}
-                  onRowsPerPageChange={(e) => {
+                  onChangeRowsPerPage={(e) => {
                     setRowsPerPage(parseInt(e.target.value, 10));
                     onPageChange(1);
                   }}
