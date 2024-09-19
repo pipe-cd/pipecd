48ab49caf57634780e76b7229266292c6c6bc5d2
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2024-09-19 15:03:08 +0900

    Use bytes as application_config
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- web/model/deployment_source_pb.d.ts
+++ web/model/deployment_source_pb.d.ts
@@ -9,10 +9,10 @@ export class DeploymentSource extends jspb.Message {
   getRevision(): string;
   setRevision(value: string): DeploymentSource;
 
-  getApplicationConfig(): PluginApplicationSpec | undefined;
-  setApplicationConfig(value?: PluginApplicationSpec): DeploymentSource;
-  hasApplicationConfig(): boolean;
-  clearApplicationConfig(): DeploymentSource;
+  getApplicationConfig(): Uint8Array | string;
+  getApplicationConfig_asU8(): Uint8Array;
+  getApplicationConfig_asB64(): string;
+  setApplicationConfig(value: Uint8Array | string): DeploymentSource;
 
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): DeploymentSource.AsObject;
@@ -26,35 +26,7 @@ export namespace DeploymentSource {
   export type AsObject = {
     applicationDirectory: string,
     revision: string,
-    applicationConfig?: PluginApplicationSpec.AsObject,
-  }
-}
-
-export class PluginApplicationSpec extends jspb.Message {
-  getKind(): string;
-  setKind(value: string): PluginApplicationSpec;
-
-  getApiVersion(): string;
-  setApiVersion(value: string): PluginApplicationSpec;
-
-  getSpec(): Uint8Array | string;
-  getSpec_asU8(): Uint8Array;
-  getSpec_asB64(): string;
-  setSpec(value: Uint8Array | string): PluginApplicationSpec;
-
-  serializeBinary(): Uint8Array;
-  toObject(includeInstance?: boolean): PluginApplicationSpec.AsObject;
-  static toObject(includeInstance: boolean, msg: PluginApplicationSpec): PluginApplicationSpec.AsObject;
-  static serializeBinaryToWriter(message: PluginApplicationSpec, writer: jspb.BinaryWriter): void;
-  static deserializeBinary(bytes: Uint8Array): PluginApplicationSpec;
-  static deserializeBinaryFromReader(message: PluginApplicationSpec, reader: jspb.BinaryReader): PluginApplicationSpec;
-}
-
-export namespace PluginApplicationSpec {
-  export type AsObject = {
-    kind: string,
-    apiVersion: string,
-    spec: Uint8Array | string,
+    applicationConfig: Uint8Array | string,
   }
 }
 
