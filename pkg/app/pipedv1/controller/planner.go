4d5357485b53250b369fcbbdc851b5c1e2b0c31b
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2024-09-27 09:19:33 +0900

    Add PipelineStage.Rollback and mark PipelineStage.Visible as deprecated (#5232)
    * Add PipelineStage.Rollback and marke PipelineStage.Visible as deprecated
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>
    
    * Fix flaky tests and update TODO
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>
    
    ---------
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>

--- pkg/app/pipedv1/controller/planner.go
+++ pkg/app/pipedv1/controller/planner.go
@@ -408,15 +408,20 @@ func (p *planner) buildQuickSyncStages(ctx context.Context, cfg *config.GenericA
 		if err != nil {
 			return nil, fmt.Errorf("failed to build quick sync stage deployment (%w)", err)
 		}
+		// TODO: Ensure responsed stages indexies is valid.
 		for i := range res.Stages {
-			// TODO: Consider add Stage.Rollback to specify a stage is a rollback stage or forward stage instead.
-			if res.Stages[i].Visible {
-				stages = append(stages, res.Stages[i])
-			} else {
+			if res.Stages[i].Rollback {
 				rollbackStages = append(rollbackStages, res.Stages[i])
+			} else {
+				stages = append(stages, res.Stages[i])
 			}
 		}
 	}
+
+	// Sort stages by index.
+	sort.Sort(model.PipelineStages(stages))
+	sort.Sort(model.PipelineStages(rollbackStages))
+
 	stages = append(stages, rollbackStages...)
 	if len(stages) == 0 {
 		return nil, fmt.Errorf("unable to build quick sync stages for deployment")
@@ -468,11 +473,12 @@ func (p *planner) buildPipelineSyncStages(ctx context.Context, cfg *config.Gener
 		if err != nil {
 			return nil, fmt.Errorf("failed to build pipeline sync stages for deployment (%w)", err)
 		}
+		// TODO: Ensure responsed stages indexies is valid.
 		for i := range res.Stages {
-			if res.Stages[i].Visible {
-				stages = append(stages, res.Stages[i])
-			} else {
+			if res.Stages[i].Rollback {
 				rollbackStages = append(rollbackStages, res.Stages[i])
+			} else {
+				stages = append(stages, res.Stages[i])
 			}
 		}
 	}
