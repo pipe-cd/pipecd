2f86f7dca297f7f2d402c2040a4a4e14837587ab
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2024-10-22 16:27:09 +0900

    Implement LoadPlainYAMLManifests (#5284)
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- pkg/app/pipedv1/plugin/kubernetes/provider/kubernetes.go
+++ pkg/app/pipedv1/plugin/kubernetes/provider/kubernetes.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,16 +14,6 @@
 
 package provider
 
-import "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-
-// Manifest represents a Kubernetes resource manifest.
-type Manifest struct {
-	Key ResourceKey
-	Body *unstructured.Unstructured
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-func (m *Manifest) UnmarshalJSON(data []byte) error {
-	m.Body = new(unstructured.Unstructured)
-	return m.Body.UnmarshalJSON(data)
-}
+const (
+	AnnotationOrder = "pipecd.dev/order" // The order number of resource used to sort them before using.
+)
