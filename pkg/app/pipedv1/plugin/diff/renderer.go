4397ee895b42ca8cfa766ae31d15cc840e3af56b
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2024-10-28 15:08:00 +0900

    Copy almost of pkg/diff to under the plugin package
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

---
+++ pkg/app/pipedv1/plugin/diff/renderer.go
@@ -0,0 +1,224 @@
+// Copyright 2024 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package diff
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Renderer struct {
+	leftPadding    int
+	maskPathPrefix string
+}
+
+type RenderOption func(*Renderer)
+
+const maskString = "*****"
+
+func WithLeftPadding(p int) RenderOption {
+	return func(r *Renderer) {
+		r.leftPadding = p
+	}
+}
+
+func WithMaskPath(prefix string) RenderOption {
+	return func(r *Renderer) {
+		r.maskPathPrefix = prefix
+	}
+}
+
+func NewRenderer(opts ...RenderOption) *Renderer {
+	r := &Renderer{}
+	for _, opt := range opts {
+		opt(r)
+	}
+	return r
+}
+
+func (r *Renderer) Render(ns Nodes) string {
+	if len(ns) == 0 {
+		return ""
+	}
+
+	var prePath []PathStep
+	var b strings.Builder
+
+	printValue := func(mark string, v reflect.Value, lastStep PathStep, depth int) {
+		if !v.IsValid() {
+			return
+		}
+
+		nodeString, nl := renderNodeValue(v, "")
+		if lastStep.Type == SliceIndexPathStep {
+			nl = false
+		}
+
+		switch {
+		case lastStep.Type == SliceIndexPathStep:
+			b.WriteString(fmt.Sprintf("%s%*s- ", mark, depth*2-1, ""))
+		case nl:
+			b.WriteString(fmt.Sprintf("%s%*s%s:\n", mark, depth*2-1, "", lastStep.String()))
+		default:
+			b.WriteString(fmt.Sprintf("%s%*s%s: ", mark, depth*2-1, "", lastStep.String()))
+		}
+
+		parts := strings.Split(nodeString, "\n")
+		for i, p := range parts {
+			if lastStep.Type != SliceIndexPathStep {
+				if nl {
+					b.WriteString(fmt.Sprintf("%s%*s%s\n", mark, depth*2+1, "", p))
+				} else {
+					b.WriteString(fmt.Sprintf("%s\n", p))
+				}
+				continue
+			}
+			if i == 0 {
+				b.WriteString(fmt.Sprintf("%s\n", p))
+				continue
+			}
+			b.WriteString(fmt.Sprintf("%s%*s%s\n", mark, depth*2+1, "", p))
+		}
+	}
+
+	for _, n := range ns {
+		duplicateDepth := pathDuplicateDepth(n.Path, prePath)
+		prePath = n.Path
+		pathLen := len(n.Path)
+
+		var array bool
+		for i := duplicateDepth; i < pathLen-1; i++ {
+			if n.Path[i].Type == SliceIndexPathStep {
+				b.WriteString(fmt.Sprintf("%*s-", (r.leftPadding+i)*2, ""))
+				array = true
+				continue
+			}
+			if array {
+				b.WriteString(fmt.Sprintf(" %s:\n", n.Path[i].String()))
+				array = false
+				continue
+			}
+			b.WriteString(fmt.Sprintf("%*s%s:\n", (r.leftPadding+i)*2, "", n.Path[i].String()))
+		}
+		if array {
+			b.WriteString("\n")
+		}
+
+		lastStep := n.Path[pathLen-1]
+		valueX, valueY := n.ValueX, n.ValueY
+		if r.maskPathPrefix != "" && strings.HasPrefix(n.PathString, r.maskPathPrefix) {
+			valueX = reflect.ValueOf(maskString)
+			valueY = reflect.ValueOf(maskString)
+		}
+
+		b.WriteString(fmt.Sprintf("%*s#%s\n", (r.leftPadding+pathLen-1)*2, "", n.PathString))
+		printValue("-", valueX, lastStep, r.leftPadding+pathLen-1)
+		printValue("+", valueY, lastStep, r.leftPadding+pathLen-1)
+		b.WriteString("\n")
+	}
+
+	return b.String()
+}
+
+func pathDuplicateDepth(x, y []PathStep) int {
+	minLen := len(x)
+	if minLen > len(y) {
+		minLen = len(y)
+	}
+
+	for i := 0; i < minLen; i++ {
+		if x[i] == y[i] {
+			continue
+		}
+		return i
+	}
+	return 0
+}
+
+func renderNodeValue(v reflect.Value, prefix string) (string, bool) {
+	switch v.Kind() {
+	case reflect.Map:
+		out := make([]string, 0, v.Len())
+		keys := v.MapKeys()
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].String() < keys[j].String()
+		})
+		for _, k := range keys {
+			sub := v.MapIndex(k)
+			subString, nl := renderNodeValue(sub, prefix+"  ")
+			if !nl {
+				out = append(out, fmt.Sprintf("%s%s: %s", prefix, k.String(), subString))
+				continue
+			}
+			out = append(out, fmt.Sprintf("%s%s:\n%s", prefix, k.String(), subString))
+		}
+		if len(out) == 0 {
+			return "", false
+		}
+		return strings.Join(out, "\n"), true
+
+	case reflect.Slice, reflect.Array:
+		out := make([]string, 0, v.Len())
+		for i := 0; i < v.Len(); i++ {
+			sub, _ := renderNodeValue(v.Index(i), prefix+"  ")
+			parts := strings.Split(sub, "\n")
+			for i, p := range parts {
+				p = strings.TrimPrefix(p, prefix+"  ")
+				if i == 0 {
+					out = append(out, fmt.Sprintf("%s- %s", prefix, p))
+					continue
+				}
+				out = append(out, fmt.Sprintf("%s  %s", prefix, p))
+			}
+		}
+		return strings.Join(out, "\n"), true
+
+	case reflect.Interface:
+		return renderNodeValue(v.Elem(), prefix)
+
+	case reflect.String:
+		return v.String(), false
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return strconv.FormatInt(v.Int(), 10), false
+
+	case reflect.Float32, reflect.Float64:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 64), false
+
+	default:
+		return v.String(), false
+	}
+}
+
+func RenderPrimitiveValue(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.String:
+		return v.String()
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return strconv.FormatInt(v.Int(), 10)
+
+	case reflect.Float32, reflect.Float64:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 64)
+
+	default:
+		return v.String()
+	}
+}
---
+++ pkg/app/pipedv1/plugin/diff/renderer_test.go
@@ -0,0 +1,206 @@
+// Copyright 2024 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package diff
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRenderNodeValue(t *testing.T) {
+	var (
+		mapOfPrimative = map[string]string{
+			"one": "1",
+			"two": "2",
+		}
+		mapOfMap = map[string]interface{}{
+			"one": map[string]string{
+				"one": "1-1",
+				"two": "1-2",
+			},
+			"two": map[string]string{
+				"one": "2-1",
+				"two": "2-2",
+			},
+		}
+		mapOfSlice = map[string]interface{}{
+			"one": []string{"one-1", "one-2"},
+			"two": []string{"two-1", "two-2"},
+		}
+	)
+
+	testcases := []struct {
+		name     string
+		value    reflect.Value
+		expected string
+	}{
+		{
+			name:     "int value",
+			value:    reflect.ValueOf(1),
+			expected: "1",
+		},
+		{
+			name:     "float value",
+			value:    reflect.ValueOf(1.25),
+			expected: "1.25",
+		},
+		{
+			name:     "string value",
+			value:    reflect.ValueOf("hello"),
+			expected: "hello",
+		},
+		{
+			name: "slice of primitive elements",
+			value: func() reflect.Value {
+				v := []int{1, 2, 3}
+				return reflect.ValueOf(v)
+			}(),
+			expected: `- 1
+- 2
+- 3`,
+		},
+		{
+			name: "slice of interface",
+			value: func() reflect.Value {
+				v := []interface{}{
+					map[string]int{
+						"1-one": 1,
+						"2-two": 2,
+					},
+					map[string]int{
+						"3-three": 3,
+						"4-four":  4,
+					},
+				}
+				return reflect.ValueOf(v)
+			}(),
+			expected: `- 1-one: 1
+  2-two: 2
+- 3-three: 3
+  4-four: 4`,
+		},
+		{
+			name: "simple map",
+			value: reflect.ValueOf(map[string]string{
+				"one": "one-value",
+				"two": "two-value",
+			}),
+			expected: `one: one-value
+two: two-value`,
+		},
+		{
+			name: "nested map",
+			value: func() reflect.Value {
+				v := map[string]interface{}{
+					"1-number":           1,
+					"2-string":           "hello",
+					"3-map-of-primitive": mapOfPrimative,
+					"4-map-of-map":       mapOfMap,
+					"5-map-of-slice":     mapOfSlice,
+					"6-slice":            []string{"a", "b"},
+					"7-string":           "hi",
+				}
+				return reflect.ValueOf(v)
+			}(),
+			expected: `1-number: 1
+2-string: hello
+3-map-of-primitive:
+  one: 1
+  two: 2
+4-map-of-map:
+  one:
+    one: 1-1
+    two: 1-2
+  two:
+    one: 2-1
+    two: 2-2
+5-map-of-slice:
+  one:
+    - one-1
+    - one-2
+  two:
+    - two-1
+    - two-2
+6-slice:
+  - a
+  - b
+7-string: hi`,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, _ := renderNodeValue(tc.value, "")
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
+
+func TestRenderNodeValueComplex(t *testing.T) {
+	// Complex node. Note that the keys in the yaml file must be in order.
+	objs, err := loadUnstructureds("testdata/complex-node.yaml")
+	require.NoError(t, err)
+	require.Equal(t, 1, len(objs))
+
+	root := reflect.ValueOf(objs[0].Object)
+	got, _ := renderNodeValue(root, "")
+
+	data, err := os.ReadFile("testdata/complex-node.yaml")
+	require.NoError(t, err)
+	assert.Equal(t, string(data), got)
+}
+
+func TestRenderPrimitiveValue(t *testing.T) {
+	testcases := []struct {
+		name     string
+		value    interface{}
+		expected string
+	}{
+		{
+			name:     "string",
+			value:    "hello",
+			expected: "hello",
+		},
+		{
+			name:     "int",
+			value:    1,
+			expected: "1",
+		},
+		{
+			name:     "float",
+			value:    1.25,
+			expected: "1.25",
+		},
+		{
+			name: "map",
+			value: map[string]int{
+				"one": 1,
+			},
+			expected: "<map[string]int Value>",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			v := reflect.ValueOf(tc.value)
+			got := RenderPrimitiveValue(v)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
