afff927fd80baf2141515d73c076e7cea32dd4e5
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2024-10-29 11:11:34 +0900

    Implement k8s manifest diff
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

---
+++ pkg/app/pipedv1/plugin/kubernetes/provider/diffutil_test.go
@@ -0,0 +1,218 @@
+// Copyright 2024 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoveMapFields(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		name     string
+		config   map[string]interface{}
+		live     map[string]interface{}
+		expected map[string]interface{}
+	}{
+		{
+			name:     "Empty map",
+			config:   make(map[string]interface{}, 0),
+			live:     make(map[string]interface{}, 0),
+			expected: make(map[string]interface{}, 0),
+		},
+		{
+			name: "Not nested 1",
+			config: map[string]interface{}{
+				"key a": "value a",
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": "value b",
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+			},
+		},
+		{
+			name: "Not nested 2",
+			config: map[string]interface{}{
+				"key a": "value a",
+				"key b": "value b",
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+			},
+		},
+		{
+			name: "Nested live deleted",
+			config: map[string]interface{}{
+				"key a": "value a",
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": map[string]interface{}{
+					"nested key a": "nested value a",
+				},
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+			},
+		},
+		{
+			name: "Nested same",
+			config: map[string]interface{}{
+				"key a": "value a",
+				"key b": map[string]interface{}{
+					"nested key a": "nested value a",
+				},
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": map[string]interface{}{
+					"nested key a": "nested value a",
+				},
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+				"key b": map[string]interface{}{
+					"nested key a": "nested value a",
+				},
+			},
+		},
+		{
+			name: "Nested nested live deleted",
+			config: map[string]interface{}{
+				"key a": "value a",
+				"key b": map[string]interface{}{
+					"nested key a": "nested value a",
+				},
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": map[string]interface{}{
+					"nested key a": "nested value a",
+					"nested key b": "nested value b",
+				},
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+				"key b": map[string]interface{}{
+					"nested key a": "nested value a",
+				},
+			},
+		},
+		{
+			name: "Nested array",
+			config: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", 3,
+				},
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", 3,
+				},
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", 3,
+				},
+			},
+		},
+		{
+			name: "Nested array 2",
+			config: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", 3, 4,
+				},
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", 3,
+				},
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", 3,
+				},
+			},
+		},
+		{
+			name: "Nested array remain",
+			config: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b",
+				},
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", map[string]interface{}{
+						"aa": "aa",
+					},
+				},
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", map[string]interface{}{
+						"aa": "aa",
+					},
+				},
+			},
+		},
+		{
+			name: "Nested array same",
+			config: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"a", "b", 3,
+				},
+			},
+			live: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"b", "a", 3,
+				},
+			},
+			expected: map[string]interface{}{
+				"key a": "value a",
+				"key b": []interface{}{
+					"b", "a", 3,
+				},
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			removed := removeMapFields(tc.config, tc.live)
+			assert.Equal(t, tc.expected, removed)
+		})
+	}
+}
--- pkg/app/pipedv1/plugin/kubernetes/provider/resource.go
+++ pkg/app/pipedv1/plugin/kubernetes/provider/resource.go
@@ -20,15 +20,53 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
+var builtInAPIVersions = map[string]struct{}{
+	"admissionregistration.k8s.io/v1":      {},
+	"admissionregistration.k8s.io/v1beta1": {},
+	"apiextensions.k8s.io/v1":              {},
+	"apiextensions.k8s.io/v1beta1":         {},
+	"apiregistration.k8s.io/v1":            {},
+	"apiregistration.k8s.io/v1beta1":       {},
+	"apps/v1":                              {},
+	"authentication.k8s.io/v1":             {},
+	"authentication.k8s.io/v1beta1":        {},
+	"authorization.k8s.io/v1":              {},
+	"authorization.k8s.io/v1beta1":         {},
+	"autoscaling/v1":                       {},
+	"autoscaling/v2beta1":                  {},
+	"autoscaling/v2beta2":                  {},
+	"batch/v1":                             {},
+	"batch/v1beta1":                        {},
+	"certificates.k8s.io/v1beta1":          {},
+	"coordination.k8s.io/v1":               {},
+	"coordination.k8s.io/v1beta1":          {},
+	"extensions/v1beta1":                   {},
+	"internal.autoscaling.k8s.io/v1alpha1": {},
+	"metrics.k8s.io/v1beta1":               {},
+	"networking.k8s.io/v1":                 {},
+	"networking.k8s.io/v1beta1":            {},
+	"node.k8s.io/v1beta1":                  {},
+	"policy/v1":                            {},
+	"policy/v1beta1":                       {},
+	"rbac.authorization.k8s.io/v1":         {},
+	"rbac.authorization.k8s.io/v1beta1":    {},
+	"scheduling.k8s.io/v1":                 {},
+	"scheduling.k8s.io/v1beta1":            {},
+	"storage.k8s.io/v1":                    {},
+	"storage.k8s.io/v1beta1":               {},
+	"v1":                                   {},
+}
 
-
-const KindDeployment = "Deployment"
+const (
+	KindDeployment = "Deployment"
+	KindSecret     = "Secret"
+)
 
 type ResourceKey struct {
 	APIVersion string
-	Kind      string
-	Namespace string
-	Name      string
+	Kind       string
+	Namespace  string
+	Name       string
 }
 
 func (k ResourceKey) String() string {
@@ -47,4 +85,21 @@ func MakeResourceKey(obj *unstructured.Unstructured) ResourceKey {
 		Name:       obj.GetName(),
 	}
 	return k
+}
+
+func (k ResourceKey) IsSecret() bool {
+	if k.Kind != KindSecret {
+		return false
+	}
+	if !IsKubernetesBuiltInResource(k.APIVersion) {
+		return false
+	}
+	return true
+}
+
+func IsKubernetesBuiltInResource(apiVersion string) bool {
+	_, ok := builtInAPIVersions[apiVersion]
+	// TODO: Change the way to detect whether an APIVersion is built-in or not
+	// rather than depending on this fixed list.
+	return ok
 }
