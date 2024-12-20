3b46eb49bf40e2a24b8284c0c6f09b4e3366c901
khanhtc1202 <khanhtc1202@gmail.com>
2024-12-20 14:45:12 +0700

    Using uber-go mock instead of golang mock in tests
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>

--- pkg/app/piped/executor/kubernetes/sync_test.go
+++ pkg/app/piped/executor/kubernetes/sync_test.go
@@ -21,9 +21,9 @@ import (
 	"path"
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 	"go.uber.org/zap"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
--- pkg/app/server/analysisresultstore/filestore_test.go
+++ pkg/app/server/analysisresultstore/filestore_test.go
@@ -18,9 +18,9 @@ import (
 	"context"
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 
 	"github.com/pipe-cd/pipecd/pkg/filestore"
 	"github.com/pipe-cd/pipecd/pkg/filestore/filestoretest"
