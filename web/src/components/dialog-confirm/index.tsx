8c1cfb2595dcab61ceb06bef664c95c5fbf7f85c
kypham <hongkyhvnh@gmail.com>
2025-01-28 00:08:26 +0700

    Add loading attribute for Dialog Confirm
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/components/dialog-confirm/index.tsx
+++ web/src/components/dialog-confirm/index.tsx
@@ -1,10 +1,12 @@
 import {
   Button,
   Dialog,
+  CircularProgress,
   DialogActions,
   DialogContent,
   DialogTitle,
   DialogProps,
+  makeStyles,
 } from "@material-ui/core";
 import { FC } from "react";
 import { UI_TEXT_CANCEL, UI_TEXT_SAVE } from "~/constants/ui-text";
@@ -16,25 +18,52 @@ export type DialogConfirmProps = DialogProps & {
   description?: string;
   cancelText?: string;
   confirmText?: string;
+  loading?: boolean;
 };
+
+const useStyles = makeStyles((theme) => ({
+  progress: {
+    color: theme.palette.primary.main,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
 const DialogConfirm: FC<DialogConfirmProps> = ({
   onCancel,
+  onClose,
   onConfirm,
   title,
   description,
   cancelText = UI_TEXT_CANCEL,
   confirmText = UI_TEXT_SAVE,
+  loading = false,
   ...dialogProps
 }) => {
+  const classes = useStyles();
+
   return (
-    <Dialog {...dialogProps}>
+    <Dialog
+      onClose={(event, reason) => {
+        if (loading) return;
+        onClose?.(event, reason);
+      }}
+      {...dialogProps}
+    >
       <DialogTitle>{title}</DialogTitle>
       <DialogContent>{description}</DialogContent>
       <DialogActions>
-        <Button onClick={onCancel}>{cancelText}</Button>
-        <Button color="primary" onClick={onConfirm}>
+        <Button onClick={onCancel} disabled={loading}>
+          {cancelText}
+        </Button>
+        <Button color="primary" onClick={onConfirm} disabled={loading}>
           {confirmText}
+          {loading && (
+            <CircularProgress size={24} className={classes.progress} />
+          )}
         </Button>
       </DialogActions>
     </Dialog>
