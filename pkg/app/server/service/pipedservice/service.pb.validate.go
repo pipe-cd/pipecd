4eee290e354b4a51e3f956eee77a9814698293d2
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2025-02-06 21:19:08 +0700

    Add plugins to piped model (#5545)
    * Add plugins to piped model
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>
    
    * Update web test
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>
    
    ---------
    
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>

--- pkg/app/server/service/pipedservice/service.pb.validate.go
+++ pkg/app/server/service/pipedservice/service.pb.validate.go
@@ -339,6 +339,40 @@ func (m *ReportPipedMetaRequest) validate(all bool) error {
 
 	}
 
+	for idx, item := range m.GetPlugins() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ReportPipedMetaRequestValidationError{
+						field:  fmt.Sprintf("Plugins[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ReportPipedMetaRequestValidationError{
+						field:  fmt.Sprintf("Plugins[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ReportPipedMetaRequestValidationError{
+					field:  fmt.Sprintf("Plugins[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	for idx, item := range m.GetRepositories() {
 		_, _ = idx, item
 
