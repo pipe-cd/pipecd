ee20b27faa57d0f184a917b4f96b5273f6509af2
t-kikuc <tkikuchi07f@gmail.com>
2025-02-07 16:49:47 +0900

    clarify log messages
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/app/piped/eventwatcher/eventwatcher.go
+++ pkg/app/piped/eventwatcher/eventwatcher.go
@@ -603,7 +603,7 @@ func (w *watcher) updateValues(ctx context.Context, repo git.Repo, repoID string
 	retry := backoff.NewRetry(retryPushNum, backoff.NewConstant(retryPushInterval))
 	_, err = retry.Do(ctx, func() (interface{}, error) {
 		if err := tmpRepo.Push(ctx, tmpRepo.GetClonedBranch()); err != nil {
-			w.logger.Warn("failed to push commits", zap.String("repo-id", repoID), zap.String("branch", tmpRepo.GetClonedBranch()), zap.Error(err))
+			w.logger.Warn(fmt.Sprintf("failed to push commits. Retry attempt %d/%d", retry.Calls(), retryPushNum), zap.String("repo-id", repoID), zap.String("branch", tmpRepo.GetClonedBranch()), zap.Error(err))
 			return nil, err
 		}
 		return nil, nil
@@ -619,7 +619,7 @@ func (w *watcher) updateValues(ctx context.Context, repo git.Repo, repoID string
 
 	// If push fails because the local branch was not fresh, exit to retry again in the next interval.
 	if err == git.ErrBranchNotFresh {
-		w.logger.Warn("failed to push commits", zap.Error(err))
+		w.logger.Warn("failed to push commits: local branch was not up-to-date; will retry in the next cycle", zap.Error(err))
 		return nil
 	}
 
