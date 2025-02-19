eee3b0e55a927892762cc31ef1d1d159767e7d25
t-kikuc <tkikuchi07f@gmail.com>
2025-02-19 13:07:10 +0900

    copied from example plugin and implemented FetchDefinedStages()
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

---
+++ pkg/app/pipedv1/plugin/wait/plugin.go
@@ -0,0 +1,55 @@
+// Copyright 2025 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+
+	"github.com/pipe-cd/pipecd/pkg/plugin/logpersister"
+	"github.com/pipe-cd/pipecd/pkg/plugin/sdk"
+)
+
+const (
+	stageWait string = "WAIT"
+)
+
+type plugin struct{}
+
+type config struct{}
+
+// Name implements sdk.Plugin.
+func (p *plugin) Name() string {
+	return "wait"
+}
+
+// Version implements sdk.Plugin.
+func (p *plugin) Version() string {
+	return "0.0.1" // TODO
+}
+
+// BuildPipelineSyncStages implements sdk.PipelineSyncPlugin.
+func (p *plugin) BuildPipelineSyncStages(context.Context, *config, *sdk.Client, sdk.TODO) (sdk.TODO, error) {
+	return sdk.TODO{}, nil
+}
+
+// ExecuteStage implements sdk.PipelineSyncPlugin.
+func (p *plugin) ExecuteStage(context.Context, *config, sdk.DeployTargetsNone, *sdk.Client, logpersister.StageLogPersister, sdk.TODO) (sdk.TODO, error) {
+	return sdk.TODO{}, nil
+}
+
+// FetchDefinedStages implements sdk.PipelineSyncPlugin.
+func (p *plugin) FetchDefinedStages() []string {
+	return []string{stageWait}
+}
