eb4e34f05490ab56f58b57424eb435f2238e1ad5
Yoshiki Fujikane <ffjlabo@gmail.com>
2025-02-21 12:26:18 +0900

    Implement BuildPipelineSync
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>

---
+++ pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/pipeline.go
@@ -0,0 +1,125 @@
+// Copyright 202 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deployment
+
+import (
+	"slices"
+
+	"github.com/pipe-cd/pipecd/pkg/model"
+	"github.com/pipe-cd/pipecd/pkg/plugin/sdk"
+)
+
+type Stage string
+
+const (
+	// StageK8sSync represents the state where
+	// all resources should be synced with the Git state.
+	StageK8sSync Stage = "K8S_SYNC"
+	// StageK8sPrimaryRollout represents the state where
+	// the PRIMARY variant resources has been updated to the new version/configuration.
+	StageK8sPrimaryRollout Stage = "K8S_PRIMARY_ROLLOUT"
+	// StageK8sCanaryRollout represents the state where
+	// the CANARY variant resources has been rolled out with the new version/configuration.
+	StageK8sCanaryRollout Stage = "K8S_CANARY_ROLLOUT"
+	// StageK8sCanaryClean represents the state where
+	// the CANARY variant resources has been cleaned.
+	StageK8sCanaryClean Stage = "K8S_CANARY_CLEAN"
+	// StageK8sBaselineRollout represents the state where
+	// the BASELINE variant resources has been rolled out.
+	StageK8sBaselineRollout Stage = "K8S_BASELINE_ROLLOUT"
+	// StageK8sBaselineClean represents the state where
+	// the BASELINE variant resources has been cleaned.
+	StageK8sBaselineClean Stage = "K8S_BASELINE_CLEAN"
+	// StageK8sTrafficRouting represents the state where the traffic to application
+	// should be splitted as the specified percentage to PRIMARY, CANARY, BASELINE variants.
+	StageK8sTrafficRouting Stage = "K8S_TRAFFIC_ROUTING"
+	// StageK8sRollback represents the state where all deployed resources should be rollbacked.
+	StageK8sRollback Stage = "K8S_ROLLBACK"
+)
+
+var AllStages = []Stage{
+	StageK8sSync,
+	StageK8sPrimaryRollout,
+	StageK8sCanaryRollout,
+	StageK8sCanaryClean,
+	StageK8sBaselineRollout,
+	StageK8sBaselineClean,
+	StageK8sTrafficRouting,
+	StageK8sRollback,
+}
+
+func (s Stage) String() string {
+	return string(s)
+}
+
+const (
+	PredefinedStageK8sSync  = "K8sSync"
+	PredefinedStageRollback = "K8sRollback"
+)
+
+var predefinedStages = map[string]*model.PipelineStage{
+	PredefinedStageK8sSync: {
+		Id:       PredefinedStageK8sSync,
+		Name:     string(StageK8sSync),
+		Desc:     "Sync by applying all manifests",
+		Rollback: false,
+	},
+	PredefinedStageRollback: {
+		Id:       PredefinedStageRollback,
+		Name:     string(StageK8sRollback),
+		Desc:     "Rollback the deployment",
+		Rollback: true,
+	},
+}
+
+// GetPredefinedStage finds and returns the predefined stage for the given id.
+func GetPredefinedStage(id string) (*model.PipelineStage, bool) {
+	stage, ok := predefinedStages[id]
+	return stage, ok
+}
+
+func BuildPipelineStages(input *sdk.BuildPipelineSyncStagesInput) []sdk.PipelineStage {
+	out := make([]sdk.PipelineStage, 0, len(input.Request.Stages)+1)
+
+	for _, s := range input.Request.Stages {
+		stage := sdk.PipelineStage{
+			Index:              s.Index,
+			Name:               s.Name,
+			Rollback:           false,
+			Metadata:           make(map[string]string, 0),
+			AvailableOperation: sdk.ManualOperationNone,
+		}
+		out = append(out, stage)
+	}
+
+	if input.Request.Rollback {
+		// we set the index of the rollback stage to the minimum index of all stages.
+		minIndex := slices.MinFunc(out, func(a, b sdk.PipelineStage) int {
+			return int(a.Index - b.Index)
+		}).Index
+
+		s, _ := GetPredefinedStage(PredefinedStageRollback)
+		// we copy the predefined stage to avoid modifying the original one.
+		out = append(out, sdk.PipelineStage{
+			Index:              minIndex,
+			Name:               s.Name,
+			Rollback:           true,
+			Metadata:           make(map[string]string, 0),
+			AvailableOperation: sdk.ManualOperationNone,
+		})
+	}
+
+	return out
+}
---
+++ pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/pipeline_test.go
@@ -0,0 +1,120 @@
+// Copyright 2024 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deployment
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pipe-cd/pipecd/pkg/plugin/sdk"
+)
+
+func TestBuildPipelineStages(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    *sdk.BuildPipelineSyncStagesInput
+		expected []sdk.PipelineStage
+	}{
+		{
+			name: "without auto rollback",
+			input: &sdk.BuildPipelineSyncStagesInput{
+				Request: sdk.BuildPipelineSyncStagesRequest{
+					Rollback: false,
+					Stages: []sdk.StageConfig{
+						{
+							Index:  0,
+							Name:   "Stage 1",
+							Config: []byte(""),
+						},
+						{
+							Index:  1,
+							Name:   "Stage 2",
+							Config: []byte(""),
+						},
+					},
+				},
+			},
+			expected: []sdk.PipelineStage{
+				{
+					Index:              0,
+					Name:               "Stage 1",
+					Rollback:           false,
+					Metadata:           make(map[string]string, 0),
+					AvailableOperation: sdk.ManualOperationNone,
+				},
+				{
+					Index:              1,
+					Name:               "Stage 2",
+					Rollback:           false,
+					Metadata:           make(map[string]string, 0),
+					AvailableOperation: sdk.ManualOperationNone,
+				},
+			},
+		},
+		{
+			name: "with auto rollback",
+			input: &sdk.BuildPipelineSyncStagesInput{
+				Request: sdk.BuildPipelineSyncStagesRequest{
+					Rollback: true,
+					Stages: []sdk.StageConfig{
+						{
+							Index:  0,
+							Name:   "Stage 1",
+							Config: []byte(""),
+						},
+						{
+							Index:  1,
+							Name:   "Stage 2",
+							Config: []byte(""),
+						},
+					},
+				},
+			},
+			expected: []sdk.PipelineStage{
+				{
+					Index:              0,
+					Name:               "Stage 1",
+					Rollback:           false,
+					Metadata:           make(map[string]string, 0),
+					AvailableOperation: sdk.ManualOperationNone,
+				},
+				{
+					Index:              1,
+					Name:               "Stage 2",
+					Rollback:           false,
+					Metadata:           make(map[string]string, 0),
+					AvailableOperation: sdk.ManualOperationNone,
+				},
+				{
+					Index:              0,
+					Name:               string(StageK8sRollback),
+					Rollback:           true,
+					Metadata:           make(map[string]string, 0),
+					AvailableOperation: sdk.ManualOperationNone,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := BuildPipelineStages(tt.input)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
