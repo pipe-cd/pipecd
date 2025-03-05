95ee25129c1c73cb5411909c3474893994daed5a
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-03-06 08:49:27 +0900

    Implement GetLivestate method in the LivestatePlugin interface
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- pkg/plugin/sdk/livestate.go
+++ pkg/plugin/sdk/livestate.go
@@ -23,6 +23,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/pipe-cd/pipecd/pkg/model"
 	"github.com/pipe-cd/pipecd/pkg/plugin/api/v1alpha1/livestate"
 )
 
@@ -45,7 +46,7 @@ type LivestatePlugin[Config, DeployTargetConfig any] interface {
 	// GetLivestate returns the live state of the resources in the given application.
 	// It returns the resources' live state and the difference between the desired state and the live state.
 	// It's allowed to return only the resources' live state if the difference is not available, or only the difference if the live state is not available.
-	GetLivestate(context.Context, *Config, []*DeployTarget[DeployTargetConfig], TODO) (TODO, error)
+	GetLivestate(context.Context, *Config, []*DeployTarget[DeployTargetConfig], *GetLivestateInput) (*GetLivestateResponse, error)
 }
 
 // LivestatePluginServer is a wrapper for LivestatePlugin to satisfy the LivestateServiceServer interface.
@@ -110,5 +111,74 @@ func (s *LivestatePluginServer[Config, DeployTargetConfig]) setFields(fields com
 
 // GetLivestate returns the live state of the resources in the given application.
 func (s *LivestatePluginServer[Config, DeployTargetConfig]) GetLivestate(ctx context.Context, request *livestate.GetLivestateRequest) (*livestate.GetLivestateResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetLivestate not implemented")
+	// Get the deploy targets set on the deployment from the piped plugin config.
+	deployTargets := make([]*DeployTarget[DeployTargetConfig], 0, len(request.GetDeployTargets()))
+	for _, name := range request.GetDeployTargets() {
+		dt, ok := s.deployTargets[name]
+		if !ok {
+			return nil, status.Errorf(codes.Internal, "the deploy target %s is not found in the piped plugin config", name)
+		}
+
+		deployTargets = append(deployTargets, dt)
+	}
+
+	client := &Client{
+		base:          s.client,
+		pluginName:    s.Name(),
+		applicationID: request.GetApplicationId(),
+		toolRegistry:  s.toolRegistry,
+	}
+
+	response, err := s.base.GetLivestate(ctx, &s.config, deployTargets, &GetLivestateInput{
+		Request: GetLivestateRequest{
+			ApplicationID:    request.ApplicationId,
+			DeploymentSource: newDeploymentSource(request.GetDeploySource()),
+		},
+		Client: client,
+		Logger: s.logger,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to get the live state: %v", err)
+	}
+
+	return response.toModel(), nil
+}
+
+type GetLivestateInput struct {
+	Request GetLivestateRequest
+	Client  *Client
+	Logger  *zap.Logger
+}
+
+type GetLivestateRequest struct {
+	// ApplicationID is the ID of the application.
+	ApplicationID string
+	// DeploymentSource is the source of the deployment.
+	DeploymentSource DeploymentSource
+}
+
+type GetLivestateResponse struct {
+	LiveState ApplicationLiveState
+	SyncState ApplicationSyncState
+}
+
+func (r *GetLivestateResponse) toModel() *livestate.GetLivestateResponse {
+	return &livestate.GetLivestateResponse{
+		ApplicationLiveState: r.LiveState.toModel(),
+		SyncState:            r.SyncState.toModel(),
+	}
+}
+
+type ApplicationLiveState struct {
+}
+
+func (s *ApplicationLiveState) toModel() *model.ApplicationLiveState {
+	return nil
+}
+
+type ApplicationSyncState struct {
+}
+
+func (s *ApplicationSyncState) toModel() *model.ApplicationSyncState {
+	return nil
 }
