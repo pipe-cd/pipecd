992f17de441c6739be81f656dabd3df2e280a673
Yoshiki Fujikane <40124947+ffjlabo@users.noreply.github.com>
2025-03-06 18:44:18 +0900

    Fix for lint (#5629)
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>

--- pkg/app/pipedv1/plugin/wait/wait.go
+++ pkg/app/pipedv1/plugin/wait/wait.go
@@ -77,7 +77,7 @@ func wait(ctx context.Context, duration time.Duration, initialStart time.Time, s
 
 		case <-ctx.Done(): // on cancelled
 			slp.Info("Wait cancelled")
-			// We can return any status here because the piped handles this case as cancelled by a user, 
+			// We can return any status here because the piped handles this case as cancelled by a user,
 			// ignoring the result from a plugin.
 			return sdk.StageStatusFailure
 		}
