1f3aa2b1a41cad39f346a532d7a4ae337eb6c8de
Yoshiki Fujikane <ffjlabo@gmail.com>
2025-03-07 11:36:04 +0900

    Prune resources
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>

--- pkg/app/pipedv1/plugin/kubernetes/deployment/plugin.go
+++ pkg/app/pipedv1/plugin/kubernetes/deployment/plugin.go
@@ -211,6 +211,23 @@ func (p *Plugin) executeK8sSyncStage(ctx context.Context, input *sdk.ExecuteStag
 		return sdk.StageStatusSuccess
 	}
 
+	lp.Infof("Start pruning %d resources", len(removeKeys))
+	var deletedCount int
+	for _, key := range removeKeys {
+		if err := kubectl.Delete(ctx, deployTargetConfig.KubeConfigPath, key.Namespace(), key); err != nil {
+			if errors.Is(err, provider.ErrNotFound) {
+				lp.Infof("Specified resource does not exist, so skip deleting the resource: %s (%v)", key.ReadableString(), err)
+				continue
+			}
+			lp.Errorf("Failed while deleting resource %s (%v)", key.ReadableString(), err)
+			continue // continue to delete other resources
+		}
+		deletedCount++
+		lp.Successf("- deleted resource: %s", key.ReadableString())
+	}
+
+	lp.Successf("Successfully deleted %d resources", deletedCount)
+
 	return sdk.StageStatusSuccess
 }
 
