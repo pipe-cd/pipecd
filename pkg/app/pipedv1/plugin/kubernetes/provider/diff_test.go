317a7c9385705cde56f2354d538bcd40bf00fe53
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-03-17 16:18:08 +0900

    Implement calculate diffs and rendering diffs of k8s (#5674)
    * Implement the diff list rendering.
    
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>
    
    * Add test for DiffListResult.Render
    
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>
    
    ---------
    
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- pkg/app/pipedv1/plugin/kubernetes/provider/diff_test.go
+++ pkg/app/pipedv1/plugin/kubernetes/provider/diff_test.go
@@ -15,6 +15,7 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,6 +23,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/pipe-cd/pipecd/pkg/plugin/diff"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func TestDiff(t *testing.T) {
@@ -234,6 +236,559 @@ spec:
 				assert.Equal(t, tc.diffNum, result.NumNodes())
 				assert.Equal(t, tc.expected, ds)
 			}
+		})
+	}
+}
+
+func TestDiffList(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		name     string
+		live     string
+		desired  string
+		wantAdds int
+		wantDels int
+		wantMods int
+	}{
+		{
+			name: "no changes",
+			live: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment
+spec:
+  replicas: 3`,
+			desired: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment
+spec:
+  replicas: 3`,
+			wantAdds: 0,
+			wantDels: 0,
+			wantMods: 0,
+		},
+		{
+			name: "one addition",
+			live: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+spec:
+  replicas: 3`,
+			desired: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+spec:
+  replicas: 3
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2
+spec:
+  replicas: 3`,
+			wantAdds: 1,
+			wantDels: 0,
+			wantMods: 0,
+		},
+		{
+			name: "one deletion",
+			live: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+spec:
+  replicas: 3
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2
+spec:
+  replicas: 3`,
+			desired: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+spec:
+  replicas: 3`,
+			wantAdds: 0,
+			wantDels: 1,
+			wantMods: 0,
+		},
+		{
+			name: "one modification",
+			live: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment
+spec:
+  replicas: 3`,
+			desired: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment
+spec:
+  replicas: 5`,
+			wantAdds: 0,
+			wantDels: 0,
+			wantMods: 1,
+		},
+		{
+			name: "mixed changes",
+			live: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+spec:
+  replicas: 3
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2
+spec:
+  replicas: 3`,
+			desired: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+spec:
+  replicas: 5
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-3
+spec:
+  replicas: 3`,
+			wantAdds: 1,
+			wantDels: 1,
+			wantMods: 1,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			liveManifests, err := ParseManifests(tc.live)
+			require.NoError(t, err)
+			desiredManifests, err := ParseManifests(tc.desired)
+			require.NoError(t, err)
+
+			result, err := DiffList(liveManifests, desiredManifests, zap.NewNop(), diff.WithEquateEmpty(), diff.WithIgnoreAddingMapKeys(), diff.WithCompareNumberAndNumericString())
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.wantAdds, len(result.Adds))
+			assert.Equal(t, tc.wantDels, len(result.Deletes))
+			assert.Equal(t, tc.wantMods, len(result.Changes))
+			assert.Equal(t, tc.wantAdds+tc.wantDels+tc.wantMods == 0, result.NoChanges())
+			assert.Equal(t, tc.wantAdds+tc.wantDels+tc.wantMods, result.TotalOutOfSync())
+		})
+	}
+}
+
+func TestGroupManifests(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		name        string
+		olds        string
+		news        string
+		wantAdds    int
+		wantDeletes int
+		wantChanges int
+	}{
+		{
+			name:        "empty lists",
+			olds:        "",
+			news:        "",
+			wantAdds:    0,
+			wantDeletes: 0,
+			wantChanges: 0,
+		},
+		{
+			name: "only additions",
+			olds: "",
+			news: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2`,
+			wantAdds:    2,
+			wantDeletes: 0,
+			wantChanges: 0,
+		},
+		{
+			name: "only deletions",
+			olds: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2`,
+			news:        "",
+			wantAdds:    0,
+			wantDeletes: 2,
+			wantChanges: 0,
+		},
+		{
+			name: "only changes",
+			olds: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2`,
+			news: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2`,
+			wantAdds:    0,
+			wantDeletes: 0,
+			wantChanges: 2,
+		},
+		{
+			name: "mixed changes",
+			olds: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2`,
+			news: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-2
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-deployment-3`,
+			wantAdds:    1,
+			wantDeletes: 1,
+			wantChanges: 1,
+		},
+		{
+			name: "different resource types with same name",
+			olds: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: test-1`,
+			news: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: test-1`,
+			wantAdds:    0,
+			wantDeletes: 0,
+			wantChanges: 2,
+		},
+		{
+			name: "different namespaces with same name",
+			olds: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+  namespace: ns1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+  namespace: ns2`,
+			news: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+  namespace: ns1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+  namespace: ns2`,
+			wantAdds:    0,
+			wantDeletes: 0,
+			wantChanges: 2,
+		},
+		{
+			name: "old list larger than new list",
+			olds: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-2
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-3`,
+			news: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-2`,
+			wantAdds:    0,
+			wantDeletes: 2,
+			wantChanges: 1,
+		},
+		{
+			name: "new list larger than old list",
+			olds: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-2`,
+			news: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-1
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-2
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: test-3`,
+			wantAdds:    2,
+			wantDeletes: 0,
+			wantChanges: 1,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			var olds, news []Manifest
+			var err error
+
+			if tc.olds != "" {
+				olds, err = ParseManifests(tc.olds)
+				require.NoError(t, err)
+			}
+
+			if tc.news != "" {
+				news, err = ParseManifests(tc.news)
+				require.NoError(t, err)
+			}
+
+			adds, deletes, newChanges, oldChanges := groupManifests(olds, news)
+			assert.Equal(t, tc.wantAdds, len(adds))
+			assert.Equal(t, tc.wantDeletes, len(deletes))
+			assert.Equal(t, tc.wantChanges, len(newChanges))
+			assert.Equal(t, len(newChanges), len(oldChanges))
+		})
+	}
+}
+
+func TestDiffListResult_Render(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		name     string
+		result   *DiffListResult
+		opt      DiffRenderOptions
+		expected string
+	}{
+		{
+			name: "empty result",
+			result: &DiffListResult{
+				Adds:    []Manifest{},
+				Deletes: []Manifest{},
+				Changes: []DiffListChange{},
+			},
+			opt:      DiffRenderOptions{},
+			expected: "",
+		},
+		{
+			name: "only deletes",
+			result: &DiffListResult{
+				Deletes: []Manifest{
+					{
+						body: &unstructured.Unstructured{
+							Object: map[string]interface{}{
+								"apiVersion": "apps/v1",
+								"kind":       "Deployment",
+								"metadata": map[string]interface{}{
+									"name":      "test-deployment",
+									"namespace": "default",
+								},
+							},
+						},
+					},
+				},
+			},
+			opt:      DiffRenderOptions{},
+			expected: `- 1. name="test-deployment", kind="Deployment", namespace="default", apiGroup="apps"` + "\n\n",
+		},
+		{
+			name: "only adds",
+			result: &DiffListResult{
+				Adds: []Manifest{
+					{
+						body: &unstructured.Unstructured{
+							Object: map[string]interface{}{
+								"apiVersion": "apps/v1",
+								"kind":       "Deployment",
+								"metadata": map[string]interface{}{
+									"name":      "test-deployment",
+									"namespace": "default",
+								},
+							},
+						},
+					},
+				},
+			},
+			opt:      DiffRenderOptions{},
+			expected: `+ 1. name="test-deployment", kind="Deployment", namespace="default", apiGroup="apps"` + "\n\n",
+		},
+		{
+			name: "with changes",
+			result: func(t *testing.T) *DiffListResult {
+				old := Manifest{
+					body: &unstructured.Unstructured{
+						Object: map[string]interface{}{
+							"apiVersion": "v1",
+							"kind":       "Secret",
+							"metadata": map[string]interface{}{
+								"name":      "test-secret",
+								"namespace": "default",
+							},
+							"data": map[string]interface{}{
+								"password": "old-password",
+							},
+						},
+					},
+				}
+				new := Manifest{
+					body: &unstructured.Unstructured{
+						Object: map[string]interface{}{
+							"apiVersion": "v1",
+							"kind":       "Secret",
+							"metadata": map[string]interface{}{
+								"name":      "test-secret",
+								"namespace": "default",
+							},
+							"data": map[string]interface{}{
+								"password": "new-password",
+							},
+						},
+					},
+				}
+				diffResult, err := Diff(old, new, zap.NewNop(), diff.WithEquateEmpty(), diff.WithIgnoreAddingMapKeys())
+				if err != nil {
+					t.Fatal(err)
+				}
+				return &DiffListResult{
+					Changes: []DiffListChange{
+						{
+							Old:  old,
+							New:  new,
+							Diff: diffResult,
+						},
+					},
+				}
+			}(t),
+			opt: DiffRenderOptions{
+				MaskSecret: true,
+			},
+			expected: `# 1. name="test-secret", kind="Secret", namespace="default", apiGroup=""` + "\n\n  #data\n- data: *****\n+ data: *****\n\n\n",
+		},
+		{
+			name: "with max changed manifests",
+			result: func(t *testing.T) *DiffListResult {
+				changes := make([]DiffListChange, 0, 2)
+				for i := 1; i <= 2; i++ {
+					old := Manifest{
+						body: &unstructured.Unstructured{
+							Object: map[string]interface{}{
+								"apiVersion": "v1",
+								"kind":       "ConfigMap",
+								"metadata": map[string]interface{}{
+									"name":      fmt.Sprintf("test-cm-%d", i),
+									"namespace": "default",
+								},
+								"data": map[string]interface{}{
+									"key": "value1",
+								},
+							},
+						},
+					}
+					new := Manifest{
+						body: &unstructured.Unstructured{
+							Object: map[string]interface{}{
+								"apiVersion": "v1",
+								"kind":       "ConfigMap",
+								"metadata": map[string]interface{}{
+									"name":      fmt.Sprintf("test-cm-%d", i),
+									"namespace": "default",
+								},
+								"data": map[string]interface{}{
+									"key": "value2",
+								},
+							},
+						},
+					}
+					diffResult, err := Diff(old, new, zap.NewNop(), diff.WithEquateEmpty(), diff.WithIgnoreAddingMapKeys())
+					if err != nil {
+						t.Fatal(err)
+					}
+					changes = append(changes, DiffListChange{
+						Old:  old,
+						New:  new,
+						Diff: diffResult,
+					})
+				}
+				return &DiffListResult{
+					Changes: changes,
+				}
+			}(t),
+			opt: DiffRenderOptions{
+				MaxChangedManifests: 1,
+				MaskConfigMap:       true,
+			},
+			expected: `# 1. name="test-cm-1", kind="ConfigMap", namespace="default", apiGroup=""` + "\n\n  data:\n    #data.key\n-   key: *****\n+   key: *****\n\n\n... (omitted 1 other changed manifests)\n",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.result.Render(tc.opt)
+			assert.Equal(t, tc.expected, got)
 		})
 	}
 }
