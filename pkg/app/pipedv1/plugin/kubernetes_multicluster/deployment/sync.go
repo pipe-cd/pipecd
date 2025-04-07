e48ebf69e5b79061e4d1b496eb4d347f9c987887
Yoshiki Fujikane <ffjlabo@gmail.com>
2025-04-07 15:19:34 +0900

    Modify loadManifests to allow multiTarget
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>

--- pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/sync.go
+++ pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/sync.go
@@ -117,7 +117,7 @@ func (p *Plugin) sync(
 	loader := provider.NewLoader(toolRegistry)
 
 	lp.Infof("Loading manifests at commit %s for handling", input.Request.TargetDeploymentSource.CommitHash)
-	manifests, err := p.loadManifests(ctx, &input.Request.Deployment, cfg.Spec, &input.Request.TargetDeploymentSource, loader)
+	manifests, err := p.loadManifests(ctx, &input.Request.Deployment, cfg.Spec, &input.Request.TargetDeploymentSource, loader, multiTarget)
 	if err != nil {
 		lp.Errorf("Failed while loading manifests (%v)", err)
 		return sdk.StageStatusFailure
