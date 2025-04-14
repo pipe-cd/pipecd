7d343618a32afd9e6cf96310c26dea63070badc3
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-04-14 10:16:28 +0900

    Add feature to check the pipeline has specific stage
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- pkg/plugin/sdk/config.go
+++ pkg/plugin/sdk/config.go
@@ -14,7 +14,11 @@
 
 package sdk
 
-import config "github.com/pipe-cd/pipecd/pkg/configv1"
+import (
+	"slices"
+
+	config "github.com/pipe-cd/pipecd/pkg/configv1"
+)
 
 // Spec[T] represents both of follows
 // - the type is pointer type of T
@@ -29,4 +33,35 @@ func LoadConfigSpec[T Spec[RT], RT any](ds DeploymentSource) (T, error) {
 		return nil, err
 	}
 	return cfg.Spec, nil
+}
+
+// LoadStages loads the pipeline stages from the application config.
+func LoadStages(ds DeploymentSource) (Stages, error) {
+	cfg, err := config.DecodeYAML[*config.GenericApplicationSpec](ds.ApplicationConfig)
+	if err != nil {
+		return Stages{}, err
+	}
+
+	// If the pipeline is not defined, return an empty list.
+	if cfg == nil || cfg.Spec == nil || cfg.Spec.Pipeline == nil || len(cfg.Spec.Pipeline.Stages) == 0 {
+		return Stages{}, nil
+	}
+
+	// Convert the pipeline stages to a list of stage names.
+	stages := make([]string, 0, len(cfg.Spec.Pipeline.Stages))
+	for _, s := range cfg.Spec.Pipeline.Stages {
+		stages = append(stages, string(s.Name))
+	}
+	return Stages{stages: stages}, nil
+}
+
+// Stages is a list of pipeline stages.
+// It's intended to use for checking if a stage is included in the pipeline.
+type Stages struct {
+	stages []string
+}
+
+// Has checks if the given stage is included in the list.
+func (s Stages) Has(stage string) bool {
+	return slices.Contains(s.stages, stage)
 }
