065f2545b0629930000896c3a8908d4613caeaed
kypham <hongkyhvnh@gmail.com>
2025-05-15 15:42:43 +0700

    remove unused makeStyles styles-button
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/styles/button.tsx
+++ web/src/styles/button.tsx
@@ -1,16 +1,4 @@
 import { CircularProgress, CircularProgressProps, styled } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-
-export const useStyles = makeStyles((theme) => ({
-  progress: {
-    color: theme.palette.primary.main,
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12,
-  },
-}));
 
 export const SpinnerIcon = styled(
   ({ size = 24, ...props }: CircularProgressProps) => (
