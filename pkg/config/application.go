f8c663fb87a5d03adbefab1f1ff716f16571faf1
khanhtc1202 <khanhtc1202@gmail.com>
2025-05-15 19:18:53 +0900

    Enable pipedv0 to parse app config with plugins
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>

--- pkg/config/application.go
+++ pkg/config/application.go
@@ -58,6 +58,10 @@ type GenericApplicationSpec struct {
 	EventWatcher []EventWatcherConfig `json:"eventWatcher"`
 	// Configuration for drift detection
 	DriftDetection *DriftDetection `json:"driftDetection"`
+	// NOTE: Do not use the field, only added here to avoid unmarshalling error.
+	// List of the configuration for plugin
+	// This field is plugin-specific, so intentionally restrict the access for the actual value here and decode it on the SDK side.
+	Plugins map[string]struct{} `json:"plugins"`
 }
 
 type DeploymentPlanner struct {
