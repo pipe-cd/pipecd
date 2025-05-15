7370e89ba4f591ff49468431d1631fc316483069
kypham <hongkyhvnh@gmail.com>
2025-05-15 17:54:15 +0700

    replace some deprecated propd Mui component
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/components/settings-page/piped/components/piped-table-row.tsx
+++ web/src/components/settings-page/piped/components/piped-table-row.tsx
@@ -51,12 +51,6 @@ interface Props {
 }
 
 const ITEM_HEIGHT = 48;
-const menuStyle = {
-  style: {
-    maxHeight: ITEM_HEIGHT * 5.5,
-    width: "25ch",
-  },
-};
 
 export const PipedTableRow: FC<Props> = memo(function PipedTableRow({
   pipedId,
@@ -221,7 +215,14 @@ export const PipedTableRow: FC<Props> = memo(function PipedTableRow({
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
         onClose={handleMenuClose}
-        PaperProps={menuStyle}
+        slotProps={{
+          paper: {
+            style: {
+              maxHeight: ITEM_HEIGHT * 5.5,
+              width: "25ch",
+            },
+          },
+        }}
       >
         {piped.disabled ? (
           <MenuItem onClick={handleEnable}>{UI_TEXT_ENABLE}</MenuItem>
