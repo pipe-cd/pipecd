3231c81d4ff267e0a137366e12c74dc379d2ceac
kypham <hongkyhvnh@gmail.com>
2025-05-15 14:32:39 +0700

    replace makeStyles in deployment-chains-page
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/components/deployment-chains-page/index.tsx
+++ web/src/components/deployment-chains-page/index.tsx
@@ -1,33 +1,13 @@
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Divider,
-  Link,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
+import { Box, Button, Divider, Link, Toolbar, Typography } from "@mui/material";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import { FC } from "react";
 import { UI_TEXT_REFRESH } from "~/constants/ui-text";
-import { useStyles as useButtonStyles } from "~/styles/button";
+import { SpinnerIcon } from "~/styles/button";
 
 const COMING_SOON_MESSAGE = "This UI is under development";
 const FEATURE_STATUS_INTRO = "PipeCD feature status";
 
-const useStyles = makeStyles(() => ({
-  container: {
-    flex: 1,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-}));
-
 export const DeploymentChainsIndexPage: FC = () => {
-  const classes = useStyles();
-  const buttonClasses = useButtonStyles();
   const isLoading = false;
 
   return (
@@ -40,13 +20,19 @@ export const DeploymentChainsIndexPage: FC = () => {
           disabled={isLoading}
         >
           {UI_TEXT_REFRESH}
-          {isLoading && (
-            <CircularProgress size={24} className={buttonClasses.progress} />
-          )}
+          {isLoading && <SpinnerIcon />}
         </Button>
       </Toolbar>
       <Divider />
-      <Box flexDirection="column" className={classes.container}>
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
         <Typography variant="body1">{COMING_SOON_MESSAGE}</Typography>
         <Link
           href="https://pipecd.dev/docs/feature-status/"
