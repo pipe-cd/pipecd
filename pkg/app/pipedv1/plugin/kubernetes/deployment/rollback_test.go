05310bb4570fee69874334dcaad2698e82ca41c9
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-05-27 15:12:54 +0900

    Use new import paths for toolregistrytest in k8s
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- pkg/app/pipedv1/plugin/kubernetes/deployment/rollback_test.go
+++ pkg/app/pipedv1/plugin/kubernetes/deployment/rollback_test.go
@@ -25,9 +25,9 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	kubeConfigPkg "github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/kubernetes/config"
-	"github.com/pipe-cd/pipecd/pkg/plugin/toolregistry/toolregistrytest"
 	sdk "github.com/pipe-cd/piped-plugin-sdk-go"
 	"github.com/pipe-cd/piped-plugin-sdk-go/logpersister/logpersistertest"
+	"github.com/pipe-cd/piped-plugin-sdk-go/toolregistry/toolregistrytest"
 )
 
 func TestPlugin_executeK8sRollbackStage_NoPreviousDeployment(t *testing.T) {
