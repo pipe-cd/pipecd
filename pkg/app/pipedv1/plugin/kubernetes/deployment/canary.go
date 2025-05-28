7293e810becd0bca9e2842be3dba3765af8007a3
Yoshiki Fujikane <ffjlabo@gmail.com>
2025-05-28 17:37:22 +0900

    Add patchManifests function
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>

--- pkg/app/pipedv1/plugin/kubernetes/deployment/canary.go
+++ pkg/app/pipedv1/plugin/kubernetes/deployment/canary.go
@@ -16,9 +16,12 @@ package deployment
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/kubernetes/config"
 	kubeconfig "github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/kubernetes/config"
 	"github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/kubernetes/provider"
+	"github.com/pipe-cd/pipecd/pkg/yamlprocessor"
 	sdk "github.com/pipe-cd/piped-plugin-sdk-go"
 )
 
@@ -52,4 +55,122 @@ func findSecretManifests(manifests []provider.Manifest) []provider.Manifest {
 		out = append(out, m)
 	}
 	return out
+}
+
+type patcher func(m provider.Manifest, cfg config.K8sResourcePatch) (*provider.Manifest, error)
+
+func patchManifests(manifests []provider.Manifest, patches []config.K8sResourcePatch, patcher patcher) ([]provider.Manifest, error) {
+	if len(patches) == 0 {
+		return manifests, nil
+	}
+
+	out := make([]provider.Manifest, len(manifests))
+	copy(out, manifests)
+
+	for _, p := range patches {
+		target := -1
+		for i, m := range out {
+			if m.Key().Kind() != p.Target.Kind {
+				continue
+			}
+			if m.Key().Name() != p.Target.Name {
+				continue
+			}
+			target = i
+			break
+		}
+		if target < 0 {
+			return nil, fmt.Errorf("no manifest matches the given patch: kind=%s, name=%s", p.Target.Kind, p.Target.Name)
+		}
+		patched, err := patcher(out[target], p)
+		if err != nil {
+			return nil, fmt.Errorf("failed to patch manifest: %s, error: %w", out[target].Key(), err)
+		}
+		out[target] = *patched
+	}
+
+	return out, nil
+}
+
+func patchManifest(m provider.Manifest, patch config.K8sResourcePatch) (*provider.Manifest, error) {
+	if len(patch.Ops) == 0 {
+		return &m, nil
+	}
+
+	fullBytes, err := m.YamlBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	process := func(bytes []byte) ([]byte, error) {
+		p, err := yamlprocessor.NewProcessor(bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, o := range patch.Ops {
+			switch o.Op {
+			case config.K8sResourcePatchOpYAMLReplace:
+				if err := p.ReplaceString(o.Path, o.Value); err != nil {
+					return nil, fmt.Errorf("failed to replace value at path: %s, error: %w", o.Path, err)
+				}
+			default:
+				// TODO: Support more patch operation for K8sCanaryRolloutStageOptions.
+				return nil, fmt.Errorf("%s operation is not supported currently", o.Op)
+			}
+		}
+
+		return p.Bytes(), nil
+	}
+
+	buildManifest := func(bytes []byte) (*provider.Manifest, error) {
+		manifests, err := provider.ParseManifests(string(bytes))
+		if err != nil {
+			return nil, err
+		}
+		if len(manifests) != 1 {
+			return nil, fmt.Errorf("unexpected number of manifests, expected 1, got %d", len(manifests))
+		}
+		return &manifests[0], nil
+	}
+
+	// When the target is the whole manifest,
+	// just pass full bytes to process and build a new manifest based on the returned data.
+	root := patch.Target.DocumentRoot
+	if root == "" {
+		out, err := process(fullBytes)
+		if err != nil {
+			return nil, err
+		}
+		return buildManifest(out)
+	}
+
+	// When the target is a manifest field specified by documentRoot,
+	// we have to extract that field value as a string.
+	p, err := yamlprocessor.NewProcessor(fullBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	v, err := p.GetValue(root)
+	if err != nil {
+		return nil, err
+	}
+	sv, ok := v.(string)
+	if !ok {
+		return nil, fmt.Errorf("the value for the specified root %s must be a string", root)
+	}
+
+	// And process that field data.
+	out, err := process([]byte(sv))
+	if err != nil {
+		return nil, err
+	}
+
+	// Then rewrite the new data into the specified root.
+	if err := p.ReplaceString(root, string(out)); err != nil {
+		return nil, err
+	}
+
+	return buildManifest(p.Bytes())
 }
--- pkg/app/pipedv1/plugin/kubernetes/deployment/canary_test.go
+++ pkg/app/pipedv1/plugin/kubernetes/deployment/canary_test.go
@@ -1,11 +1,14 @@
 package deployment
 
 import (
+	"errors"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	"github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/kubernetes/config"
 	"github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/kubernetes/provider"
 )
 
@@ -152,6 +155,289 @@ data:
 
 			got := findSecretManifests(tt.manifests)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_patchManifest(t *testing.T) {
+	t.Parallel()
+
+	testcases := []struct {
+		name          string
+		manifests     string
+		patch         config.K8sResourcePatch
+		expectedError error
+	}{
+		{
+			name:      "one op",
+			manifests: "testdata/patch_manifest/patch_configmap.yaml",
+			patch: config.K8sResourcePatch{
+				Ops: []config.K8sResourcePatchOp{
+					{
+						Op:    config.K8sResourcePatchOpYAMLReplace,
+						Path:  "$.data.key1",
+						Value: "value-1",
+					},
+				},
+			},
+		},
+		{
+			name:      "multi ops",
+			manifests: "testdata/patch_manifest/patch_configmap_multi_ops.yaml",
+			patch: config.K8sResourcePatch{
+				Ops: []config.K8sResourcePatchOp{
+					{
+						Op:    config.K8sResourcePatchOpYAMLReplace,
+						Path:  "$.data.key1",
+						Value: "value-1",
+					},
+					{
+						Op:    config.K8sResourcePatchOpYAMLReplace,
+						Path:  "$.data.key2",
+						Value: "value-2",
+					},
+				},
+			},
+		},
+		{
+			name:      "one op with a given field",
+			manifests: "testdata/patch_manifest/patch_configmap_field.yaml",
+			patch: config.K8sResourcePatch{
+				Target: config.K8sResourcePatchTarget{
+					DocumentRoot: "$.data.envoy-config",
+				},
+				Ops: []config.K8sResourcePatchOp{
+					{
+						Op:    config.K8sResourcePatchOpYAMLReplace,
+						Path:  "$.admin.address.socket_address.port_value",
+						Value: "9096",
+					},
+				},
+			},
+		},
+		{
+			name:      "multi ops with a given field",
+			manifests: "testdata/patch_manifest/patch_configmap_field_multi_ops.yaml",
+			patch: config.K8sResourcePatch{
+				Target: config.K8sResourcePatchTarget{
+					DocumentRoot: "$.data.envoy-config",
+				},
+				Ops: []config.K8sResourcePatchOp{
+					{
+						Op:    config.K8sResourcePatchOpYAMLReplace,
+						Path:  "$.admin.address.socket_address.port_value",
+						Value: "19095",
+					},
+					{
+						Op:    config.K8sResourcePatchOpYAMLReplace,
+						Path:  "$.static_resources.clusters[1].load_assignment.endpoints[0].lb_endpoints[0].endpoint.address.socket_address.port_value",
+						Value: "19081",
+					},
+					{
+						Op:    config.K8sResourcePatchOpYAMLReplace,
+						Path:  "$.static_resources.clusters[1].type",
+						Value: "DNS",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			manifests, err := provider.LoadManifestsFromYAMLFile(tc.manifests)
+			require.NoError(t, err)
+
+			if tc.expectedError == nil {
+				require.Equal(t, 2, len(manifests))
+			} else {
+				require.Equal(t, 1, len(manifests))
+			}
+
+			got, err := patchManifest(manifests[0], tc.patch)
+			require.Equal(t, tc.expectedError, err)
+
+			expectedBytes, err := manifests[1].YamlBytes()
+			require.NoError(t, err)
+
+			gotBytes, err := got.YamlBytes()
+			require.NoError(t, err)
+
+			if tc.expectedError == nil {
+				assert.Equal(t, string(expectedBytes), string(gotBytes))
+			}
+		})
+	}
+}
+
+func Test_patchManifests(t *testing.T) {
+	testcases := []struct {
+		name          string
+		manifests     []provider.Manifest
+		patches       []config.K8sResourcePatch
+		expected      []provider.Manifest
+		expectedError error
+	}{
+		{
+			name: "no patches",
+			manifests: mustParseManifests(t, strings.TrimSpace(`
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: deployment-1
+spec:
+  template:
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.19.3
+        env:
+        - name: VALUE
+          value: none
+`)),
+			patches: []config.K8sResourcePatch{},
+			expected: mustParseManifests(t, strings.TrimSpace(`
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: deployment-1
+spec:
+  template:
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.19.3
+        env:
+        - name: VALUE
+          value: none
+`)),
+		},
+		{
+			name: "no manifest for the given patch",
+			manifests: mustParseManifests(t, strings.TrimSpace(`
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: deployment-1
+spec:
+  template:
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.19.3
+        env:
+        - name: VALUE
+          value: none
+`)),
+			patches: []config.K8sResourcePatch{
+				{
+					Target: config.K8sResourcePatchTarget{
+						K8sResourceReference: config.K8sResourceReference{
+							Kind: "Deployment",
+							Name: "deployment-2",
+						},
+					},
+				},
+			},
+			expectedError: errors.New("no manifest matches the given patch: kind=Deployment, name=deployment-2"),
+		},
+		{
+			name: "multiple patches",
+			manifests: mustParseManifests(t, strings.TrimSpace(`
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: deployment-1
+spec:
+  template:
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.19.3
+        env:
+        - name: VALUE
+          value: none
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: config
+data:
+  key1: value-1
+  key2: value-2
+`)),
+			patches: []config.K8sResourcePatch{
+				{
+					Target: config.K8sResourcePatchTarget{
+						K8sResourceReference: config.K8sResourceReference{
+							Kind: "Deployment",
+							Name: "deployment-1",
+						},
+					},
+					Ops: []config.K8sResourcePatchOp{
+						{
+							Op:    config.K8sResourcePatchOpYAMLReplace,
+							Path:  "$.spec.template.spec.containers[0].env[0].value",
+							Value: "patched",
+						},
+					},
+				},
+				{
+					Target: config.K8sResourcePatchTarget{
+						K8sResourceReference: config.K8sResourceReference{
+							Kind: "ConfigMap",
+							Name: "config",
+						},
+					},
+					Ops: []config.K8sResourcePatchOp{
+						{
+							Op:    config.K8sResourcePatchOpYAMLReplace,
+							Path:  "$.data.key1",
+							Value: "patched",
+						},
+						{
+							Op:    config.K8sResourcePatchOpYAMLReplace,
+							Path:  "$.data.key2",
+							Value: "patched",
+						},
+					},
+				},
+			},
+			expected: mustParseManifests(t, strings.TrimSpace(`
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: deployment-1
+spec:
+  template:
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.19.3
+        env:
+        - name: VALUE
+          value: patched
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: config
+data:
+  key1: patched
+  key2: patched
+`)),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := patchManifests(tc.manifests, tc.patches, patchManifest)
+			assert.Equal(t, tc.expectedError, err)
+			assert.Equal(t, tc.expected, got)
 		})
 	}
 }
