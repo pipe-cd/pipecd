ec357066a1a766c1a10c8966fbd1753b521449ce
kypham <hongkyhvnh@gmail.com>
2025-05-28 22:59:03 +0700

    Install react query v4 and add auth context
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/mocks/services/me.ts
+++ web/src/mocks/services/me.ts
@@ -1,6 +1,7 @@
 import { GetMeResponse } from "pipecd/web/api_client/service_pb";
 import { dummyMe } from "~/__fixtures__/dummy-me";
-import { createHandler } from "../create-handler";
+import { createHandler, createHandlerWithError } from "../create-handler";
+import { StatusCode } from "grpc-web";
 
 export const getMeHandler = createHandler<GetMeResponse>("/GetMe", () => {
   const response = new GetMeResponse();
@@ -9,5 +10,10 @@ export const getMeHandler = createHandler<GetMeResponse>("/GetMe", () => {
   response.setAvatarUrl(dummyMe.avatarUrl);
   return response;
 });
+
+export const getMeUnauthenticatedHandler = createHandlerWithError(
+  "/GetMe",
+  StatusCode.UNAUTHENTICATED
+);
 
 export const meHandlers = [getMeHandler];
