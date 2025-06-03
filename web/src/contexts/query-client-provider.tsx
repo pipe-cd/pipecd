e1bb2a3569bf4f86c2dfd0f6b8b23e3bf86b988e
kypham <hongkyhvnh@gmail.com>
2025-06-04 01:44:16 +0700

    Replace redux in insight page - remove module insight - remove module deployment-change-failure-rate - remove module deployment-frequency - add query for insight - add query for app list , piped list - add toast messages context - update query provider with error handler show toast
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/contexts/query-client-provider.tsx
+++ web/src/contexts/query-client-provider.tsx
@@ -1,10 +1,48 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { FC, PropsWithChildren } from "react";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { FC, PropsWithChildren, useCallback, useMemo } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient();
+import { useToast } from "./toast-context";
 
 const QueryClientWrap: FC<PropsWithChildren<unknown>> = ({ children }) => {
+  const { addToast } = useToast();
+
+  const handleError = useCallback(
+    (err: unknown): void => {
+      if (process.env.NODE_ENV === "development") {
+        console.error(err);
+      }
+
+      if (
+        err &&
+        typeof err === "object" &&
+        "code" in err &&
+        err.code &&
+        "message" in err &&
+        err.message
+      ) {
+        addToast({ message: err.message as string, severity: "error" });
+      } else {
+        throw err;
+      }
+    },
+    [addToast]
+  );
+
+  const queryClient = useMemo(() => {
+    return new QueryClient({
+      queryCache: new QueryCache({
+        onError: handleError,
+      }),
+      mutationCache: new MutationCache({
+        onError: handleError,
+      }),
+    });
+  }, [handleError]);
   return (
     <QueryClientProvider client={queryClient}>
       {children}
