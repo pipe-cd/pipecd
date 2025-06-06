55098b59a4881a89a2755692e21159f3c42061a1
Yoshiki Fujikane <40124947+ffjlabo@users.noreply.github.com>
2025-06-06 17:39:43 +0900

    Return stage status fail when failing applyManifests (#5921)
    Signed-off-by: Yoshiki Fujikane <ffjlabo@gmail.com>

--- pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/sync.go
+++ pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/sync.go
@@ -185,7 +185,7 @@ func (p *Plugin) sync(
 	// TODO: use applyManifests instead of applyManifestsSDK
 	if err := applyManifests(ctx, applier, manifests, cfg.Spec.Input.Namespace, lp); err != nil {
 		lp.Errorf("Failed while applying manifests (%v)", err)
-		return sdk.StageStatusSuccess
+		return sdk.StageStatusFailure
 	}
 
 	if !stageCfg.Prune {
