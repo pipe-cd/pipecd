cbcfb1a927301cb498f68b2853ed1d900747816f
kypham <hongkyhvnh@gmail.com>
2025-06-10 01:06:17 +0700

    Replace redux in page setting/api-keys - Remove modules/api-keys - Add query for api-keys - Update test case for ApiKeyPage
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/components/settings-page/api-key/components/generated-api-key-dialog/index.tsx
+++ web/src/components/settings-page/api-key/components/generated-api-key-dialog/index.tsx
@@ -6,37 +6,32 @@ import {
   DialogTitle,
   Typography,
 } from "@mui/material";
-import { FC, memo, useCallback } from "react";
+import { FC, memo } from "react";
 import { TextWithCopyButton } from "~/components/text-with-copy-button";
-import { useAppDispatch, useAppSelector } from "~/hooks/redux";
-import { clearGeneratedKey } from "~/modules/api-keys";
 
 const DIALOG_TITLE = "Generated API Key";
 const VALUE_CAPTION = "API Key";
 
-export const GeneratedAPIKeyDialog: FC = memo(function GeneratedAPIKeyDialog() {
-  const dispatch = useAppDispatch();
-  const generatedKey = useAppSelector<string | null>(
-    (state) => state.apiKeys.generatedKey
-  );
-  const open = Boolean(generatedKey);
+type Props = {
+  generatedKey: string | null;
+  onClose: () => void;
+};
 
-  const handleClose = useCallback(() => {
-    dispatch(clearGeneratedKey());
-  }, [dispatch]);
-
-  return (
-    <Dialog open={open} fullWidth>
-      <DialogTitle>{DIALOG_TITLE}</DialogTitle>
-      <DialogContent>
-        <Typography variant="caption">{VALUE_CAPTION}</Typography>
-        {generatedKey ? (
-          <TextWithCopyButton name="API Key" value={generatedKey} />
-        ) : null}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Close</Button>
-      </DialogActions>
-    </Dialog>
-  );
-});
+export const GeneratedAPIKeyDialog: FC<Props> = memo(
+  function GeneratedAPIKeyDialog({ generatedKey, onClose }) {
+    return (
+      <Dialog open={Boolean(generatedKey)} fullWidth>
+        <DialogTitle>{DIALOG_TITLE}</DialogTitle>
+        <DialogContent>
+          <Typography variant="caption">{VALUE_CAPTION}</Typography>
+          {generatedKey ? (
+            <TextWithCopyButton name="API Key" value={generatedKey} />
+          ) : null}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+);
