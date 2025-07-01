e1f0a7ed91373963aca9bd920ab7ecc4e8f2f252
t-kikuc <tkikuchi07f@gmail.com>
2025-07-01 11:48:24 +0900

    add if-condition of pipedv0
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/datastore/deploymentstore.go
+++ pkg/datastore/deploymentstore.go
@@ -170,15 +170,17 @@ func (s *deploymentStore) Add(ctx context.Context, d *model.Deployment) error {
 	}
 
 	// To make compatibility with pipedv0 and pipedv1 on the UI.
-	// TODO: Add "If the piped is v0," condition
-	for _, s := range d.Stages {
-		switch s.Name {
-		case model.StageAnalysis.String():
-			s.AvailableOperation = model.ManualOperation_MANUAL_OPERATION_SKIP
-		case model.StageWaitApproval.String():
-			s.AvailableOperation = model.ManualOperation_MANUAL_OPERATION_APPROVE
-			if approvers := s.Metadata["Approvers"]; approvers != "" {
-				s.AuthorizedOperators = strings.Split(approvers, ",")
+	// TODO: Remove this block after ending support of pipedv0.
+	if len(d.DeployTargetsByPlugin) == 0 {
+		for _, s := range d.Stages {
+			switch s.Name {
+			case model.StageAnalysis.String():
+				s.AvailableOperation = model.ManualOperation_MANUAL_OPERATION_SKIP
+			case model.StageWaitApproval.String():
+				s.AvailableOperation = model.ManualOperation_MANUAL_OPERATION_APPROVE
+				if approvers := s.Metadata["Approvers"]; approvers != "" {
+					s.AuthorizedOperators = strings.Split(approvers, ",")
+				}
 			}
 		}
 	}
