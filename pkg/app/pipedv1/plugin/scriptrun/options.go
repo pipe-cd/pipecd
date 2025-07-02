abe5af79c649bdc4aa3d784011707a74f1694d59
hiep-tk <hiep.trinhkhanh0466@gmail.com>
2025-07-02 16:41:07 +0900

    scriptrun base and planning
    Signed-off-by: hiep-tk <hiep.trinhkhanh0466@gmail.com>

---
+++ pkg/app/pipedv1/plugin/scriptrun/options.go
@@ -0,0 +1,51 @@
+// Copyright 2025 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	unit "github.com/pipe-cd/piped-plugin-sdk-go/unit"
+)
+
+type scriptRunStageOptions struct {
+	// user provided env variables to run the script with
+	Env map[string]string `json:"env,omitempty"`
+	// the command(s) to run
+	Run string `json:"run,omitempty"`
+	// timeout limit for this stage
+	Timeout unit.Duration `json:"timeout,omitempty" default:"6h"`
+	// the rollback command(s) to run if deployment fails
+	OnRollback string `json:"onRollback,omitempty"`
+}
+
+func (o scriptRunStageOptions) validate() error {
+	if o.Timeout <= 0 {
+		return fmt.Errorf("timeout must be greater than 0")
+	}
+	return nil
+}
+
+// decode decodes the raw JSON data and validates it.
+func decode(data json.RawMessage) (scriptRunStageOptions, error) {
+	opts := scriptRunStageOptions{}
+	if err := json.Unmarshal(data, &opts); err != nil {
+		return scriptRunStageOptions{}, fmt.Errorf("failed to unmarshal the config: %w", err)
+	}
+	if err := opts.validate(); err != nil {
+		return scriptRunStageOptions{}, fmt.Errorf("failed to validate the config: %w", err)
+	}
+	return opts, nil
+}
