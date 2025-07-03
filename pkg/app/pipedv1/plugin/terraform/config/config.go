eb367eaa4b29ba404e44aa69db1687b4995855ac
Tetsuya KIKUCHI <97105818+t-kikuc@users.noreply.github.com>
2025-07-03 11:58:49 +0900

    Init Terraform plugin (#5993)
    * go mod init
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Init config.go
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Init Plugin and main.go
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Impl FetchDefinedStages
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Impl DetermineStrategy
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Impl BuildQuickSyncStages
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    * Impl BuildPipelineSyncStages and delete SYNC stage
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>
    
    ---------
    
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

---
+++ pkg/app/pipedv1/plugin/terraform/config/config.go
@@ -0,0 +1,24 @@
+// Copyright 2025 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+// Config represents the plugin-scoped configuration.
+type Config struct{}
+
+// DeployTargetConfig represents the deploy-target-scoped configuration.
+type DeployTargetConfig struct{}
+
+// ApplicationConfigSpec represents the application-scoped plugin config.
+type ApplicationConfigSpec struct{}
---
+++ pkg/app/pipedv1/plugin/terraform/deployment/plugin.go
@@ -0,0 +1,118 @@
+// Copyright 2025 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deployment
+
+import (
+	"context"
+	"slices"
+
+	sdk "github.com/pipe-cd/piped-plugin-sdk-go"
+
+	"github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/terraform/config"
+)
+
+// Stage names for Terraform plugin.
+const (
+	// TERRAFORM_PLAN stage executes `terraform plan`.
+	stagePlan = "TERRAFORM_PLAN"
+	// TERRAFORM_APPLY stage executes `terraform apply`.
+	stageApply = "TERRAFORM_APPLY"
+	// TERRAFORM_ROLLBACK stage rollbacks by executing `terraform apply` for the previous commit.`
+	stageRollback = "TERRAFORM_ROLLBACK"
+)
+
+// Plugin implements sdk.DeploymentPlugin for Terraform.
+type Plugin struct{}
+
+var _ sdk.DeploymentPlugin[config.Config, config.DeployTargetConfig, config.ApplicationConfigSpec] = (*Plugin)(nil)
+
+// BuildPipelineSyncStages implements sdk.DeploymentPlugin.
+func (p *Plugin) BuildPipelineSyncStages(ctx context.Context, _ *config.Config, input *sdk.BuildPipelineSyncStagesInput) (*sdk.BuildPipelineSyncStagesResponse, error) {
+	reqStages := input.Request.Stages
+	out := make([]sdk.PipelineStage, 0, len(reqStages)+1)
+
+	for _, s := range reqStages {
+		out = append(out, sdk.PipelineStage{
+			Name:               s.Name,
+			Index:              s.Index,
+			Rollback:           false,
+			Metadata:           make(map[string]string),
+			AvailableOperation: sdk.ManualOperationNone,
+		})
+	}
+	if input.Request.Rollback {
+		minIndex := slices.MinFunc(reqStages, func(a, b sdk.StageConfig) int { return a.Index - b.Index }).Index
+		out = append(out, sdk.PipelineStage{
+			Name:               stageRollback,
+			Index:              minIndex,
+			Rollback:           true,
+			Metadata:           make(map[string]string),
+			AvailableOperation: sdk.ManualOperationNone,
+		})
+	}
+	return &sdk.BuildPipelineSyncStagesResponse{
+		Stages: out,
+	}, nil
+}
+
+// BuildQuickSyncStages implements sdk.DeploymentPlugin.
+func (p *Plugin) BuildQuickSyncStages(ctx context.Context, _ *config.Config, input *sdk.BuildQuickSyncStagesInput) (*sdk.BuildQuickSyncStagesResponse, error) {
+	stages := make([]sdk.QuickSyncStage, 0, 2)
+	stages = append(stages, sdk.QuickSyncStage{
+		Name:               stageApply,
+		Description:        "Sync by applying any detected changes",
+		Rollback:           false,
+		Metadata:           map[string]string{},
+		AvailableOperation: sdk.ManualOperationNone,
+	})
+
+	if input.Request.Rollback {
+		stages = append(stages, sdk.QuickSyncStage{
+			Name:               stageRollback,
+			Description:        "Rollback by applying the previous Terraform files",
+			Rollback:           true,
+			Metadata:           map[string]string{},
+			AvailableOperation: sdk.ManualOperationNone,
+		})
+	}
+	return &sdk.BuildQuickSyncStagesResponse{
+		Stages: stages,
+	}, nil
+}
+
+// DetermineStrategy implements sdk.DeploymentPlugin.
+// It returns (nil, nil) because this plugin does not have specific logic for DetermineStrategy.
+func (p *Plugin) DetermineStrategy(ctx context.Context, _ *config.Config, input *sdk.DetermineStrategyInput[config.ApplicationConfigSpec]) (*sdk.DetermineStrategyResponse, error) {
+	return nil, nil
+}
+
+// DetermineVersions implements sdk.DeploymentPlugin.
+func (p *Plugin) DetermineVersions(ctx context.Context, _ *config.Config, input *sdk.DetermineVersionsInput[config.ApplicationConfigSpec]) (*sdk.DetermineVersionsResponse, error) {
+	panic("unimplemented")
+}
+
+// ExecuteStage implements sdk.DeploymentPlugin.
+func (p *Plugin) ExecuteStage(ctx context.Context, _ *config.Config, dts []*sdk.DeployTarget[config.DeployTargetConfig], input *sdk.ExecuteStageInput[config.ApplicationConfigSpec]) (*sdk.ExecuteStageResponse, error) {
+	panic("unimplemented")
+}
+
+// FetchDefinedStages implements sdk.DeploymentPlugin.
+func (p *Plugin) FetchDefinedStages() []string {
+	return []string{
+		stagePlan,
+		stageApply,
+		stageRollback,
+	}
+}
