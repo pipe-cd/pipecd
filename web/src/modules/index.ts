71187f3660a9d11ff7b0cb4e244b87ac34a9c93a
kypham <hongkyhvnh@gmail.com>
2025-07-07 17:01:26 +0700

    Replace redux in page deployment list - Remove modules/deployment - Remove modules/stage-logs - Add query for deployment list infinite - add component label-auto-complete - fix test application filter
    Signed-off-by: kypham <hongkyhvnh@gmail.com>

--- web/src/modules/index.ts
+++ web/src/modules/index.ts
@@ -5,21 +5,17 @@ import { applicationsSlice } from "./applications";
 import { applicationLiveStateSlice } from "./applications-live-state";
 import { commandsSlice } from "./commands";
 import { deleteApplicationSlice } from "./delete-application";
-import { deploymentsSlice } from "./deployments";
 import { pipedsSlice } from "./pipeds";
 import { sealedSecretSlice } from "./sealed-secret";
-import { stageLogsSlice } from "./stage-logs";
 import { toastsSlice } from "./toasts";
 import { updateApplicationSlice } from "./update-application";
 import { unregisteredApplicationsSlice } from "./unregistered-applications";
 
 export const reducers = combineReducers({
-  deployments: deploymentsSlice.reducer,
   applicationLiveState: applicationLiveStateSlice.reducer,
   applications: applicationsSlice.reducer,
   updateApplication: updateApplicationSlice.reducer,
   deleteApplication: deleteApplicationSlice.reducer,
-  stageLogs: stageLogsSlice.reducer,
   activeStage: activeStageSlice.reducer,
   pipeds: pipedsSlice.reducer,
   commands: commandsSlice.reducer,
