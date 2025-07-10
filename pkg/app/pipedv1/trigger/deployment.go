51ce3d5d722191cb8bdaba3c74e87d7fd96ba172
Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>
2025-07-10 13:26:26 +0700

    Remove deprecated platform provider and cloud provider from trigger logic (#6003)
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>

--- pkg/app/pipedv1/trigger/deployment.go
+++ pkg/app/pipedv1/trigger/deployment.go
@@ -93,8 +93,6 @@ func buildDeployment(
 			StrategySummary: strategySummary,
 		},
 		GitPath:                   app.GitPath,
-		CloudProvider:             app.CloudProvider,
-		PlatformProvider:          app.PlatformProvider,
 		DeployTargetsByPlugin:     app.DeployTargetsByPlugin,
 		Labels:                    app.Labels,
 		Status:                    model.DeploymentStatus_DEPLOYMENT_PENDING,
