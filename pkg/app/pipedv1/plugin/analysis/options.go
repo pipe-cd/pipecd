cf0423e31ec6cd8aad68ce4b566e80ce12d2d3bd
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-08-01 11:29:42 +0000

    analysisStageOptions struct and decode function for ANALYSIS stage

---
+++ pkg/app/pipedv1/plugin/analysis/options.go
@@ -0,0 +1,34 @@
+// Copyright 2025 The PipeCD Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+)
+
+// analysisStageOptions represents the configuration options for the ANALYSIS stage.
+// This struct is currently empty and will be extended in future PRs as needed.
+type analysisStageOptions struct {
+	// Future analysis configuration fields will be added here
+}
+
+// decode decodes the raw JSON data and validates it.
+func decode(data json.RawMessage) (analysisStageOptions, error) {
+	opts := analysisStageOptions{}
+	if err := json.Unmarshal(data, &opts); err != nil {
+		return analysisStageOptions{}, err
+	}
+	return opts, nil
+}
