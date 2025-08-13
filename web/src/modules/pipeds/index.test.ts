b5c68cbfe92a2d3b4d35793288a4606f623dde32
Ky_pham <57948086+hongky-1994@users.noreply.github.com>
2025-08-13 12:28:18 +0700

    Replace redux in setting piped pages (#5954)
    - remove some function in modules/pipeds
    - add query for get breaking changes and released versions
    - add mutation for add, disable, enable, restart piped
    - add mutation for update piped desired version
    - add mutaiton for add new, delete old piped key.
    - remove unused state in module/pipeds
    - update tests affected
    
    Signed-off-by: kypham <hongkyhvnh@gmail.com>
    Co-authored-by: Khanh Tran <32532742+khanhtc1202@users.noreply.github.com>

--- web/src/modules/pipeds/index.test.ts
+++ web/src/modules/pipeds/index.test.ts
@@ -1,20 +1,9 @@
 import { dummyPiped } from "~/__fixtures__/dummy-piped";
-import {
-  pipedsSlice,
-  clearRegisteredPipedInfo,
-  addPiped,
-  fetchPipeds,
-  addNewPipedKey,
-  editPiped,
-} from "./";
+import { pipedsSlice, fetchPipeds } from "./";
 
 const baseState = {
   entities: {},
   ids: [],
-  registeredPiped: null,
-  updating: false,
-  releasedVersions: [],
-  breakingChangesNote: "",
 };
 
 describe("pipedsSlice reducer", () => {
@@ -24,46 +13,6 @@ describe("pipedsSlice reducer", () => {
         type: "TEST_ACTION",
       })
     ).toEqual(baseState);
-  });
-
-  it(`should handle ${clearRegisteredPipedInfo.type}`, () => {
-    expect(
-      pipedsSlice.reducer(
-        {
-          ...baseState,
-          registeredPiped: {
-            id: "piped-1",
-            key: "piped-key",
-            isNewKey: false,
-          },
-        },
-        {
-          type: clearRegisteredPipedInfo.type,
-        }
-      )
-    ).toEqual(baseState);
-  });
-
-  describe("addPiped", () => {
-    it(`should handle ${addPiped.fulfilled.type}`, () => {
-      expect(
-        pipedsSlice.reducer(baseState, {
-          type: addPiped.fulfilled.type,
-          payload: {
-            id: "piped-1",
-            key: "piped-key",
-            isNewKey: false,
-          },
-        })
-      ).toEqual({
-        ...baseState,
-        registeredPiped: {
-          id: "piped-1",
-          key: "piped-key",
-          isNewKey: false,
-        },
-      });
-    });
   });
 
   describe("fetchPipeds", () => {
@@ -77,70 +26,6 @@ describe("pipedsSlice reducer", () => {
         ...baseState,
         entities: { [dummyPiped.id]: dummyPiped },
         ids: [dummyPiped.id],
-      });
-    });
-  });
-
-  describe("recreatePipedKey", () => {
-    it(`should handle ${addNewPipedKey.fulfilled.type}`, () => {
-      expect(
-        pipedsSlice.reducer(baseState, {
-          type: addNewPipedKey.fulfilled.type,
-          payload: "add-new-piped-key",
-          meta: {
-            arg: {
-              pipedId: "piped-1",
-            },
-          },
-        })
-      ).toEqual({
-        ...baseState,
-        registeredPiped: {
-          id: "piped-1",
-          key: "add-new-piped-key",
-          isNewKey: true,
-        },
-      });
-    });
-  });
-
-  describe("editPiped", () => {
-    it(`should handle ${editPiped.pending.type}`, () => {
-      expect(
-        pipedsSlice.reducer(baseState, {
-          type: editPiped.pending.type,
-        })
-      ).toEqual({
-        ...baseState,
-        updating: true,
-      });
-    });
-
-    it(`should handle ${editPiped.rejected.type}`, () => {
-      expect(
-        pipedsSlice.reducer(
-          { ...baseState, updating: true },
-          {
-            type: editPiped.rejected.type,
-          }
-        )
-      ).toEqual({
-        ...baseState,
-        updating: false,
-      });
-    });
-
-    it(`should handle ${editPiped.fulfilled.type}`, () => {
-      expect(
-        pipedsSlice.reducer(
-          { ...baseState, updating: true },
-          {
-            type: editPiped.fulfilled.type,
-          }
-        )
-      ).toEqual({
-        ...baseState,
-        updating: false,
       });
     });
   });
--- web/src/modules/pipeds/index.ts
+++ web/src/modules/pipeds/index.ts
@@ -8,12 +8,6 @@ import { Piped } from "pipecd/web/model/piped_pb";
 import type { AppState } from "~/store";
 import * as pipedsApi from "~/api/piped";
 
-export interface RegisteredPiped {
-  id: string;
-  key: string;
-  isNewKey: boolean;
-}
-
 const MODULE_NAME = "pipeds";
 
 const pipedsAdapter = createEntityAdapter<Piped.AsObject>({});
@@ -29,24 +23,6 @@ export const selectPipedIds = (state: AppState): EntityId[] =>
 export const selectAllPipeds = (state: AppState): Piped.AsObject[] =>
   selectAll(state.pipeds);
 
-export const fetchReleasedVersions = createAsyncThunk<string[]>(
-  `${MODULE_NAME}/fetchReleasedVersions`,
-  async () => {
-    const { versionsList } = await pipedsApi.listReleasedVersions();
-    return versionsList;
-  }
-);
-
-export const fetchBreakingChanges = createAsyncThunk<
-  string,
-  { projectId: string }
->(`${MODULE_NAME}/fetchBreakingChanges`, async (props) => {
-  const { notes } = await pipedsApi.listBreakingChanges({
-    projectId: props.projectId,
-  });
-  return notes;
-});
-
 export const fetchPipeds = createAsyncThunk<Piped.AsObject[], boolean>(
   `${MODULE_NAME}/fetchList`,
   async (withStatus: boolean) => {
@@ -56,125 +32,16 @@ export const fetchPipeds = createAsyncThunk<Piped.AsObject[], boolean>(
   }
 );
 
-export const addPiped = createAsyncThunk<
-  RegisteredPiped,
-  { name: string; desc: string }
->(`${MODULE_NAME}/add`, async (props) => {
-  const res = await pipedsApi.registerPiped({
-    desc: props.desc,
-    name: props.name,
-  });
-  return { ...res, isNewKey: false };
-});
-
-export const disablePiped = createAsyncThunk<void, { pipedId: string }>(
-  `${MODULE_NAME}/disable`,
-  async ({ pipedId }) => {
-    await pipedsApi.disablePiped({ pipedId });
-  }
-);
-
-export const enablePiped = createAsyncThunk<void, { pipedId: string }>(
-  `${MODULE_NAME}/enable`,
-  async ({ pipedId }) => {
-    await pipedsApi.enablePiped({ pipedId });
-  }
-);
-
-export const restartPiped = createAsyncThunk<void, { pipedId: string }>(
-  `${MODULE_NAME}/restart`,
-  async ({ pipedId }) => {
-    await pipedsApi.restartPiped({ pipedId });
-  }
-);
-
-export const addNewPipedKey = createAsyncThunk<string, { pipedId: string }>(
-  `${MODULE_NAME}/addNewKey`,
-  async ({ pipedId }) => {
-    const { key } = await pipedsApi.recreatePipedKey({ id: pipedId });
-    return key;
-  }
-);
-
-export const deleteOldKey = createAsyncThunk<void, { pipedId: string }>(
-  `${MODULE_NAME}/deleteOldKey`,
-  async ({ pipedId }) => {
-    await pipedsApi.deleteOldPipedKey({ pipedId });
-  }
-);
-
-export const editPiped = createAsyncThunk<
-  void,
-  { pipedId: string; name: string; desc: string }
->(`${MODULE_NAME}/edit`, async ({ pipedId, name, desc }) => {
-  await pipedsApi.updatePiped({
-    pipedId,
-    name,
-    desc,
-  });
-});
-
-export const updatePipedDesiredVersion = createAsyncThunk<
-  void,
-  { version: string; pipedIds: string[] }
->(`${MODULE_NAME}/updatePipedDesiredVersion`, async ({ version, pipedIds }) => {
-  await pipedsApi.updatePipedDesiredVersion({
-    version,
-    pipedIdsList: pipedIds,
-  });
-});
-
 export const pipedsSlice = createSlice({
   name: MODULE_NAME,
-  initialState: pipedsAdapter.getInitialState<{
-    registeredPiped: RegisteredPiped | null;
-    updating: boolean;
-    releasedVersions: string[];
-    breakingChangesNote: string;
-  }>({
-    registeredPiped: null,
-    updating: false,
-    releasedVersions: [],
-    breakingChangesNote: "",
-  }),
-  reducers: {
-    clearRegisteredPipedInfo(state) {
-      state.registeredPiped = null;
-    },
-  },
+  initialState: pipedsAdapter.getInitialState({}),
+  reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(addPiped.fulfilled, (state, action) => {
-        state.registeredPiped = action.payload;
-      })
-      .addCase(fetchPipeds.fulfilled, (state, action) => {
-        pipedsAdapter.removeAll(state);
-        pipedsAdapter.addMany(state, action.payload);
-      })
-      .addCase(addNewPipedKey.fulfilled, (state, action) => {
-        state.registeredPiped = {
-          id: action.meta.arg.pipedId,
-          key: action.payload,
-          isNewKey: true,
-        };
-      })
-      .addCase(editPiped.pending, (state) => {
-        state.updating = true;
-      })
-      .addCase(editPiped.rejected, (state) => {
-        state.updating = false;
-      })
-      .addCase(editPiped.fulfilled, (state) => {
-        state.updating = false;
-      })
-      .addCase(fetchReleasedVersions.fulfilled, (state, action) => {
-        state.releasedVersions = action.payload;
-      })
-      .addCase(fetchBreakingChanges.fulfilled, (state, action) => {
-        state.breakingChangesNote = action.payload;
-      });
+    builder.addCase(fetchPipeds.fulfilled, (state, action) => {
+      pipedsAdapter.removeAll(state);
+      pipedsAdapter.addMany(state, action.payload);
+    });
   },
 });
 
-export const { clearRegisteredPipedInfo } = pipedsSlice.actions;
 export { Piped, PipedKey } from "pipecd/web/model/piped_pb";
