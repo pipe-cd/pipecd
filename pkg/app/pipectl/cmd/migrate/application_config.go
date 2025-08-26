1594024350fb0ff005ce735cefa6944ebccce416
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-08-26 17:23:28 +0900

    Change the way to migrate application config
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- pkg/app/pipectl/cmd/migrate/application_config.go
+++ pkg/app/pipectl/cmd/migrate/application_config.go
@@ -16,7 +16,6 @@ package migrate
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -65,267 +64,139 @@ func (c *applicationConfig) run(ctx context.Context, input cli.Input) error {
 }
 
 func (c *applicationConfig) migrateApplicationConfig(ctx context.Context, configFile string, logger *zap.Logger) error {
-	cfg, err := config.LoadFromYAML(configFile)
+	data, err := os.ReadFile(configFile)
 	if err != nil {
-		logger.Error("failed to load application config", zap.String("config-file", configFile), zap.Error(err))
+		return err
+	}
+
+	var cfg map[string]any
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		return err
 	}
 
 	migrated := make(map[string]any)
-
-	genericSpec, ok := cfg.GetGenericApplication()
-	if !ok {
-		logger.Error("failed to get generic application spec", zap.String("config-file", configFile))
-		return fmt.Errorf("failed to get generic application spec")
-	}
-
-	switch cfg.Kind {
-	case config.KindCloudRunApp:
-		genericSpec.Plugins = map[string]any{
-			"cloudrun": (*migratedCloudRunApplicationSpec)(cfg.CloudRunApplicationSpec),
-		}
-	case config.KindECSApp:
-		genericSpec.Plugins = map[string]any{
-			"ecs": (*migratedECSApplicationSpec)(cfg.ECSApplicationSpec),
-		}
-	case config.KindKubernetesApp:
-		genericSpec.Plugins = map[string]any{
-			"kubernetes": (*migratedKubernetesApplicationSpec)(cfg.KubernetesApplicationSpec),
-		}
-	case config.KindLambdaApp:
-		genericSpec.Plugins = map[string]any{
-			"lambda": (*migratedLambdaApplicationSpec)(cfg.LambdaApplicationSpec),
-		}
-	case config.KindTerraformApp:
-		genericSpec.Plugins = map[string]any{
-			"terraform": (*migratedTerraformApplicationSpec)(cfg.TerraformApplicationSpec),
-		}
-	}
-
 	migrated["kind"] = config.KindApplication
 	migrated["apiVersion"] = config.VersionV1Beta1
-	migrated["spec"] = (*migratedApplicationSpec)(&genericSpec)
 
-	b, err := json.Marshal(migrated)
-	if err != nil {
-		logger.Error("failed to marshal migrated application config", zap.String("config-file", configFile), zap.Error(err))
-		return err
+	spec := make(map[string]any)
+
+	// generic spec
+	keys := []string{
+		"name",
+		"labels",
+		"description",
+		"planner",
+		"commitMatcher",
+		"pipeline",
+		"trigger",
+		"postSync",
+		"timeout",
+		"encryption",
+		"attachment",
+		"notification",
+		"eventWatcher",
+		"driftDetection",
 	}
 
-	y, err := yaml.JSONToYAML(b)
+	oldSpec := cfg["spec"].(map[string]any)
+	for _, key := range keys {
+		if _, ok := oldSpec[key]; ok {
+			spec[key] = oldSpec[key]
+		}
+	}
+
+	switch config.Kind(cfg["kind"].(string)) {
+	case config.KindKubernetesApp:
+		keys := []string{
+			"input",
+			"quickSync",
+			"service",
+			"workloads",
+			"trafficRouting",
+			"variantLabel",
+			"resourceRoutes",
+		}
+		pluginCfg := make(map[string]map[string]any)
+		pluginCfg["kubernetes"] = make(map[string]any)
+		for _, key := range keys {
+			if _, ok := oldSpec[key]; ok {
+				pluginCfg["kubernetes"][key] = oldSpec[key]
+			}
+		}
+		spec["plugins"] = pluginCfg
+	case config.KindTerraformApp:
+		keys := []string{
+			"input",
+			"quickSync",
+		}
+		pluginCfg := make(map[string]map[string]any)
+		pluginCfg["terraform"] = make(map[string]any)
+		for _, key := range keys {
+			if _, ok := oldSpec[key]; ok {
+				pluginCfg["terraform"][key] = oldSpec[key]
+			}
+		}
+		spec["plugins"] = pluginCfg
+	case config.KindECSApp:
+		keys := []string{
+			"input",
+			"quickSync",
+		}
+		pluginCfg := make(map[string]map[string]any)
+		pluginCfg["ecs"] = make(map[string]any)
+		for _, key := range keys {
+			if _, ok := oldSpec[key]; ok {
+				pluginCfg["ecs"][key] = oldSpec[key]
+			}
+		}
+		spec["plugins"] = pluginCfg
+	case config.KindLambdaApp:
+		keys := []string{
+			"input",
+			"quickSync",
+		}
+		pluginCfg := make(map[string]map[string]any)
+		pluginCfg["lambda"] = make(map[string]any)
+		for _, key := range keys {
+			if _, ok := oldSpec[key]; ok {
+				pluginCfg["lambda"][key] = oldSpec[key]
+			}
+		}
+		spec["plugins"] = pluginCfg
+	case config.KindCloudRunApp:
+		keys := []string{
+			"input",
+			"quickSync",
+		}
+		pluginCfg := make(map[string]map[string]any)
+		pluginCfg["cloudrun"] = make(map[string]any)
+		for _, key := range keys {
+			if _, ok := oldSpec[key]; ok {
+				pluginCfg["cloudrun"][key] = oldSpec[key]
+			}
+		}
+		spec["plugins"] = pluginCfg
+	default:
+		return fmt.Errorf("unsupported application kind: %s", cfg["kind"])
+	}
+
+	migrated["spec"] = spec
+
+	yamlData, err := yaml.Marshal(migrated)
 	if err != nil {
-		logger.Error("failed to convert migrated application config to YAML", zap.String("config-file", configFile), zap.Error(err))
 		return err
 	}
 
 	if err := os.Rename(configFile, configFile+".old"); err != nil {
-		logger.Error("failed to rename application config file", zap.String("config-file", configFile), zap.Error(err))
 		return err
 	}
 
-	if err := os.WriteFile(configFile, y, 0644); err != nil {
-		logger.Error("failed to write migrated application config", zap.String("config-file", configFile), zap.Error(err))
-
-		// If the write failed, we need to restore the old config file.
+	if err := os.WriteFile(configFile, yamlData, 0644); err != nil {
 		if e := os.Rename(configFile+".old", configFile); e != nil {
-			logger.Error("failed to rename application config file", zap.String("config-file", configFile), zap.Error(e))
 			return errors.Join(err, e)
 		}
-
 		return err
 	}
 
-	logger.Info("successfully migrated application config", zap.String("config-file", configFile))
 	return nil
-}
-
-type migratedApplicationSpec config.GenericApplicationSpec
-
-func (c *migratedApplicationSpec) MarshalJSON() ([]byte, error) {
-
-	type migratedPipelineStage struct {
-		ID      string          `json:"id,omitempty"`
-		Name    string          `json:"name"`
-		Desc    string          `json:"desc,omitempty"`
-		Timeout config.Duration `json:"timeout,omitempty"`
-		With    json.RawMessage `json:"with,omitempty"`
-	}
-
-	type migratedDeploymentPipeline struct {
-		Stages []migratedPipelineStage `json:"stages"`
-	}
-
-	type spec struct {
-		// The application name.
-		// This is required if you set the application through the application configuration file.
-		Name string `json:"name,omitempty"`
-		// Additional attributes to identify applications.
-		Labels map[string]string `json:"labels,omitempty"`
-		// Notes on the Application.
-		Description string `json:"description,omitempty"`
-
-		// Configuration used while planning deployment.
-		Planner config.DeploymentPlanner `json:"planner,omitempty"`
-		// Forcibly use QuickSync or Pipeline when commit message matched the specified pattern.
-		CommitMatcher config.DeploymentCommitMatcher `json:"commitMatcher,omitempty"`
-		// Pipeline for deploying progressively.
-		Pipeline *migratedDeploymentPipeline `json:"pipeline"`
-		// The trigger configuration use to determine trigger logic.
-		Trigger config.Trigger `json:"trigger,omitempty"`
-		// Configuration to be used once the deployment is triggered successfully.
-		PostSync *config.PostSync `json:"postSync,omitempty"`
-		// The maximum length of time to execute deployment before giving up.
-		// Default is 6h.
-		Timeout config.Duration `json:"timeout,omitempty" default:"6h"`
-		// List of encrypted secrets and targets that should be decoded before using.
-		Encryption *config.SecretEncryption `json:"encryption,omitempty"`
-		// List of files that should be attached to application manifests before using.
-		Attachment *config.Attachment `json:"attachment,omitempty"`
-		// Additional configuration used while sending notification to external services.
-		DeploymentNotification *config.DeploymentNotification `json:"notification,omitempty"`
-		// List of the configuration for event watcher.
-		EventWatcher []config.EventWatcherConfig `json:"eventWatcher,omitempty"`
-		// Configuration for drift detection
-		DriftDetection *config.DriftDetection `json:"driftDetection,omitempty"`
-
-		// This is a workaround not to raise unknown-field error when the application config file contains the plugins field.
-		Plugins any `json:"plugins"`
-	}
-
-	pipeline := &migratedDeploymentPipeline{}
-	for _, stage := range c.Pipeline.Stages {
-		pipeline.Stages = append(pipeline.Stages, migratedPipelineStage{
-			ID:      stage.ID,
-			Name:    string(stage.Name),
-			Desc:    stage.Desc,
-			Timeout: stage.Timeout,
-			With:    stage.With,
-		})
-	}
-
-	return json.Marshal(spec{
-		Name:                   c.Name,
-		Labels:                 c.Labels,
-		Description:            c.Description,
-		Planner:                c.Planner,
-		CommitMatcher:          c.CommitMatcher,
-		Pipeline:               pipeline,
-		Trigger:                c.Trigger,
-		PostSync:               c.PostSync,
-		Timeout:                c.Timeout,
-		Encryption:             c.Encryption,
-		Attachment:             c.Attachment,
-		DeploymentNotification: c.DeploymentNotification,
-		EventWatcher:           c.EventWatcher,
-		DriftDetection:         c.DriftDetection,
-		Plugins:                c.Plugins,
-	})
-}
-
-type migratedCloudRunApplicationSpec config.CloudRunApplicationSpec
-
-func (c *migratedCloudRunApplicationSpec) MarshalJSON() ([]byte, error) {
-	// spec is copied from config.CloudRunApplicationSpec, but we need to remove the GenericApplicationSpec field.
-	type spec struct {
-		// Input for CloudRun deployment such as docker image...
-		Input config.CloudRunDeploymentInput `json:"input"`
-		// Configuration for quick sync.
-		QuickSync config.CloudRunSyncStageOptions `json:"quickSync"`
-	}
-	return json.Marshal(spec{
-		Input:     c.Input,
-		QuickSync: c.QuickSync,
-	})
-}
-
-type migratedECSApplicationSpec config.ECSApplicationSpec
-
-func (c *migratedECSApplicationSpec) MarshalJSON() ([]byte, error) {
-	// spec is copied from config.ECSApplicationSpec, but we need to remove the GenericApplicationSpec field.
-	type spec struct {
-		// Input for ECS deployment such as where to fetch source code...
-		Input config.ECSDeploymentInput `json:"input"`
-		// Configuration for quick sync.
-		QuickSync config.ECSSyncStageOptions `json:"quickSync"`
-	}
-	return json.Marshal(spec{
-		Input:     c.Input,
-		QuickSync: c.QuickSync,
-	})
-}
-
-type migratedKubernetesApplicationSpec config.KubernetesApplicationSpec
-
-func (c *migratedKubernetesApplicationSpec) MarshalJSON() ([]byte, error) {
-	// spec is copied from config.KubernetesApplicationSpec, but we need to remove the GenericApplicationSpec field.
-	type spec struct {
-		// Input for Kubernetes deployment such as kubectl version, helm version, manifests filter...
-		Input config.KubernetesDeploymentInput `json:"input"`
-		// Configuration for quick sync.
-		QuickSync config.K8sSyncStageOptions `json:"quickSync"`
-		// Which resource should be considered as the Service of application.
-		// Empty means the first Service resource will be used.
-		Service config.K8sResourceReference `json:"service"`
-		// Which resources should be considered as the Workload of application.
-		// Empty means all Deployments.
-		// e.g.
-		// - kind: Deployment
-		//   name: deployment-name
-		// - kind: ReplicationController
-		//   name: replication-controller-name
-		Workloads []config.K8sResourceReference `json:"workloads"`
-		// Which method should be used for traffic routing.
-		TrafficRouting *config.KubernetesTrafficRouting `json:"trafficRouting"`
-		// The label will be configured to variant manifests used to distinguish them.
-		VariantLabel config.KubernetesVariantLabel `json:"variantLabel"`
-		// List of route configurations to resolve the platform provider for application resources.
-		// Each resource will be checked over the match conditions of each route.
-		// If matches, it will be applied to the route's provider,
-		// otherwise, it will be fallen through the next route to check.
-		// Any resource which does not match any specified route will be applied
-		// to the default platform provider which had been specified while registering the application.
-		ResourceRoutes []config.KubernetesResourceRoute `json:"resourceRoutes"`
-	}
-
-	return json.Marshal(spec{
-		Input:          c.Input,
-		QuickSync:      c.QuickSync,
-		Service:        c.Service,
-		Workloads:      c.Workloads,
-		TrafficRouting: c.TrafficRouting,
-		VariantLabel:   c.VariantLabel,
-		ResourceRoutes: c.ResourceRoutes,
-	})
-}
-
-type migratedLambdaApplicationSpec config.LambdaApplicationSpec
-
-func (c *migratedLambdaApplicationSpec) MarshalJSON() ([]byte, error) {
-	// spec is copied from config.LambdaApplicationSpec, but we need to remove the GenericApplicationSpec field.
-	type spec struct {
-		// Input for Lambda deployment such as where to fetch source code...
-		Input config.LambdaDeploymentInput `json:"input"`
-		// Configuration for quick sync.
-		QuickSync config.LambdaSyncStageOptions `json:"quickSync"`
-	}
-	return json.Marshal(spec{
-		Input:     c.Input,
-		QuickSync: c.QuickSync,
-	})
-}
-
-type migratedTerraformApplicationSpec config.TerraformApplicationSpec
-
-func (c *migratedTerraformApplicationSpec) MarshalJSON() ([]byte, error) {
-	// spec is copied from config.TerraformApplicationSpec, but we need to remove the GenericApplicationSpec field.
-	type spec struct {
-		// Input for Terraform deployment such as terraform version, workspace...
-		Input config.TerraformDeploymentInput `json:"input"`
-		// Configuration for quick sync.
-		QuickSync config.TerraformApplyStageOptions `json:"quickSync"`
-	}
-	return json.Marshal(spec{
-		Input:     c.Input,
-		QuickSync: c.QuickSync,
-	})
 }
