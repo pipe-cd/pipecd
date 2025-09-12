3b64e8752e2d64217745d1a4c247c04579b79404
t-kikuc <tkikuchi07f@gmail.com>
2025-09-12 17:54:23 +0900

    Use empty runningDS when precommit is empty
    Signed-off-by: t-kikuc <tkikuchi07f@gmail.com>

--- pkg/app/pipedv1/planpreview/builder.go
+++ pkg/app/pipedv1/planpreview/builder.go
@@ -261,16 +261,20 @@ func (b *builder) buildApp(ctx context.Context, worker int, command string, app 
 	}
 	r.SyncStrategy = strategy
 
-	runningDSP := deploysource.NewProvider(
-		b.workingDir,
-		deploysource.NewLocalSourceCloner(repo, "running", preCommit),
-		app.GitPath,
-		b.secretDecrypter,
-	)
-	runningDS, err := runningDSP.Get(ctx, io.Discard)
-	if err != nil {
-		r.Error = fmt.Sprintf("failed to get the running deploy source, %v", err)
-		return r
+	pluginRunningDS := &common.DeploymentSource{}
+	if preCommit != "" {
+		runningDSP := deploysource.NewProvider(
+			b.workingDir,
+			deploysource.NewLocalSourceCloner(repo, "running", preCommit),
+			app.GitPath,
+			b.secretDecrypter,
+		)
+		runningDS, err := runningDSP.Get(ctx, io.Discard)
+		if err != nil {
+			r.Error = fmt.Sprintf("failed to get the running deploy source, %v", err)
+			return r
+		}
+		pluginRunningDS = runningDS.ToPluginDeploySource()
 	}
 
 	logger.Info("successfully decided sync strategy for the application", zap.String("strategy", r.SyncStrategy.String()))
@@ -279,12 +283,12 @@ func (b *builder) buildApp(ctx context.Context, worker int, command string, app 
 	for _, plugin := range plugins {
 		r.PluginNames = append(r.PluginNames, plugin.Name())
 		res, err := plugin.GetPlanPreview(ctx, &planpreviewapi.GetPlanPreviewRequest{
-			ApplicationId:          app.Id,
-			ApplicationName:        app.Name,
-			PipedId:                b.pipedCfg.PipedID,
-			DeployTargets:          app.GetDeployTargets(),
-			TargetDeploymentSource: pluginTargetDS,
-			RunningDeploymentSource: runningDS.ToPluginDeploySource(),
+			ApplicationId:           app.Id,
+			ApplicationName:         app.Name,
+			PipedId:                 b.pipedCfg.PipedID,
+			DeployTargets:           app.GetDeployTargets(),
+			TargetDeploymentSource:  pluginTargetDS,
+			RunningDeploymentSource: pluginRunningDS,
 		})
 		if err != nil {
 			if st, ok := status.FromError(err); ok && st.Code() == codes.Unimplemented {
