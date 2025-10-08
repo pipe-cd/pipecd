0fb7ad62f10a555b46a5fc6be7d05f6f07950472
khanhtc1202 <khanhtc1202@gmail.com>
2025-10-08 13:44:58 +0900

    Release v0.55.0
    Signed-off-by: khanhtc1202 <khanhtc1202@gmail.com>

--- docs/main.go
+++ docs/main.go
@@ -28,7 +28,7 @@ import (
 const dir = "/public"
 
 // Don't update here manually. /hack/gen-release-docs.sh does.
-const latestPath = "/docs-v0.54.x/"
+const latestPath = "/docs-v0.55.x/"
 
 func main() {
 	var (
