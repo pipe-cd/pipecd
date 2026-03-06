c75587548d68d08beea0c9b6e9a00bbf8542dfc2
Hoang Ngo <adlehoang118@gmail.com>
2026-03-06 11:05:25 +0700

    feat: implement ECS_SYNC stage (basic flow)
    Signed-off-by: Hoang Ngo <adlehoang118@gmail.com>

--- pkg/app/pipedv1/plugin/ecs/provider/ecs.go
+++ pkg/app/pipedv1/plugin/ecs/provider/ecs.go
@@ -15,9 +15,11 @@
 package provider
 
 import (
+	"context"
 	"path/filepath"
 	"sync"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/pipe-cd/pipecd/pkg/app/pipedv1/plugin/ecs/config"
 	"golang.org/x/sync/singleflight"
@@ -37,6 +39,17 @@ type Client interface {
 
 // ECS defines methods for interacting with ECS resources.
 type ECS interface {
+	CreateService(ctx context.Context, service types.Service) (*types.Service, error)
+	UpdateService(ctx context.Context, service types.Service) (*types.Service, error)
+	GetServiceTaskSets(ctx context.Context, service types.Service) ([]types.TaskSet, error)
+	CreateTaskSet(ctx context.Context, service types.Service, taskDefinition types.TaskDefinition, targetGroup *types.LoadBalancer, scale float64) (*types.TaskSet, error)
+	UpdateServicePrimaryTaskSet(ctx context.Context, service types.Service, taskSet types.TaskSet) (*types.TaskSet, error)
+	DeleteTaskSet(ctx context.Context, taskSet types.TaskSet) error
+	ServiceExists(ctx context.Context, cluster, serviceName string) (bool, error)
+	GetServiceStatus(ctx context.Context, cluster, serviceName string) (string, error)
+	WaitServiceStable(ctx context.Context, cluster, serviceName string) error
+	RegisterTaskDefinition(ctx context.Context, taskDef types.TaskDefinition) (*types.TaskDefinition, error)
+	RunTask(ctx context.Context, taskDefinition types.TaskDefinition, clusterArn string, launchType string, awsVpcConfiguration *config.ECSVpcConfiguration, tags []types.Tag) error
 }
 
 // LoadTaskDefinition returns TaskDefinition object from a given task definition file.
@@ -49,6 +62,11 @@ func LoadTaskDefinition(appDir, taskDefinition string) (types.TaskDefinition, er
 func LoadServiceDefinition(appDir, serviceDefinition string) (types.Service, error) {
 	path := filepath.Join(appDir, serviceDefinition)
 	return loadServiceDefinition(path)
+}
+
+// LoadTargetGroups returns primary & canary target groups according to the defined in pipe definition file.
+func LoadTargetGroups(targetGroups config.ECSTargetGroups) (*types.LoadBalancer, *types.LoadBalancer, error) {
+	return loadTargetGroups(targetGroups)
 }
 
 // Registry holds a pool of aws client wrappers.
@@ -93,4 +111,18 @@ var defaultRegistry = &registry{
 // DefaultRegistry returns a pool of aws clients and a mutex associated with it.
 func DefaultRegistry() Registry {
 	return defaultRegistry
+}
+
+// MakeTags converts a map of tags to a slice of types.Tag which is used by AWS SDK.
+func MakeTags(tags map[string]string) []types.Tag {
+	resourceTags := make([]types.Tag, 0, len(tags))
+	for key, value := range tags {
+		resourceTags = append(
+			resourceTags,
+			types.Tag{
+				Key:   aws.String(key),
+				Value: aws.String(value),
+			})
+	}
+	return resourceTags
 }
