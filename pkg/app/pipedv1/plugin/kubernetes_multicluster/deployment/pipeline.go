719af37bf43a191fffdd717273b973b82a536c51
Mohammed Firdous <mohammedfirdousaraoye@gmail.com>
2026-03-14 08:23:42 +0000

    Add K8s canary clean stage and improve live resource fetching
    Signed-off-by: Mohammed Firdous <mohammedfirdousaraoye@gmail.com>

--- pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/pipeline.go
+++ pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/pipeline.go
@@ -26,11 +26,14 @@ const (
 	StageK8sMultiSync = "K8S_MULTI_SYNC"
 	// StageK8sMultiRollback represents the state where all deployed resources should be rollbacked.
 	StageK8sMultiRollback = "K8S_MULTI_ROLLBACK"
+	// StageK8sMultiCanaryClean represents the state where all canary resources should be removed.
+	StageK8sMultiCanaryClean = "K8S_CANARY_CLEAN"
 )
 
 var allStages = []string{
 	StageK8sMultiSync,
 	StageK8sMultiRollback,
+	StageK8sMultiCanaryClean,
 }
 
 const (
@@ -38,6 +41,8 @@ const (
 	StageDescriptionK8sMultiSync = "Sync by applying all manifests"
 	// StageDescriptionK8sMultiRollback represents the description of the K8sRollback stage.
 	StageDescriptionK8sMultiRollback = "Rollback the deployment"
+	// StageDescriptionK8sMultiCanaryClean represents the description of the K8sCanaryClean stage.
+	StageDescriptionK8sMultiCanaryClean = "Remove all canary resources"
 )
 
 func buildQuickSyncPipeline(autoRollback bool) []sdk.QuickSyncStage {
--- pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/plugin.go
+++ pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/plugin.go
@@ -67,6 +67,10 @@ func (p *Plugin) ExecuteStage(ctx context.Context, _ *sdk.ConfigNone, dts []*sdk
 		return &sdk.ExecuteStageResponse{
 			Status: p.executeK8sMultiRollbackStage(ctx, input, dts),
 		}, nil
+	case StageK8sMultiCanaryClean:
+		return &sdk.ExecuteStageResponse{
+			Status: p.executeK8sMultiCanaryCleanStage(ctx, input, dts),
+		}, nil
 	default:
 		return nil, errors.New("unimplemented or unsupported stage")
 	}
