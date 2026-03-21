1e3758673b560f1ebb699c21d37da864e076eb31
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2026-03-21 20:53:18 +0000

    feat: add K8S_BASELINE_CLEAN stage for baseline resource removal
    Signed-off-by: Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>

--- pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/pipeline.go
+++ pkg/app/pipedv1/plugin/kubernetes_multicluster/deployment/pipeline.go
@@ -30,6 +30,8 @@ const (
 	StageK8sMultiCanaryRollout = "K8S_CANARY_ROLLOUT"
 	// StageK8sMultiCanaryClean represents the state where all canary resources should be removed.
 	StageK8sMultiCanaryClean = "K8S_CANARY_CLEAN"
+	// StageK8sMultiBaselineClean represents the state where all baseline resources should be removed.
+	StageK8sMultiBaselineClean = "K8S_BASELINE_CLEAN"
 )
 
 var allStages = []string{
@@ -37,6 +39,7 @@ var allStages = []string{
 	StageK8sMultiRollback,
 	StageK8sMultiCanaryRollout,
 	StageK8sMultiCanaryClean,
+	StageK8sMultiBaselineClean,
 }
 
 const (
@@ -48,6 +51,8 @@ const (
 	StageDescriptionK8sMultiCanaryRollout = "Rollout the new version as CANARY to all targets"
 	// StageDescriptionK8sMultiCanaryClean represents the description of the K8sCanaryClean stage.
 	StageDescriptionK8sMultiCanaryClean = "Remove all canary resources"
+	// StageDescriptionK8sMultiBaselineClean represents the description of the K8sBaselineClean stage.
+	StageDescriptionK8sMultiBaselineClean = "Remove all baseline resources"
 )
 
 func buildQuickSyncPipeline(autoRollback bool) []sdk.QuickSyncStage {
