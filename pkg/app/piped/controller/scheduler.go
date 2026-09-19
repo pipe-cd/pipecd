c4fc0040cadfae88c6aa08b19296b3861ce31f65
Ahmad Faraj <ahmedfrag4040@gmail.com>
2026-09-19 17:51:33 +0300

    chore(controller): explain cancel semantics in the scheduler
    Signed-off-by: Ahmad Faraj <ahmedfrag4040@gmail.com>

--- pkg/app/piped/controller/scheduler.go
+++ pkg/app/piped/controller/scheduler.go
@@ -401,6 +401,7 @@ func (s *scheduler) Run(ctx context.Context) error {
 
 		// A received cancel command wins even if the stage finished successfully,
 		// otherwise the loop would continue to the next stage after the user cancelled.
+		// Cancel stops progression, it does not rewrite an already-committed stage outcome.
 		if cancelCommand != nil {
 			deploymentStatus = model.DeploymentStatus_DEPLOYMENT_CANCELLED
 			statusReason = fmt.Sprintf("Cancelled by %s while executing stage %s", cancelCommander, ps.Id)
